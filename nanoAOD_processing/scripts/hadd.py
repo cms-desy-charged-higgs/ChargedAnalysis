d9f0b8e7f178f58fbe99c5f05e2c7bfd3b5e5303
David Brunner <davebrun@naf-cms15.desy.de>
2018-11-27 09:22:02 +0100

    Script for merging root files after processing

---
+++ nanoAOD_processing/scripts/hadd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+from glob import glob
+
+def parser():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--skim-dir", type = str, required = True)
+
+    return parser.parse_args()
+
+
+def get_hadd_targets(skimdir):
+    filedirs = os.listdir(skimdir)
+
+    haddtargets = {}
+
+    for filedir in filedirs:
+        haddtargets[filedir] = glob("{}{}/*.root".format(skimdir, filedir))
+
+    return haddtargets
+
+def main():
+    args = parser()
+
+    haddtargets = get_hadd_targets(args.skim_dir)
+
+    for target, rootfiles in haddtargets.iteritems():
+        os.system("hadd {}/{}/{}.root ".format(args.skim_dir, target, target) + " ".join(rootfiles))
+
+        
+if __name__ == "__main__":
+    main()
