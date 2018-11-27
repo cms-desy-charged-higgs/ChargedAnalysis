8249626806de2c1b52f59c8720d892e943e0bbe1
David Brunner <davebrun@naf-cms15.desy.de>
2018-11-27 09:23:03 +0100

    Bash shript for htcondor submit

---
+++ nanoAOD_processing/batch/condor_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+export SCRAM_ARCH=slc6_amd64_gcc630
+
+eval `scramv1 project CMSSW CMSSW_9_4_11_patch1`
+cd CMSSW_9_4_11_patch1/src/
+eval `scramv1 runtime -sh`
+
+mv ../../ChargedHiggs/ ./
+mv ../../x509 ./
+
+export X509_USER_PROXY=$CMSSW_BASE/src/x509
+
+git clone https://github.com/cms-nanoAOD/nanoAOD-tools.git PhysicsTools/NanoAODTools
+
+scram b
+
+python ChargedHiggs/nanoAOD_processing/scripts/nano_analysis.py --filename $1
+mv *.root ../../
+
