6864ef67ba374a3eda76aa7f6fdc7ba0c2f0afbf
DaveBrun94 <david.brunner@desy.de>
2018-11-30 12:56:52 +0100

    Module producing MC weight like xsec, ngen

---
+++ nanoAOD_processing/python/modules/MCWeightProducer.py
@@ -0,0 +1,57 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+import yaml
+import os
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object 
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+
+class MCWeightProducer(Module):
+    def __init__(self, era):
+        self.era = era
+
+        self.lumi = {
+                    2016: 35.87,
+                    2017: 41.37,
+        }
+
+    def beginJob(self):
+        pass
+
+    def endJob(self):
+        pass
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+
+        if not "RunFallII" in inputFile.GetName():
+            self.out.branch("lumi", "F")
+            self.out.branch("ngen", "F")
+            self.out.branch("xsec", "F")
+
+            self.isData = False
+            
+            self.processname = inputFile.GetName().split("/")[7]
+            self.processdic = yaml.load(file("{}/src/ChargedHiggs/nanoAOD_processing/data/xsec.yaml".format(os.environ["CMSSW_BASE"]), "r"))
+
+        else:
+            self.isData = True
+            
+            
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
+    def analyze(self, event):
+        if not self.isData:
+            self.out.fillBranch("lumi", self.lumi[self.era])
+            self.out.fillBranch("ngen", self.processdic[self.processname]["n_gen"])
+            self.out.fillBranch("xsec", self.processdic[self.processname]["xsec"])
+            
+
+        return True
+            
+# define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
+
+ChargedHiggsMCWeightProducer = lambda : MCWeightProducer(era = 2017) 
