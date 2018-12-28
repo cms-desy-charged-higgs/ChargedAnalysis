a7296ca7bb8e20bf65ccc23cd37b51f941e293f3
DaveBrun94 <david.brunner@desy.de>
2018-12-28 12:59:02 +0100

    Buildfiles for compiling

---
+++ analysis/src/classes.h
@@ -0,0 +1,10 @@
+#include <ChargedHiggs/analysis/interface/treereader.h>
+#include <ChargedHiggs/analysis/interface/plotter.h>
+
+namespace{
+    namespace{
+        TreeReader reader;
+        Plotter plotter;
+    }
+}
+
