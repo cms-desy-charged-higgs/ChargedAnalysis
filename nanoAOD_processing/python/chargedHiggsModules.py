e1cb71e715f8e8465308fcd2e9079cc4e7b21b5d
DaveBrun94 <david.brunner@desy.de>
2018-12-31 20:18:56 +0100

    Added Top Pt Reweighting

--- nanoAOD_processing/python/chargedHiggsModules.py
+++ nanoAOD_processing/python/chargedHiggsModules.py
@@ -3,18 +3,23 @@
 from ChargedHiggs.nanoAOD_processing.modules.triggerSelector import cHiggsEleTriggerSelector
 from ChargedHiggs.nanoAOD_processing.modules.electronSelector import cHiggsElectronSelector
 from ChargedHiggs.nanoAOD_processing.modules.jetSelector import cHiggsJetSelector
+from ChargedHiggs.nanoAOD_processing.modules.metFilter import cMetFilter
 
 ##Producer modules
 
+from ChargedHiggs.nanoAOD_processing.modules.quantitiesProducer import cHiggsQuantitiesProducer
 from ChargedHiggs.nanoAOD_processing.modules.MCWeightProducer import ChargedHiggsMCWeightProducer
 from ChargedHiggs.nanoAOD_processing.modules.puWeightProducer import puAutoWeight
-
+from ChargedHiggs.nanoAOD_processing.modules.topPtWeightProducer import cHiggstopPtWeightProducer
 
 module_list = [ 
+                cMetFilter(2017),
                 cHiggsEleTriggerSelector(),
-                cHiggsElectronSelector(2017, 36., 2.4),
-                cHiggsJetSelector(2017, 30., 2.4),
+                cHiggsElectronSelector(2017, 36., 2.4, 1),
+                cHiggsJetSelector(2017, 30., 2.4, 4),
    
+                cHiggsQuantitiesProducer(),
                 ChargedHiggsMCWeightProducer(),
                 puAutoWeight(),
+                cHiggstopPtWeightProducer(),
 ]
