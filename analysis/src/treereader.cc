27dead4848baf8f7b6d532c6501f51cca45935af
DaveBrun94 <david.brunner@desy.de>
2019-01-20 16:58:23 +0100

    Changed from SetBranchAddress to TTreeReader

--- analysis/src/treereader.cc
+++ analysis/src/treereader.cc
@@ -18,6 +18,7 @@ TreeReader::TreeReader(std::string &process):
     }){
 
     SetHistMap();   
+    SetCutMap();
 }
 
 //Add Histograms
@@ -25,7 +26,7 @@ TreeReader::TreeReader(std::string &process):
 void TreeReader::AddHistogram(std::vector<std::string> &parameters){
 
     for(std::string parameter: parameters){
-        TH1F* hist = new TH1F((parameter + "_" + process).c_str(), parameter.c_str(), histValues[parameter].nBins, histValues[parameter].xMin, histValues[parameter].xMax);
+        TH1F* hist = new TH1F(parameter.c_str(), parameter.c_str(), histValues[parameter].nBins, histValues[parameter].xMin, histValues[parameter].xMax);
 
         histograms[hist] = histValues[parameter].histFunc;
     } 
@@ -39,66 +40,80 @@ float TreeReader::GetWeight(Event event, std::vector<float> weights){
     }
 
     for(Lepton electron: event.leptons){
-        totalWeight *= electron.recoSF*electron.mediumMvaSF;
+        if(electron.isMedium){
+            totalWeight *= electron.recoSF*electron.mediumMvaSF;
+        }
     }
 
-    //totalWeight *= event.jets[0].bTagSF*event.jets[1].bTagSF;
+    for(Jet jet: event.jets){
+        if(jet.isMediumB){
+            //totalWeight *= jet.bTagSF;
+        }
+    }
 
     return totalWeight;
 }
 
-bool TreeReader::Cut(Event event){
-    bool cut = true;
+void TreeReader::EventLoop(std::vector<std::string> &filenames, std::vector<std::string> &cutstrings){
+    Processes proc = procDic[process];  
 
-    cut *= (event.leptons.size() == 1)*(event.leptons[0].isMedium)*(event.leptons[0].isolation < 0.1)*(event.leptons[0].fourVec.Pt() > 100.);
-
-    //*(event.jets.size() == 4)*(event.jets[0].isMediumB)*(event.jets[1].isMediumB);
-
-    return cut;
-}
-
-void TreeReader::EventLoop(std::vector<std::string> &filenames){
-    Processes proc = procDic[process];    
-
-    TChain* chain = new TChain("Events");   
-    
     for(std::string filename: filenames){
-        chain->Add(filename.c_str());
-    }
+        TFile* file = TFile::Open(filename.c_str());
+     
+        TTreeReader reader("Events", file);
+
+        TTreeReaderValue<std::vector<Lepton>> electrons(reader, "electron");
+        TTreeReaderValue<std::vector<Jet>> jets(reader, "jet");
+        TTreeReaderValue<Quantities> quantities(reader, "quantities");
+        TTreeReaderValue<TLorentzVector> MET(reader, "MET");
+
+        TTreeReaderValue<float> lumi;
+        TTreeReaderValue<float> xSec;
+        TTreeReaderValue<float> puWeight;
+        TTreeReaderValue<float> genWeight;
+
+        float nGen = 1.;
+
+        if(proc != DATA){
+            lumi = TTreeReaderValue<float>(reader, "lumi");
+            xSec = TTreeReaderValue<float>(reader, "xsec");
+            puWeight = TTreeReaderValue<float>(reader, "puWeight");
+            genWeight = TTreeReaderValue<float>(reader, "genWeight");
+    
+            TH1F* nGenHist = (TH1F*)file->Get("nGen");
+            nGen = nGenHist->Integral();
+        } 
+
+        std::vector<std::function<bool (Event)>> cuts; 
         
-    std::vector<Lepton>* electrons = NULL;
-    std::vector<Jet>* jets = NULL;
-    TLorentzVector* MET = NULL;
+        for(std::string cutstring: cutstrings){
+            cuts.push_back(cutValues[cutstring]);
+        }
 
-    chain->SetBranchAddress("electron", &electrons);
-    chain->SetBranchAddress("jet", &jets);
-    chain->SetBranchAddress("MET", &MET);
+        while(reader.Next()){    
+            std::vector<Lepton> ele = *electrons;
 
-    float lumi, nGen, xSec, puWeight, genWeight;
+            Event event = {*electrons, *jets, *quantities, *MET};
+       
+            bool cut = true;
+            
+            for(std::function<bool (Event)> cutValue: cuts){
+                cut *= cutValue(event);
+            }
+            
+            if(cut){
+                for(std::pair<TH1F*, std::function<float (Event)>> hist: histograms){
+                    if(proc != DATA){
+                        float weight = GetWeight(event, {*xSec, (*lumi)*(float)1e3, 1.f/nGen, *puWeight, *genWeight});
 
-    if(proc != DATA){
- 
-        chain->SetBranchAddress("lumi", &lumi);
-        chain->SetBranchAddress("ngen", &nGen);
-        chain->SetBranchAddress("xsec", &xSec);
-        chain->SetBranchAddress("puWeight", &puWeight);
-        chain->SetBranchAddress("genWeight", &genWeight);
-    }
+                        hist.first->Fill(hist.second(event), weight);
+                    }
+                    
+                    else{
+                        hist.first->Fill(hist.second(event));
+                    }
 
-    for (int i = 0, N = chain->GetEntries(); i < N; ++i) {
-        chain->GetEntry(i);
-        Event event = {*electrons, *jets, *MET};
-
-        if(Cut(event)){
-            for(std::pair<TH1F*, std::function<float (Event)>> hist: histograms){
-                if(proc != DATA){
-                    hist.first->Fill(hist.second(event), GetWeight(event, {lumi, 1.f/nGen, xSec, puWeight, genWeight}));
                 }
-                
-                else{
-                    hist.first->Fill(hist.second(event));
-                }
-
             }
         }
     }
