bdebf984e4d555f4d8d3a93d9186cfac6ca1c488
DaveBrun94 <david.brunner@desy.de>
2019-02-04 18:04:59 +0100

    Modified to the change of file input list

--- nanoAOD_processing/scripts/nano_analysis.py
+++ nanoAOD_processing/scripts/nano_analysis.py
@@ -2,8 +2,9 @@
 
 import os
 import argparse
-import time
 import sys
+import subprocess
+import time
 
 sys.path.append("/usr/lib64/python2.6/site-packages/")
 import htcondor
@@ -17,62 +18,87 @@ from ChargedHiggs.nanoAOD_processing.chargedHiggsModules import *
 
 
 def parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description = "Script to skim NANOAOD root files", formatter_class=argparse.RawTextHelpFormatter)
     
-    parser.add_argument("--analysis", type = str, choices = ["ChargedHiggs"], default = "ChargedHiggs")
-    parser.add_argument("--filename", type = str)
-    parser.add_argument("--filetxt", type = str)
+    parser.add_argument("--filename", action = "store", help = "Name of the input NANOAOD root file")
+    parser.add_argument("--channel", action = "store", required = True, choices = ["e4b", "m4b"], help = "Final state which is of interest")
+    parser.add_argument("--bkg-txt", action = "store", help = "Txt file with data set names of bkg samples")
+    parser.add_argument("--data-txt", action = "store", help = "Txt file with data set names of data samples")
+    parser.add_argument("--sig-txt", action = "store", help = "Txt file with SE path to personal dCache signal dir")
 
-    parser.add_argument("--batch", type = str, choices = ["local", "condor"], default = "local")
-    parser.add_argument("--out-dir", type = str, default = "{}/src".format(os.environ["CMSSW_BASE"]))
+    parser.add_argument("--batch", type = str, choices = ["local", "condor"], default = "local", help = "State if you want local or HTcondor jobs")
+    parser.add_argument("--out-dir", type = str, default = "{}/src".format(os.environ["CMSSW_BASE"]), help = "Name of output directory")
 
     return parser.parse_args()
 
-def get_filenames(filetxt):
+def get_filenames(bkgTXT, dataTXT, sigTXT):
+    ##dasgoclient python API
     dbs = DbsApi('https://cmsweb.cern.ch/dbs/prod/global/DBSReader')
-
     global_director = "root://cmsxrootd.fnal.gov/"
 
-    with open(filetxt) as f:
-        datasets = [dataset for dataset in f.read().splitlines() if dataset != ""]
+    ##Read out input files containing data set names
+    if bkgTXT:
+        with open(bkgTXT) as f:
+            background = [background for background in f.read().splitlines() if background != ""]
 
+    else:
+        background = []
+
+    if dataTXT:
+        with open(dataTXT) as f:
+            data = [data for data in f.read().splitlines() if data != ""]
+
+    else:
+        data = []
+
+    if sigTXT:
+        with open(sigTXT) as f:
+            signal = [signal for signal in f.read().splitlines() if signal != ""]
+
+    ##Fill file names in using dasgoclient API
     filelist = {}
 
-    for dataset in datasets:
-        if "RunIIFall" in dataset:
-            key = dataset.split("/")[1]
+    for setname in background + data:
+        if "mc" in setname:
+            key = setname.split("/")[1]
             
         else:
-            key = dataset.split("/")[1] + "-" +  dataset.split("/")[2]
+            key = setname.split("/")[1] + "-" +  setname.split("/")[2]
 
-        filelist[key] = [(global_director + filename['logical_file_name'], filename['event_count']) for filename in dbs.listFiles(dataset=dataset, detail=1)]
+        filelist[key] = [global_director + filename['logical_file_name'] for filename in dbs.listFiles(dataset=setname, detail=1)]
 
+    ##Read out signal files with gfal-ls command
+    for SEpath in signal:
+        key = SEpath.split("/")[-2]
+        signalFiles = subprocess.check_output(["gfal-ls", SEpath]).split("\n")[:-1]
+
+        filelist[key] = [global_director + SEpath[74:] + "/" + signalFile for signalFile in signalFiles]
+    
     return filelist
 
-def post_processor(out_dir, filelist):
-
+def post_processor(out_dir, filelist, channel):
+    ##NanoAOD postprocessor class
     p = PostProcessor(
                     outputDir=".", 
                     inputFiles= filelist,
                     noOut=False,
-                    modules= module_list,
+                    modules= module_list[channel],
                     outputbranchsel="{}/src/ChargedHiggs/nanoAOD_processing/data/keep_and_drop.txt".format(os.environ["CMSSW_BASE"]),                                        
     )
 
     p.run()
 
 
-def condor_submit(outdir, filename):
-    fname = filename[0]
-    nEvents = filename[1]
-
+def condor_submit(outdir, filename, channel):
+    ##Condor class    
     job = htcondor.Submit()
     schedd = htcondor.Schedd()
 
-    skimfilename = fname.split('/')[-1][:-5] + "_Skim.root" 
+    skimfilename = filename.split('/')[-1][:-5] + "_Skim.root" 
 
+    ##Condor configuration
     job["executable"] = "{}/src/ChargedHiggs/nanoAOD_processing/batch/condor_script.sh".format(os.environ["CMSSW_BASE"])
-    job["arguments"] = fname
+    job["arguments"] = " ".join([filename, channel])
     job["universe"]       = "vanilla"
 
     job["should_transfer_files"] = "YES"
@@ -86,10 +112,11 @@ def condor_submit(outdir, filename):
     job["when_to_transfer_output"] = "ON_EXIT"
     job["transfer_output_remaps"] = '"' + '{filename} = {outdir}/{filename}'.format(filename=skimfilename, outdir=outdir) + '"'
 
+    ##Agressively submit your jobs
     def submit(schedd, job):
         with schedd.transaction() as txn:
             job.queue(txn)
-            print "Submit job for file {}".format(fname)
+            print "Submit job for file {}".format(filename)
           
 
     while(True):
@@ -103,31 +130,29 @@ def condor_submit(outdir, filename):
 def main():
     args = parser()
 
-    filelist = []
-
-    if args.filetxt != None:
-        filelist = get_filenames(args.filetxt)
-
-    elif args.filename != None:
-        filelist.append(args.filename)
-
+    ##Call local with --filename argument
     if args.batch == "local":
-        post_processor(args.out_dir, filelist)
+        post_processor(args.out_dir, [args.filename], args.channel)
 
+    ##Call batch with txt files input
     elif args.batch == "condor":
+        filelist = get_filenames(args.bkg_txt, args.data_txt, args.sig_txt)
+
         skimdir = "/nfs/dust/cms/user/{}/Skimdir/nanoSkim_{}".format(os.environ["USER"], "_".join([str(time.localtime()[i]) for i in range(6)]))
 
+        ##Create dirs and copy proxy file to initialdir
         os.system("mkdir -p {}".format(skimdir))        
         os.system("chmod 755 {}".format(os.environ["X509_USER_PROXY"])) 
         os.system("cp -u {} {}/src/".format(os.environ["X509_USER_PROXY"], os.environ["CMSSW_BASE"])) 
 
+        ##Submit all the things
         for dirname, filenames in filelist.iteritems():
             outdir = skimdir + "/" + dirname
 
             os.system("mkdir -p {}/log".format(outdir))
 
             for filename in filenames:
-                condor_submit(outdir, filename)
+                condor_submit(outdir, filename, args.channel)
        
    
 if __name__ == "__main__":
