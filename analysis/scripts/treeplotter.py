ff66d2596ae164fb7877393e805e602de45c6f49
DaveBrun94 <david.brunner@desy.de>
2019-02-25 21:58:49 +0100

    Enabled to plot 2D hists

--- analysis/scripts/treeplotter.py
+++ analysis/scripts/treeplotter.py
@@ -22,8 +22,9 @@ def parser():
     parser.add_argument("--skim-dir", type = str)
     parser.add_argument("--plot-dir", type = str, default = "{}/src/Plots".format(os.environ["CMSSW_BASE"]))
 
-    parser.add_argument("--parameters", nargs="+")
-    parser.add_argument("--cuts", nargs="+", default=["baseline"])
+    parser.add_argument("--x-parameters", nargs="+")
+    parser.add_argument("--y-parameters", nargs="+", default = [])
+    parser.add_argument("--cuts", nargs="+", default=[])
     parser.add_argument("--weights", type = str, default = "")
 
     parser.add_argument("--hist-dir", type = str, default = "{}/src/Histograms".format(os.environ["CMSSW_BASE"]))
@@ -31,22 +32,30 @@ def parser():
 
     return parser.parse_args()
 
-def createHistograms(process, filenames, parameters, cuts, outdir):
+def createHistograms(process, filenames, xParameters, yParameters, cuts, outdir):
 
     outname = ROOT.std.string("{}/{}.root".format(outdir, process))
 
     timestamp1 = time.time()
-    reader = ROOT.TreeReader(ROOT.std.string(process), parameters, cuts)
+    reader = ROOT.TreeReader(ROOT.std.string(process), xParameters, yParameters, cuts)
     reader.EventLoop(filenames)
     reader.Write(outname)
     timestamp2 = time.time()
 
     print "Created histograms for process: {} ({} s)".format(process, timestamp2 - timestamp1)
 
-def makePlots(histDir, parameters, processes, plotDir):
+def makePlots1D(histDir, xParameters, processes, plotDir):
 
-    plotter = ROOT.Plotter(histDir, parameters)
+    plotter = ROOT.Plotter1D(histDir, xParameters)
     plotter.ConfigureHists(processes)
+    plotter.SetStyle()
+    plotter.Draw(plotDir)
+
+def makePlots2D(histDir, xParameters, yParameters, processes, plotDir):
+
+    plotter = ROOT.Plotter2D(histDir, xParameters, yParameters)
+    plotter.ConfigureHists(processes)
+    plotter.SetStyle()
     plotter.Draw(plotDir)
     
 def main():
@@ -71,8 +80,11 @@ def main():
             os.system("mkdir -p {}".format(args.www))
 
     ##Create std::vector for parameters
-    parameters = ROOT.std.vector("string")()
-    [parameters.push_back(param) for param in args.parameters]
+    xParameters = ROOT.std.vector("string")()
+    [xParameters.push_back(param) for param in args.x_parameters]
+
+    yParameters = ROOT.std.vector("string")()
+    [yParameters.push_back(param) for param in args.y_parameters]
 
     processes = ROOT.std.vector("string")()
     [processes.push_back(proc) for proc in args.background + args.data + args.signal]
@@ -93,9 +105,12 @@ def main():
         
         [filenames.push_back(fname) for fname in ["{skim}{file}/{file}.root".format(skim = args.skim_dir, file = process_file) for process_file in process_dic[process]["filenames"]]]       
 
-        createHistograms(process, filenames, parameters, cuts, histDir)
+        createHistograms(process, filenames, xParameters, yParameters, cuts, histDir)
     
-    makePlots(histDir, parameters, processes, outDir)
+    makePlots1D(histDir, xParameters, processes, outDir)
+
+    if args.y_parameters:
+        makePlots2D(histDir, xParameters, yParameters, processes, outDir)
 
     if args.www:
         os.system("rsync -arvt --exclude {./,.git*} --delete -e ssh $HOME2/webpage/ $CERN_USER@lxplus.cern.ch:/eos/home-${USER:0:1}/$CERN_USER/www/")
