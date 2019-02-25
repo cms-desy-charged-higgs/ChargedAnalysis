fe26b7870ae771da2182caaf4b3f1b19e488d980
DaveBrun94 <david.brunner@desy.de>
2019-02-25 22:01:00 +0100

    Added now plotter classes

--- analysis/src/classes.h
+++ analysis/src/classes.h
@@ -1,10 +1,14 @@
 #include <ChargedHiggs/analysis/interface/treereader.h>
-#include <ChargedHiggs/analysis/interface/plotter.h>
+#include <ChargedHiggs/analysis/interface/plotter1D.h>
+#include <ChargedHiggs/analysis/interface/plotter2D.h>
+#include <ChargedHiggs/analysis/interface/plotterGen.h>
 
 namespace{
     namespace{
         TreeReader reader;
-        Plotter plotter;
+        Plotter1D plotter1D;
+        Plotter2D plotter2D;
+        PlotterGen plotterGen;
     }
 }
 
