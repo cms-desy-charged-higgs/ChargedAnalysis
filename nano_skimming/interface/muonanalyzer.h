b856fb8c4006cb74dace04404f510e6b85fcef13
DaveBrun94 <david.brunner@desy.de>
2019-03-11 14:31:08 +0100

    Analyzer class for selecting muons

---
+++ nano_skimming/interface/muonanalyzer.h
@@ -0,0 +1,80 @@
+#ifndef MUONANALYZER_H
+#define MUONANALYZER_H
+
+#include <ChargedHiggs/nano_skimming/interface/baseanalyzer.h>
+
+//Muon class to be safed in tree
+struct Muon {
+    TLorentzVector fourVec;
+    Bool_t isMedium;
+    Bool_t isTight;
+    Bool_t isLooseIso;
+    Bool_t isTightIso;
+
+    Float_t looseIsoMediumSF = 1.;
+    Float_t tightIsoMediumSF = 1.;
+    Float_t looseIsoTightSF = 1.;
+    Float_t tightIsoTightSF = 1.;
+    Float_t mediumSF = 1.;
+    Float_t tightSF = 1.;
+    Float_t triggerSF = 1.;
+
+    Bool_t isTriggerMatched;
+
+    TLorentzVector genVec;
+    Bool_t isFromHc;
+
+    Float_t charge;
+};
+
+class MuonAnalyzer: public BaseAnalyzer{
+    private:
+        //Bool for checking if data file
+        bool isData;
+
+        //Map for SF files
+        std::map<int, std::string> isoSFfiles;
+        std::map<int, std::string> triggerSFfiles;
+        std::map<int, std::string> IDSFfiles;
+
+        //Hist with scale factors
+        TH2F* triggerSFhist;
+
+        TH2F* looseIsoMediumSFhist;
+        TH2F* tightIsoMediumSFhist;
+        TH2F* looseIsoTightSFhist;
+        TH2F* tightIsoTightSFhist;
+
+        TH2F* mediumIDHist;
+        TH2F* tightIDHist;
+
+        //Input for selecting muon
+        int era;
+        float ptCut;
+        float etaCut;
+        unsigned int minNMuon;
+
+        //TTreeReader Values
+        std::unique_ptr<TTreeReaderArray<float>> muonPt;
+        std::unique_ptr<TTreeReaderArray<float>> muonEta;
+        std::unique_ptr<TTreeReaderArray<float>> muonPhi;
+        std::unique_ptr<TTreeReaderArray<float>> muonIso;
+        std::unique_ptr<TTreeReaderArray<int>> muonCharge;
+        std::unique_ptr<TTreeReaderArray<bool>> muonMediumID;
+        std::unique_ptr<TTreeReaderArray<bool>> muonTightID;
+        std::unique_ptr<TTreeReaderArray<int>> muonGenIdx;
+
+        //Valid electron collection
+        std::vector<Muon> validMuons;
+
+        //Set Gen particle information
+        void SetGenParticles(Muon &validMuon, const int &i);
+
+    public:
+        MuonAnalyzer(const int &era, const float &ptCut, const float &etaCut, const int &minNMuon);
+        void BeginJob(TTreeReader &reader, TTree* tree, bool &isData);
+        bool Analyze();
+        void EndJob(TFile* file);
+};
+
+#endif
---
+++ nano_skimming/src/muonananalyzer.cc
@@ -0,0 +1,126 @@
+#include <ChargedHiggs/nano_skimming/interface/muonanalyzer.h>
+
+MuonAnalyzer::MuonAnalyzer(const int &era, const float &ptCut, const float &etaCut, const int &minNMuon):
+    BaseAnalyzer(),    
+    era(era),
+    ptCut(ptCut),
+    etaCut(etaCut),
+    minNMuon(minNMuon)
+    {
+        isoSFfiles = {
+            {2017, filePath + "/muonSF/RunBCDEF_SF_ISO.root"},
+        };
+
+        triggerSFfiles = {
+            {2017, filePath + "/muonSF/EfficienciesAndSF_RunBtoF_Nov17Nov2017.root"},
+        };
+
+        
+        IDSFfiles = {
+            {2017, filePath + "/muonSF/RunBCDEF_SF_ID.root"},
+        };
+}
+
+
+void MuonAnalyzer::SetGenParticles(Muon &validMuon, const int &i){
+    //Check if gen matched particle exist
+
+    if(muonGenIdx->At(i) != -1){
+        validMuon.genVec.SetPtEtaPhiM(genPt->At(muonGenIdx->At(i)), genEta->At(muonGenIdx->At(i)), genPhi->At(muonGenIdx->At(i)), genMass->At(muonGenIdx->At(i)));
+
+        //Check if gen ele is from W Boson from Charged Higgs
+        if(abs(genID->At(muonGenIdx->At(i))) == 24){
+            if(abs(genID->At(genMotherIdx->At(genID->At(muonGenIdx->At(i))))) == 37){
+                validMuon.isFromHc = true;
+            }
+        }
+    }
+}
+
+void MuonAnalyzer::BeginJob(TTreeReader &reader, TTree* tree, bool &isData){
+    //Set data bool
+    this->isData = isData;
+
+    //Hist with scale factors
+    TFile* triggerSFfile = TFile::Open(triggerSFfiles[era].c_str());
+    triggerSFhist = (TH2F*)triggerSFfile->Get("IsoMu27_PtEtaBins/pt_abseta_ratio");
+
+    TFile* isoSFfile = TFile::Open(isoSFfiles[era].c_str());
+    looseIsoMediumSFhist = (TH2F*)isoSFfile->Get("NUM_LooseRelIso_DEN_MediumID_pt_abseta");
+    tightIsoMediumSFhist = (TH2F*)isoSFfile->Get("NUM_TightRelIso_DEN_MediumID_pt_abseta");
+    looseIsoTightSFhist = (TH2F*)isoSFfile->Get("NUM_LooseRelIso_DEN_TightIDandIPCut_pt_abseta");
+    tightIsoTightSFhist = (TH2F*)isoSFfile->Get("NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta");
+
+    TFile* IDSFfile = TFile::Open(IDSFfiles[era].c_str());
+    mediumIDHist = (TH2F*)IDSFfile->Get("NUM_MediumID_DEN_genTracks_pt_abseta");
+    tightIDHist = (TH2F*)IDSFfile->Get("NUM_TightID_DEN_genTracks_pt_abseta");
+
+    //Initiliaze TTreeReaderValues
+    muonPt = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_pt");
+    muonEta = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_eta");
+    muonPhi = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_phi");
+    muonCharge = std::make_unique<TTreeReaderArray<int>>(reader, "Muon_charge");
+    muonIso = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_pfRelIso04_all");
+    muonMediumID = std::make_unique<TTreeReaderArray<bool>>(reader, "Muon_mediumId");
+    muonTightID = std::make_unique<TTreeReaderArray<bool>>(reader, "Muon_tightId");
+
+    if(!this->isData){
+        muonGenIdx = std::make_unique<TTreeReaderArray<int>>(reader, "Muon_genPartIdx");
+    }
+
+
+    //Set TTreeReader for genpart and trigger obj from baseanalyzer
+    SetCollection(reader, this->isData);
+
+    //Set Branches of output tree
+    tree->Branch("muon", &validMuons);
+}
+
+bool MuonAnalyzer::Analyze(){
+    //Clear electron vector
+    validMuons.clear();
+
+    //Loop over all electrons
+    for(unsigned int i = 0; i < muonPt->GetSize(); i++){
+        if(muonPt->At(i) > ptCut && abs(muonEta->At(i)) < etaCut){
+            Muon validMuon;
+
+            //Set muon information
+            validMuon.fourVec.SetPtEtaPhiM(muonPt->At(i), muonEta->At(i), muonPhi->At(i), 105.658*1e-3);
+
+            validMuon.isMedium = muonMediumID->At(i);
+            validMuon.isTight = muonTightID->At(i);
+            validMuon.isLooseIso = muonIso->At(i) < 0.25;
+            validMuon.isTightIso = muonIso->At(i) < 0.15;
+            validMuon.charge = muonCharge->At(i);
+            validMuon.isTriggerMatched = triggerMatching(validMuon.fourVec, 13);
+            
+            if(!isData){
+                validMuon.triggerSF = triggerSFhist->GetBinContent(triggerSFhist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.mediumSF = mediumIDHist->GetBinContent(mediumIDHist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.tightSF = tightIDHist->GetBinContent(tightIDHist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.looseIsoMediumSF = looseIsoMediumSFhist->GetBinContent(looseIsoMediumSFhist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.tightIsoMediumSF = tightIsoMediumSFhist->GetBinContent(tightIsoMediumSFhist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.looseIsoTightSF = looseIsoTightSFhist->GetBinContent(looseIsoTightSFhist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+                validMuon.tightIsoTightSF = tightIsoTightSFhist->GetBinContent(tightIsoTightSFhist->FindBin(muonPt->At(i), abs(muonEta->At(i))));
+
+                //Save gen particle information
+                SetGenParticles(validMuon, i);
+             }
+
+            //Fill electron in collection
+            validMuons.push_back(validMuon);       
+        } 
+    }
+    
+    //Check if event has enough electrons
+    if(validMuons.size() < minNMuon){
+        return false;
+    }
+
+    return true;
+}
+
+
+void MuonAnalyzer::EndJob(TFile* file){
+}
