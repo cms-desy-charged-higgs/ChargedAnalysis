900e58acc0093495c2c887898d97e920e89614f8
DaveBrun94 <david.brunner@desy.de>
2019-03-11 14:28:34 +0100

    Script which is executed at the batch for skimming

---
+++ nano_skimming/batch/produceSkim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+export SCRAM_ARCH=slc6_amd64_gcc630
+
+eval `scramv1 project CMSSW CMSSW_9_4_11_patch1`
+cd CMSSW_9_4_11_patch1/src/
+eval `scramv1 runtime -sh`
+
+mv ../../ChargedHiggs/ ./
+mv ../../x509 ./
+
+export X509_USER_PROXY=$CMSSW_BASE/src/x509
+
+scram b
+
+xrdcp $1 $(echo $1 | tr '/' '_')
+
+python ChargedHiggs/nano_skimming/scripts/nanoskimmer.py --filename $(echo $1 | tr '/' '_') --out-name $2 --channel $3 $4
+rm $(echo $1 | tr '/' '_')
+mv *.root ../../
