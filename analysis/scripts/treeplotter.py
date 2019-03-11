9c5d07ea8eca47fe55ab0fed2f73032e9050b570
DaveBrun94 <david.brunner@desy.de>
2019-03-11 14:45:04 +0100

    More parser help information and removed time measure of treerader class execution

--- analysis/scripts/treeplotter.py
+++ analysis/scripts/treeplotter.py
@@ -3,8 +3,6 @@
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-import time
-
 import os
 import yaml
 import argparse
@@ -14,35 +12,31 @@ from multiprocessing import Pool, cpu_count
 def parser():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("--background", nargs="+", default = ["TT+X", "T+X", "QCD", "W+j", "DY+j", "VV+VVV"])
-    parser.add_argument("--data", nargs="+", default = [])
-    parser.add_argument("--signal", nargs = "+", default = [])   
+    parser.add_argument("--background", nargs="+", default = ["TT+X", "T+X", "QCD", "W+j", "DY+j", "VV+VVV"], help = "Name of background processes")
+    parser.add_argument("--data", nargs="+", default = [], help = "Name of data process")
+    parser.add_argument("--signal", nargs = "+", default = [], help = "Name of data process")   
 
-    parser.add_argument("--process-yaml", type = str, default = "{}/src/ChargedHiggs/analysis/data/process.yaml".format(os.environ["CMSSW_BASE"]))
-    parser.add_argument("--skim-dir", type = str)
-    parser.add_argument("--plot-dir", type = str, default = "{}/src/Plots".format(os.environ["CMSSW_BASE"]))
+    parser.add_argument("--skim-dir", type = str, help = "Directory with skimmed files")
+    parser.add_argument("--process-yaml", type = str, default = "{}/src/ChargedHiggs/analysis/data/process.yaml".format(os.environ["CMSSW_BASE"]), help = "Yaml configuration which maps process name and skimmed files names")
+    parser.add_argument("--plot-dir", type = str, default = "{}/src/Plots".format(os.environ["CMSSW_BASE"]), help = "Output directory of plots")
 
-    parser.add_argument("--x-parameters", nargs="+")
-    parser.add_argument("--y-parameters", nargs="+", default = [])
-    parser.add_argument("--cuts", nargs="+", default=[])
-    parser.add_argument("--weights", type = str, default = "")
+    parser.add_argument("--channel", type = str, required = True, help = "Final state which is of interest")
+    parser.add_argument("--x-parameters", nargs="+", help = "Name of parameters to be plotted")
+    parser.add_argument("--y-parameters", nargs="+", default = [], help = "Y parameters for 2D plots")
+    parser.add_argument("--cuts", nargs="+", default=[], help = "Name of cut implemented in TreeReader class")
 
-    parser.add_argument("--hist-dir", type = str, default = "{}/src/Histograms".format(os.environ["CMSSW_BASE"]))
-    parser.add_argument("--www", type=str)
+    parser.add_argument("--hist-dir", type = str, default = "{}/src/Histograms".format(os.environ["CMSSW_BASE"]), help = "Dir with histograms read out of skimmed trees")
+    parser.add_argument("--www", type=str, help = "Dir to web plot dir")
 
     return parser.parse_args()
 
-def createHistograms(process, filenames, xParameters, yParameters, cuts, outdir):
+def createHistograms(process, filenames, xParameters, yParameters, cuts, outdir, channel):
 
     outname = ROOT.std.string("{}/{}.root".format(outdir, process))
 
-    timestamp1 = time.time()
     reader = ROOT.TreeReader(ROOT.std.string(process), xParameters, yParameters, cuts)
-    reader.EventLoop(filenames)
+    reader.EventLoop(filenames, ROOT.std.string(channel))
     reader.Write(outname)
-    timestamp2 = time.time()
-
-    print "Created histograms for process: {} ({} s)".format(process, timestamp2 - timestamp1)
 
 def makePlots1D(histDir, xParameters, processes, plotDir):
 
@@ -105,7 +99,7 @@ def main():
         
         [filenames.push_back(fname) for fname in ["{skim}{file}/{file}.root".format(skim = args.skim_dir, file = process_file) for process_file in process_dic[process]["filenames"]]]       
 
-        createHistograms(process, filenames, xParameters, yParameters, cuts, histDir)
+        createHistograms(process, filenames, xParameters, yParameters, cuts, histDir, args.channel)
     
     makePlots1D(histDir, xParameters, processes, outDir)
 
