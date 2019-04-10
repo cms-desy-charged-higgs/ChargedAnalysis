c233a1e7e12c34c6bedfc9c1a956c3e4be683561
DaveBrun94 <david.brunner@desy.de>
2019-04-11 00:05:19 +0200

    Fatjets are now analyzed

--- nano_skimming/interface/jetanalyzer.h
+++ nano_skimming/interface/jetanalyzer.h
@@ -12,51 +12,63 @@
 //Jet class to be safed in tree
 struct Jet {
     TLorentzVector fourVec;
-    Bool_t isLooseB;
-    Bool_t isMediumB;
-    Bool_t isTightB;
+    Bool_t isLooseB = false;
+    Bool_t isMediumB = false;
+    Bool_t isTightB = false;
+
     Float_t loosebTagSF = 1.;
     Float_t mediumbTagSF = 1.;
     Float_t tightbTagSF = 1.;
+
+    Float_t twoSubJettiness;
+    Float_t threeSubJettiness;
 
     TLorentzVector genVec;
     Bool_t isFromh1 = false;
     Bool_t isFromh2 = false;
 };
 
-
-
 class JetAnalyzer: public BaseAnalyzer{
+    enum JetType {AK4, AK8};
+
     private:
         //Bool for checking if data file
         bool isData;
 
         //Map for SF files
-        std::map<int, std::string> bTagSF;
-        std::map<int, std::string> JMESF;
-        std::map<int, std::string> JMEPtReso;
+        std::map<JetType, std::map<int, std::string>> bTagSF;
+        std::map<JetType, std::map<int, std::string>> JMESF;
+        std::map<JetType, std::map<int, std::string>> JMEPtReso;
 
         //Values for bTag cuts
-        std::map<int, std::vector<float>> bTagCuts; 
+        std::map<JetType, std::map<int, std::vector<float>>> bTagCuts; 
 
         //Classes for reading btag SF
-        BTagCalibration calib;
-        BTagCalibrationReader looseReader;
-        BTagCalibrationReader mediumReader;
-        BTagCalibrationReader tightReader;
+        std::map<JetType, BTagCalibration> calib;
+        std::map<JetType, BTagCalibrationReader> looseReader;
+        std::map<JetType, BTagCalibrationReader> mediumReader;
+        std::map<JetType, BTagCalibrationReader> tightReader;
 
         //Classes for reading jet energy SF 
         JME::JetParameters jetParameter;
-        JME::JetResolution resolution;
-        JME::JetResolutionScaleFactor resolution_sf;
+        std::map<JetType, JME::JetResolution> resolution;
+        std::map<JetType, JME::JetResolutionScaleFactor> resolution_sf;
 
         //Input for selecting jets
         int era;
         float ptCut;
         float etaCut;
-        unsigned int minNJet;
+        std::vector<std::pair<unsigned int, unsigned int>> minNJet;
 
         //TTreeReader Values
+        std::unique_ptr<TTreeReaderArray<float>> fatJetPt;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetEta;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetPhi;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetMass;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetCSV;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetTau2;
+        std::unique_ptr<TTreeReaderArray<float>> fatJetTau3;
+
         std::unique_ptr<TTreeReaderArray<float>> jetPt;
         std::unique_ptr<TTreeReaderArray<float>> jetEta;
         std::unique_ptr<TTreeReaderArray<float>> jetPhi;
@@ -66,9 +78,22 @@ class JetAnalyzer: public BaseAnalyzer{
         std::unique_ptr<TTreeReaderArray<float>> jetDeepBValue;
         std::unique_ptr<TTreeReaderValue<float>> valueHT;
 
+        std::unique_ptr<TTreeReaderArray<int>> eleIdx;
+        std::unique_ptr<TTreeReaderArray<float>> elePt;
+        std::unique_ptr<TTreeReaderArray<float>> eleEta;
+        std::unique_ptr<TTreeReaderArray<int>> muonIdx;
+        std::unique_ptr<TTreeReaderArray<float>> muonPt;
+        std::unique_ptr<TTreeReaderArray<float>> muonEta;
+
         std::unique_ptr<TTreeReaderArray<float>> genJetPt;
         std::unique_ptr<TTreeReaderArray<float>> genJetEta;
         std::unique_ptr<TTreeReaderArray<float>> genJetPhi;
+        std::unique_ptr<TTreeReaderArray<float>> genJetMass;
+
+        std::unique_ptr<TTreeReaderArray<float>> genFatJetPt;
+        std::unique_ptr<TTreeReaderArray<float>> genFatJetEta;
+        std::unique_ptr<TTreeReaderArray<float>> genFatJetPhi;
+        std::unique_ptr<TTreeReaderArray<float>> genFatJetMass;
 
         std::unique_ptr<TTreeReaderValue<float>> metPhi;
         std::unique_ptr<TTreeReaderValue<float>> metPt;
@@ -78,18 +103,19 @@ class JetAnalyzer: public BaseAnalyzer{
 
         //Valid jet collection
         std::vector<Jet> validJets;
+        std::vector<Jet> fatJets;
 
         //met Lorentzvector
         TLorentzVector met;
 
         //Get JER smear factor
-        float SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float &rho);
+        float SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float &rho, const float &coneSize, const JetType &type);
 
         //Set Gen particle information
         void SetGenParticles(Jet &validJet, const int &i);
 
     public:
-        JetAnalyzer(const int &era, const float &ptCut, const float &etaCut, const int &minNJet);
+        JetAnalyzer(const int &era, const float &ptCut, const float &etaCut, const std::vector<std::pair<unsigned int, unsigned int>> minNJet);
         void BeginJob(TTreeReader &reader, TTree* tree, bool &isData);
         bool Analyze();
         void EndJob(TFile* file);
--- nano_skimming/src/jetanalyzer.cc
+++ nano_skimming/src/jetanalyzer.cc
@@ -1,6 +1,6 @@
 #include <ChargedHiggs/nano_skimming/interface/jetanalyzer.h>
 
-JetAnalyzer::JetAnalyzer(const int &era, const float &ptCut, const float &etaCut, const int &minNJet):
+JetAnalyzer::JetAnalyzer(const int &era, const float &ptCut, const float &etaCut, const std::vector<std::pair<unsigned int, unsigned int>> minNJet):
     BaseAnalyzer(),    
     era(era),
     ptCut(ptCut),
@@ -8,44 +8,85 @@ JetAnalyzer::JetAnalyzer(const int &era, const float &ptCut, const float &etaCut
     minNJet(minNJet)
     {
         bTagSF = {
-                {2017, filePath + "/btagSF/DeepFlavour_94XSF_V1_B_F.csv"},
+                {AK4, {
+                        {2017, filePath + "/btagSF/DeepFlavour_94XSF_V1_B_F.csv"},
+                    }
+                },
+
+                {AK8, {
+                        {2017, filePath + "/btagSF/subjet_DeepCSV_94XSF_V4_B_F.csv"},
+                    }
+                },
         };
 
         JMESF = {
-                {2017, filePath + "/JME/Fall17_V3_MC_SF_AK4PFchs.txt"},
+                {AK4, {
+                        {2017, filePath + "/JME/Fall17_V3_MC_SF_AK4PFchs.txt"},
+                    }
+                },       
+
+                {AK8, {
+                        {2017, filePath + "/JME/Fall17_V3_MC_SF_AK8PFchs.txt"},
+                    }
+                },             
         };
 
         JMEPtReso = {
-                {2017, filePath + "/JME/Fall17_V3_MC_PtResolution_AK4PFchs.txt"},
+                {AK4, {
+                        {2017, filePath + "/JME/Fall17_V3_MC_PtResolution_AK4PFchs.txt"},
+                    }
+                },       
+
+                {AK8, {
+                        {2017, filePath + "/JME/Fall17_V3_MC_PtResolution_AK8PFchs.txt"},
+                    }
+                },
         };
 
         //https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation94
         bTagCuts = {
-                {2017, {0.051, 0.3033, 0.7489}}, //Loose, Medium, Tight
+                {AK4, {
+                        {2017, {0.051, 0.3033, 0.7489}}, //Loose, Medium, Tight
+                    }
+                },
+
+                {AK8, {
+                        {2017, {0.1522, 0.4941}}, //Loose, Medium, Tight
+                    }
+                },
         };
     }
 
 //https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetResolution#Smearing_procedures
 //https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_25/PhysicsTools/PatUtils/interface/SmearedJetProducerT.h#L203-L263
-float JetAnalyzer::SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float &rho){
+float JetAnalyzer::SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float &rho, const float &coneSize, const JetType &type){
     //Configure jet SF reader
     jetParameter.setJetPt(jetPt).setJetEta(jetEta).setRho(rho);
 
     float dR;
     TLorentzVector matchedGenJet;
-    float reso = resolution.getResolution(jetParameter);
-    float resoSF = resolution_sf.getScaleFactor(jetParameter);
+    float reso = resolution[type].getResolution(jetParameter);
+    float resoSF = resolution_sf[type].getScaleFactor(jetParameter);
     float smearFac = 1.; 
 
+    float genPt, genPhi, genEta, genMass;
+    unsigned int size = (type == AK4) ? genJetPt->GetSize(): genFatJetPt->GetSize();
+
     //Loop over all gen jets and find match
-    for(unsigned int i = 0; i < genJetPt->GetSize(); i++){
-        dR = std::sqrt(std::pow(jetPhi-genJetPhi->At(i), 2) + std::pow(jetEta-genJetEta->At(i), 2));
+    for(unsigned int i = 0; i < size; i++){
+        genPt = (type == AK4) ? genJetPt->At(i): genFatJetPt->At(i);
+        genPhi = (type == AK4) ? genJetPhi->At(i): genFatJetPhi->At(i);
+        genEta = (type == AK4) ? genJetEta->At(i): genFatJetEta->At(i);
+        genMass = (type == AK4) ? genJetMass->At(i): genFatJetMass->At(i);
+
+        dR = std::sqrt(std::pow(jetPhi-genPhi, 2) + std::pow(jetEta-genEta, 2));
 
         //Check if jet and gen jet are matched
-        if(dR < 0.4/2. and abs(jetPt - genJetPt->At(i)) < 3.*reso*jetPt){
-            matchedGenJet.SetPtEtaPhiM(genJetPt->At(i), genJetEta->At(i), genJetPhi->At(i), 0);
+        if(dR < coneSize/2. and abs(jetPt - genPt) < 3.*reso*jetPt){
+            matchedGenJet.SetPtEtaPhiM(genPt, genEta, genPhi, genMass);
+            break;
         }
-    }
+    }  
 
     //If you found gen matched 
     if(matchedGenJet != TLorentzVector()){
@@ -60,6 +101,7 @@ float JetAnalyzer::SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float
         smearFac = 1. + gaus(generator);
     }
 
+
     //Check if direction of jet not changed
     if(jetPt*smearFac < 1e-2){
         smearFac = 1e-2/jetPt;
@@ -71,16 +113,33 @@ float JetAnalyzer::SmearEnergy(float &jetPt, float &jetPhi, float &jetEta, float
 void JetAnalyzer::SetGenParticles(Jet &validJet, const int &i){
     //Check if gen matched particle exist
     if(jetGenIdx->At(i) != -1){
-        int jetIdx = jetGenIdx->At(i);
-
-        validJet.genVec.SetPtEtaPhiM(genPt->At(jetIdx), genEta->At(jetIdx), genPhi->At(jetIdx), genMass->At(jetIdx));
-
-        std::cout << genID->At(jetGenIdx->At(i)) << std::endl;
-            //Check if gen jet from small higgs
-
-            if(abs(genID->At(genMotherIdx->At(jetIdx))) == 25){
-                int indexh = genMotherIdx->At(jetIdx);
+        int genPartIdx = -1;
+        float dR;
         
+        //Find Gen particle to gen Jet
+        for(unsigned int index=0; index < genPt->GetSize(); index++){
+            dR = std::sqrt(std::pow(genPhi->At(index) -genJetPhi->At(jetGenIdx->At(i)), 2) + std::pow(genEta->At(index)-genJetEta->At(jetGenIdx->At(i)), 2));
+
+            if(dR < 0.05 and abs(genPt->At(index) - genJetPt->At(jetGenIdx->At(i)))/genPt->At(index) < 0.1 and abs(genID->At(index)) < 22){
+                genPartIdx = index;
+            }
+        }
+
+        if(genPartIdx != -1){
+            validJet.genVec.SetPtEtaPhiM(genPt->At(genPartIdx), genEta->At(genPartIdx), genPhi->At(genPartIdx), genMass->At(genPartIdx));
+
+            while(genID->At(genMotherIdx->At(genPartIdx)) == genID->At(genPartIdx)){
+                genPartIdx = genMotherIdx->At(genPartIdx);
+            }
+
+            //Check if gen jet from small higgs
+            if(abs(genID->At(genMotherIdx->At(genPartIdx))) == 25){
+                int indexh = genMotherIdx->At(genPartIdx);
+
+                while(genID->At(genMotherIdx->At(indexh)) == genID->At(indexh)){
+                    indexh = genMotherIdx->At(indexh);
+                }
+
                 if(abs(genID->At(genMotherIdx->At(indexh))) == 37){
                     validJet.isFromh1 = true;
                     validJet.isFromh2 = false;
@@ -91,7 +150,7 @@ void JetAnalyzer::SetGenParticles(Jet &validJet, const int &i){
                     validJet.isFromh2 = true;
                 }
             }
-   
+        }
     }
 }
 
@@ -100,6 +159,14 @@ void JetAnalyzer::BeginJob(TTreeReader &reader, TTree* tree, bool &isData){
     this->isData = isData;
 
     //Initiliaze TTreeReaderValues
+    fatJetPt = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_pt");
+    fatJetEta = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_eta");
+    fatJetPhi = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_phi");
+    fatJetMass = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_mass");
+    fatJetCSV = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_btagDeepB");
+    fatJetTau2 = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_tau2");
+    fatJetTau3 = std::make_unique<TTreeReaderArray<float>>(reader, "FatJet_tau3");
+
     jetPt = std::make_unique<TTreeReaderArray<float>>(reader, "Jet_pt");
     jetEta = std::make_unique<TTreeReaderArray<float>>(reader, "Jet_eta");
     jetPhi = std::make_unique<TTreeReaderArray<float>>(reader, "Jet_phi");
@@ -107,6 +174,13 @@ void JetAnalyzer::BeginJob(TTreeReader &reader, TTree* tree, bool &isData){
     jetRho = std::make_unique<TTreeReaderValue<float>>(reader, "fixedGridRhoFastjetAll");
     jetDeepBValue = std::make_unique<TTreeReaderArray<float>>(reader, "Jet_btagDeepFlavB");
     valueHT = std::make_unique<TTreeReaderValue<float>>(reader, "SoftActivityJetHT");
+
+    eleIdx = std::make_unique<TTreeReaderArray<int>>(reader, "Jet_electronIdx1");
+    elePt = std::make_unique<TTreeReaderArray<float>>(reader, "Electron_pt");
+    eleEta = std::make_unique<TTreeReaderArray<float>>(reader, "Electron_eta");
+    muonIdx = std::make_unique<TTreeReaderArray<int>>(reader, "Jet_muonIdx1");
+    muonPt = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_pt");
+    muonEta = std::make_unique<TTreeReaderArray<float>>(reader, "Muon_eta");
     
     metPhi = std::make_unique<TTreeReaderValue<float>>(reader, "MET_phi");
     metPt = std::make_unique<TTreeReaderValue<float>>(reader, "MET_pt");
@@ -115,29 +189,42 @@ void JetAnalyzer::BeginJob(TTreeReader &reader, TTree* tree, bool &isData){
         genJetPt = std::make_unique<TTreeReaderArray<float>>(reader, "GenJet_pt");
         genJetEta = std::make_unique<TTreeReaderArray<float>>(reader, "GenJet_eta");
         genJetPhi = std::make_unique<TTreeReaderArray<float>>(reader, "GenJet_phi");
+        genJetMass = std::make_unique<TTreeReaderArray<float>>(reader, "GenJet_mass");
+
+        genFatJetPt = std::make_unique<TTreeReaderArray<float>>(reader, "GenJetAK8_pt");
+        genFatJetEta = std::make_unique<TTreeReaderArray<float>>(reader, "GenJetAK8_eta");
+        genFatJetPhi = std::make_unique<TTreeReaderArray<float>>(reader, "GenJetAK8_phi");
+        genFatJetMass = std::make_unique<TTreeReaderArray<float>>(reader, "GenJetAK8_mass");
         
         jetGenIdx = std::make_unique<TTreeReaderArray<int>>(reader, "Jet_genJetIdx");
     }
 
-    //Set configuration for bTagSF reader  ##https://twiki.cern.ch/twiki/bin/view/CMS/BTagCalibration
-    calib = BTagCalibration("DeepFlavourB", bTagSF[era]);
-    looseReader = BTagCalibrationReader(BTagEntry::OP_LOOSE, "central", {"up", "down"});
-    mediumReader = BTagCalibrationReader(BTagEntry::OP_MEDIUM, "central", {"up", "down"});  
-    tightReader = BTagCalibrationReader(BTagEntry::OP_TIGHT, "central", {"up", "down"});  
+    for(JetType type: {AK4, AK8}){
+        //Set configuration for bTagSF reader  ##https://twiki.cern.ch/twiki/bin/view/CMS/BTagCalibration
+        calib[type] = BTagCalibration(std::to_string(type), bTagSF[type][era]);
 
-    looseReader.load(calib,  BTagEntry::FLAV_B, "comb");  
-    mediumReader.load(calib,  BTagEntry::FLAV_B, "comb");  
-    tightReader.load(calib,  BTagEntry::FLAV_B, "comb");  
+        looseReader[type] = BTagCalibrationReader(BTagEntry::OP_LOOSE, "central", {"up", "down"});
+        mediumReader[type] = BTagCalibrationReader(BTagEntry::OP_MEDIUM, "central", {"up", "down"});  
+        tightReader[type] = BTagCalibrationReader(BTagEntry::OP_TIGHT, "central", {"up", "down"});  
 
-    //Set configuration for JER tools
-    resolution = JME::JetResolution(JMEPtReso[era]);
-    resolution_sf = JME::JetResolutionScaleFactor(JMESF[era]);
+        looseReader[type].load(calib[type],  BTagEntry::FLAV_B, (type==AK4) ? "comb": "lt");  
+        mediumReader[type].load(calib[type],  BTagEntry::FLAV_B, (type==AK4) ? "comb": "lt");  
+        
+        if(type!=AK8){
+            tightReader[type].load(calib[type],  BTagEntry::FLAV_B, (type==AK4) ? "comb": "lt");  
+        }
     
+        //Set configuration for JER tools
+        resolution[type] = JME::JetResolution(JMEPtReso[type][era]);
+        resolution_sf[type] = JME::JetResolutionScaleFactor(JMESF[type][era]);
+    }
+
     //Set TTreeReader for genpart and trigger obj from baseanalyzer
     SetCollection(reader, this->isData);
 
     //Set Branches of output tree
     tree->Branch("jet", &validJets);
+    tree->Branch("fatjet", &fatJets);
     tree->Branch("met", &met);
     tree->Branch("HT", &HT);
 }
@@ -146,6 +233,7 @@ void JetAnalyzer::BeginJob(TTreeReader &reader, TTree* tree, bool &isData){
 bool JetAnalyzer::Analyze(){
     //Clear jet vector
     validJets.clear();
+    fatJets.clear();
 
     //JER smearing
     float smearFac = 1.;
@@ -156,38 +244,103 @@ bool JetAnalyzer::Analyze(){
     
     //Loop over all jets
     for(unsigned int i = 0; i < jetPt->GetSize(); i++){
-        //Smear pt if not data
-        if(!isData){
-            smearFac = SmearEnergy(jetPt->At(i), jetPhi->At(i), jetEta->At(i), *jetRho->Get());
-
-            //Correct met for JER
-            metPx = metPx - std::cos(jetPhi->At(i))*jetPt->At(i)*(1 + smearFac);
-            metPy = metPy - std::sin(jetPhi->At(i))*jetPt->At(i)*(1 + smearFac);
+        //Clean jets from electron and muon
+        if(eleIdx->At(i) != -1){
+            if(elePt->At(eleIdx->At(i)) > 20. and abs(eleEta->At(eleIdx->At(i))) < 2.4){
+                continue;
+            }
         }
 
-        if(smearFac*jetPt->At(i) > ptCut and abs(jetEta->At(i)) < etaCut){
-            Jet validJet;
+        if(muonIdx->At(i) != -1){
+            if(muonPt->At(muonIdx->At(i)) > 20. and abs(muonEta->At(muonIdx->At(i))) < 2.4){
+                continue;
+            }
+        }
 
-            validJet.fourVec.SetPtEtaPhiM(smearFac*jetPt->At(i), jetEta->At(i), jetPhi->At(i), jetMass->At(i));
+        //Define here already jet, because of smearing of 4-vec
+        Jet jetCand;
+        jetCand.fourVec.SetPtEtaPhiM(jetPt->At(i), jetEta->At(i), jetPhi->At(i), jetMass->At(i));
 
+        //Smear pt if not data
+        if(!isData){
+            smearFac = SmearEnergy(jetPt->At(i), jetPhi->At(i), jetEta->At(i), *jetRho->Get(), 0.4, AK4);
+            jetCand.fourVec *= smearFac;
+
+            //Correct met
+            metPx+= std::cos(jetPhi->At(i))*jetPt->At(i) - std::cos(jetCand.fourVec.Phi())*jetCand.fourVec.Pt();
+            metPy+= std::sin(jetPhi->At(i))*jetPt->At(i) - std::sin(jetCand.fourVec.Phi())*jetCand.fourVec.Pt();
+        }
+
+        if(jetCand.fourVec.Pt() > ptCut and abs(jetCand.fourVec.Eta()) < etaCut){
             //Check for btag
-            validJet.isLooseB = bTagCuts[era][0] < jetDeepBValue->At(i);
-            validJet.isMediumB = bTagCuts[era][1] < jetDeepBValue->At(i);
-            validJet.isTightB = bTagCuts[era][2] < jetDeepBValue->At(i);
+            jetCand.isLooseB = bTagCuts[AK4][era][0] < jetDeepBValue->At(i);
+            jetCand.isMediumB = bTagCuts[AK4][era][1] < jetDeepBValue->At(i);
+            jetCand.isTightB = bTagCuts[AK4][era][2] < jetDeepBValue->At(i);
 
             if(!isData){
                 //btag SF
-                validJet.loosebTagSF = looseReader.eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetEta->At(i)), smearFac*jetPt->At(i));
-                validJet.mediumbTagSF = mediumReader.eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetEta->At(i)), smearFac*jetPt->At(i));
-                validJet.tightbTagSF = tightReader.eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetEta->At(i)), smearFac*jetPt->At(i));
+                jetCand.loosebTagSF = looseReader[AK4].eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetCand.fourVec.Eta()), jetCand.fourVec.Pt());
+                jetCand.mediumbTagSF = mediumReader[AK4].eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetCand.fourVec.Eta()), jetCand.fourVec.Pt());
+                jetCand.tightbTagSF = tightReader[AK4].eval_auto_bounds("central", BTagEntry::FLAV_B, abs(jetCand.fourVec.Eta()), jetCand.fourVec.Pt());
 
 
                 //Save gen particle information
-                SetGenParticles(validJet, i);
+                SetGenParticles(jetCand, i);
             }
 
-            validJets.push_back(validJet);
+            validJets.push_back(jetCand);
         } 
+    }
+
+    //Loop over all fat jets
+    for(unsigned int i = 0; i < fatJetPt->GetSize(); i++){
+        //Define fat jet
+        Jet fatJet;
+        fatJet.fourVec.SetPtEtaPhiM(fatJetPt->At(i), fatJetEta->At(i), fatJetPhi->At(i), fatJetMass->At(i));
+    
+        //Smear pt if not data
+        if(!isData){
+            smearFac = SmearEnergy(fatJetPt->At(i), fatJetPhi->At(i), fatJetEta->At(i), *jetRho->Get(), 0.8, AK8);
+            fatJet.fourVec *= smearFac;
+        }
+
+        //bool for cleaning fatjets from AK4 jets
+        bool isCleaned = true;
+
+        //Check overlap with AK4 valid jets
+        for(Jet jet: validJets){
+            if(fatJet.fourVec.DeltaR(jet.fourVec) < 0.6){
+                isCleaned = false;
+                break;
+            }
+        }        
+
+        //If not cleaned dont use this fatjet
+        if(!isCleaned){
+            continue;
+        }
+
+        if(!isData){
+            //Correct met
+            metPx+= std::cos(fatJetPhi->At(i))*fatJetPt->At(i) - std::cos(fatJet.fourVec.Phi())*fatJet.fourVec.Pt();
+            metPy+= std::sin(fatJetPhi->At(i))*fatJetPt->At(i) - std::sin(fatJet.fourVec.Phi())*fatJet.fourVec.Pt();
+        }
+
+        //Check for btag
+        fatJet.isLooseB = bTagCuts[AK8][era][0] < fatJetCSV->At(i);
+        fatJet.isMediumB = bTagCuts[AK8][era][1] < fatJetCSV->At(i);
+
+        //Nsubjettiness
+        fatJet.twoSubJettiness = fatJetTau2->At(i);
+        fatJet.threeSubJettiness = fatJetTau3->At(i);
+
+        if(!isData){
+            //btag SF
+            fatJet.loosebTagSF = looseReader[AK8].eval_auto_bounds("central", BTagEntry::FLAV_B, abs(fatJet.fourVec.Eta()), fatJet.fourVec.Pt());
+            fatJet.mediumbTagSF = mediumReader[AK8].eval_auto_bounds("central", BTagEntry::FLAV_B, abs(fatJet.fourVec.Eta()), fatJet.fourVec.Pt());
+        }
+
+        fatJets.push_back(fatJet);
     }
 
     //Set HT
@@ -196,11 +349,15 @@ bool JetAnalyzer::Analyze(){
     //Set met
     met.SetPxPyPzE(metPx, metPy, 0 , 0);
 
-    if(validJets.size() < minNJet){
-        return false;
+    //Check if one combination of jet and fatjet number is fullfilled
+
+    for(std::pair<unsigned int, unsigned int> minN: minNJet){
+        if(validJets.size() >= minN.first && fatJets.size() >= minN.second){
+            return true;
+        }
     }
     
-    return true;
+    return false;
 }
 
 
