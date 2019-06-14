a37ab6c7ce1f8ab424ac4c185fe6767392e40217
DaveBrun94 <david.brunner@desy.de>
2019-06-15 00:44:18 +0200

    Added new plotter class

--- analysis/src/classes.h
+++ analysis/src/classes.h
@@ -2,6 +2,7 @@
 #include <ChargedHiggs/analysis/interface/plottertriggeff.h>
 #include <ChargedHiggs/analysis/interface/plotter1D.h>
 #include <ChargedHiggs/analysis/interface/plotter2D.h>
+#include <ChargedHiggs/analysis/interface/plotterCut.h>
 #include <ChargedHiggs/analysis/interface/plotterGen.h>
 #include <ChargedHiggs/analysis/interface/bdt.h>
 
@@ -11,6 +12,7 @@ namespace{
         PlotterTriggEff plotterTriggEff;
         Plotter1D plotter1D;
         Plotter2D plotter2D;
+        PlotterCut plotterCut;
         PlotterGen plotterGen;
         BDT bdt;
     }
