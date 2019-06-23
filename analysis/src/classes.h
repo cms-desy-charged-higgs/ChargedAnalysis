7be6da93871d7372ab833e55b6fd8f73639c2fe6
DaveBrun94 <david.brunner@desy.de>
2019-06-23 16:52:12 +0200

    Added class for plotting limits

--- analysis/src/classes.h
+++ analysis/src/classes.h
@@ -2,9 +2,11 @@
 #include <ChargedHiggs/analysis/interface/plottertriggeff.h>
 #include <ChargedHiggs/analysis/interface/plotter1D.h>
 #include <ChargedHiggs/analysis/interface/plotter2D.h>
+#include <ChargedHiggs/analysis/interface/plotterLimit.h>
 #include <ChargedHiggs/analysis/interface/plotterCut.h>
 #include <ChargedHiggs/analysis/interface/plotterGen.h>
 #include <ChargedHiggs/analysis/interface/bdt.h>
+#include <ChargedHiggs/analysis/interface/limit.h>
 
 namespace{
     namespace{
@@ -12,9 +14,11 @@ namespace{
         PlotterTriggEff plotterTriggEff;
         Plotter1D plotter1D;
         Plotter2D plotter2D;
+        PlotterLimit plotterLimit;
         PlotterCut plotterCut;
         PlotterGen plotterGen;
         BDT bdt;
+        Limit lim;
     }
 }
 
