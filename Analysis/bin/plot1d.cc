37248b3b8f17c061c013c6d577162c25c9af58cb
DaveBrun94 <david.brunner@desy.de>
2019-09-30 11:14:33 +0200

    Created executable to call plot1d class

---
+++ Analysis/bin/plot1d.cc
@@ -0,0 +1,23 @@
+#include <ChargedAnalysis/Analysis/interface/plotter1D.h>
+
+std::vector<std::string> SplitString(std::string splitString){
+    std::istringstream iss(splitString);
+    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
+                                 std::istream_iterator<std::string>());
+
+    return results;
+}
+
+int main(int argc, char *argv[]){
+    //Extract informations of command line
+    std::string histDir = std::string(argv[1]);
+    std::vector<std::string> xParameters = SplitString(std::string(argv[2]));
+    std::string channel = std::string(argv[3]);
+    std::vector<std::string> processes = SplitString(std::string(argv[4]));
+    std::vector<std::string> outDirs = SplitString(std::string(argv[5]));
+
+    //Call and run Plotter1D class
+    Plotter1D plotter(histDir, xParameters, channel);
+    plotter.ConfigureHists(processes);
+    plotter.Draw(outDirs);
+}
--- Analysis/python/plot1d.py
+++ Analysis/python/plot1d.py
@@ -2,40 +2,44 @@ from ChargedAnalysis.Workflow.task import Task
 
 import os
 
-from ROOT import Plotter1D, string, vector
-
 class Plot1D(Task):
     def __init__(self, config = {}):
         Task.__init__(self, config)
-
-    def __toStd(self):
-        self._stdDir = {}
-
-        ##Translate string and list to std::string and std::vector
-        for key, value in self.iteritems():
-            if type(value) == str:
-                self._stdDir[key] = string(value)
-
-            elif type(value) == list:
-                self._stdDir[key] = vector("string")()
-                
-                for v in value:
-                   self._stdDir[key].push_back(v) 
-
-            else:
-                pass
 
     def status(self):
         if not False in [os.path.isfile(output) for output in self["output"]]:
             self["status"] = "FINISHED"
 
     def run(self):
-        self.__toStd()
-
-        plotter = Plotter1D(self._stdDir["hist-dir"], self._stdDir["x-parameter"], self._stdDir["channel"])
-        plotter.ConfigureHists(self._stdDir["processes"])
-        plotter.Draw(vector("string")(1, self._stdDir["dir"]))
-
+        ##Run the Plotter1D executable
+        os.system("Plot1D {} '{}' {} '{}' '{}'".format(
+                self["hist-dir"], 
+                " ".join(self["x-parameter"]),  
+                self["channel"],    
+                " ".join(self["processes"]), 
+                " ".join([self["dir"]]))
+        )
 
     def output(self):
         self["output"] = ["{}/{}.pdf".format(self["dir"], x) for x in self["x-parameter"]]
+
+    @staticmethod
+    def configure(treeTasks, conf):
+        chanToDir = {"mu4j": "Muon4J", "e4j": "Ele4J", "mu2j1f": "Muon2J1F", "e2j1f": "Ele2J1F", "mu2f": "Muon2F", "e2f": "Ele2F"}
+        
+        tasks = []
+
+        for channel in conf.keys():
+            plotConf = {"name": "Plot_{}".format(channel), 
+                        "channel": channel, 
+                        "hist-dir": os.environ["CHDIR"] + "Hist/{}/{}".format(conf[channel]["dir"], chanToDir[channel]), 
+                        "dir":  os.environ["CHDIR"] + "/CernWebpage/Plots/{}/{}".format(conf[channel]["dir"], chanToDir[channel]), 
+                        "display-name": "Plots: {}".format(channel), 
+                        "x-parameter": conf[channel]["x-parameter"], 
+                        "dependencies": [t["name"] for t in treeTasks if t["channel"] == channel], 
+                        "processes": [t["process"] for t in treeTasks if t["channel"] == channel]
+            }
+
+            tasks.append(Plot1D(plotConf))
+
+        return tasks
