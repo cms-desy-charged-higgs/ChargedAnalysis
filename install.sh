8db5f31b7d83d2abf8dbcb284e545e98fe15f44a
DaveBrun94 <david.brunner@desy.de>
2019-10-01 14:50:42 +0200

    Changed due to splitting of analysis code to standalone

--- install.sh
+++ install.sh
@@ -5,16 +5,34 @@ mkdir -p ChargedHiggs/
 export CHDIR=$(pwd)/ChargedHiggs
 cd $CHDIR
 
-##Install anaconda for python2.7 (because of CMSSW)
-wget https://repo.anaconda.com/archive/Anaconda2-2019.03-Linux-x86_64.sh
-chmod a+rx Anaconda2-2019.03-Linux-x86_64.sh
-./Anaconda2-2019.03-Linux-x86_64.sh  ##Set anaconda in $CHDIR/Anaconda
-command rm Anaconda2-2019.03-Linux-x86_64.sh
+##Install Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+chmod +x Anaconda3-2019.07-Linux-x86_64.sh
+./Anaconda3-2019.07-Linux-x86_64.sh
+command rm Anaconda3-2019.07-Linux-x86_64.sh
 
-##Install packages with anaconda
-source $CHDIR/Anaconda/bin/activate
-conda install jupyter numpy tensorflow-gpu scipy scikit-learn pandas
+##Install ROOT, python packages and g++ compiler with conda
+export PYTHONPATH=$CHDIR/Anaconda3/lib/python3.7/site-packages/
+source $CHDIR/Anaconda3/bin/activate
+
+conda install -c conda-forge root -y
+conda install jupyter numpy tensorflow-gpu scipy scikit-learn pandas -y
 pip install htcondor
+
+conda install -c conda-forge compilers -y
+conda install -c anaconda git -y
+
+##Git standalone analysis code and compile everything
+git clone https://github.com/cms-desy-charged-higgs/ChargedAnalysis.git
+git clone https://github.com/cms-desy-charged-higgs/ChargedNetwork.git
+
+cd ChargedAnalysis
+make -j 20
+cd ..
+
+##Add webpage for cern
+git clone https://github.com/DaveBrun94/CernWebpage.git
+
 conda deactivate
 
 ##Install CMSSW 
@@ -22,28 +40,18 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 export SCRAM_ARCH=slc6_amd64_gcc700
 
-##Add webpage for cern
-git clone https://github.com/DaveBrun94/CernWebpage.git
-
 eval `scramv1 project CMSSW CMSSW_9_4_13`
 cd CMSSW_9_4_13/src/
 eval `scramv1 runtime -sh`
 git cms-init
 
 ##Clone charged higgs repos
-git clone https://github.com/cms-desy-charged-higgs/ChargedAnalysis.git
+git clone https://github.com/cms-desy-charged-higgs/ChargedSkimming.git
 git clone https://github.com/cms-desy-charged-higgs/ChargedProduction.git
-git clone https://github.com/cms-desy-charged-higgs/ChargedNetwork.git
 
 ##Other stuff neeeded
 git cms-merge-topic cms-egamma:EgammaPostRecoTools
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
-
 
 ##Compile everthing
 scram b -j 20
-
-##Set PYTHONPATH
-PYTHONPATH=$CHDIR/Anaconda/lib/python2.7/site-packages/:$PYTHONPATH
-
