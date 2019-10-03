75887a9a7127d256bcf872880e160e29a58a33ce
DaveBrun94 <david.brunner@desy.de>
2019-10-03 12:09:48 +0200

    Rewritten due to decoupled Analysis code from CMSSW

--- setenv.sh
+++ setenv.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-export CMSSW_VERSION=CMSSW_9_4_13
+export PATH=$(getconf PATH)
+export LD_LIBRARY_PATH=""
 
 ##Set analysis dir
 CHDIR=$(readlink -f $BASH_SOURCE)
-export CHDIR=${CHDIR/"$CMSSW_VERSION/src/ChargedAnalysis/setenv.sh"}
+export CHDIR=${CHDIR/"/ChargedAnalysis/setenv.sh"}
 
-##CMSSW related enviroment
-export SCRAM_ARCH="slc6_amd64_gcc700"
+##Set actual CMSSW version
+export CMSSW_VERSION=CMSSW_9_4_13
 
-cd $CHDIR/$CMSSW_VERSION/src
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsenv
-source /cvmfs/cms.cern.ch/crab3/crab.sh
+case $1 in
+    "CMSSW")
+         ##CMSSW related enviroment
+        export SCRAM_ARCH="slc6_amd64_gcc700"
 
-alias scramb="scram b -j 20"
+        cd $CHDIR/$CMSSW_VERSION/src
+        source /cvmfs/cms.cern.ch/cmsset_default.sh
+        eval `scramv1 runtime -sh`
+        source /cvmfs/cms.cern.ch/crab3/crab.sh
+
+        alias scramb="scram b -j 20"
+
+        cd $CHDIR
+        ;;
+            
+    "StandAlone")
+        export PYTHONPATH=$CHDIR/Anaconda3/lib/python3.7/site-packages/:$CHDIR/ChargedAnalysis/Analysis/python:$CHDIR/ChargedAnalysis/Workflow/python:$CHDIR/ChargedNetwork/python
+        export PATH=$CHDIR/Anaconda3/bin:$CHDIR/ChargedAnalysis/Analysis/bin:$CHDIR/ChargedAnalysis/Workflow/bin:$CHDIR/ChargedNetwork/bin:$PATH
+        export LD_LIBRARY_PATH=$CHDIR/Anaconda3/lib:$CHDIR/ChargedAnalysis/Analysis/lib
+        ;;
+
+    *)    
+        echo "Invalid option: $1"
+        echo "Please use this options: {CMSSW, StandAlone}"
+        return 1
+        ;;
+esac
 
 ##VO related stuff
 export X509_USER_PROXY=$HOME/.globus/x509
@@ -26,6 +48,3 @@ alias voms="voms-proxy-init --voms cms --valid 168:00"
 ##CERN stuff
 export CERN_USER="dbrunner"
 alias www="rsync -arvt --exclude '.*' --delete -e ssh $CHDIR/CernWebpage/ $CERN_USER@lxplus.cern.ch:/eos/home-${USER:0:1}/$CERN_USER/www/"
-
-##Put Anaconda into PYTHONPATH
-PYTHONPATH=$CHDIR/Anaconda/lib/python2.7/site-packages/:$PYTHONPATH
