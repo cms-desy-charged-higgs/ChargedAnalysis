76a859e848e8563a96d17574b25268e9c46af997
DaveBrun94 <david.brunner@desy.de>
2019-10-04 12:44:03 +0200

    Moved string splitting into utility namespace

--- Analysis/bin/plot1d.cc
+++ Analysis/bin/plot1d.cc
@@ -1,20 +1,13 @@
+#include <ChargedAnalysis/Analysis/include/utils.h>
 #include <ChargedAnalysis/Analysis/include/plotter1D.h>
-
-std::vector<std::string> SplitString(std::string splitString){
-    std::istringstream iss(splitString);
-    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
-                                 std::istream_iterator<std::string>());
-
-    return results;
-}
 
 int main(int argc, char *argv[]){
     //Extract informations of command line
     std::string histDir = std::string(argv[1]);
-    std::vector<std::string> xParameters = SplitString(std::string(argv[2]));
+    std::vector<std::string> xParameters = Utils::SplitString(std::string(argv[2]));
     std::string channel = std::string(argv[3]);
-    std::vector<std::string> processes = SplitString(std::string(argv[4]));
-    std::vector<std::string> outDirs = SplitString(std::string(argv[5]));
+    std::vector<std::string> processes = Utils::SplitString(std::string(argv[4]));
+    std::vector<std::string> outDirs = Utils::SplitString(std::string(argv[5]));
 
     //Call and run Plotter1D class
     Plotter1D plotter(histDir, xParameters, channel);
