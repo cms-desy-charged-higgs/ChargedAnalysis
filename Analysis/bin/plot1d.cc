9ce7242b0b87045311012d83d8260449646587b6
DaveBrun94 <david.brunner@desy.de>
2019-10-15 11:22:55 +0200

    Modified call of SplitString function

--- Analysis/bin/plot1d.cc
+++ Analysis/bin/plot1d.cc
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]){
     //Extract informations of command line
     std::string histDir = std::string(argv[1]);
-    std::vector<std::string> xParameters = Utils::SplitString(std::string(argv[2]));
+    std::vector<std::string> xParameters = Utils::SplitString(std::string(argv[2]), " ");
     std::string channel = std::string(argv[3]);
-    std::vector<std::string> processes = Utils::SplitString(std::string(argv[4]));
-    std::vector<std::string> outDirs = Utils::SplitString(std::string(argv[5]));
+    std::vector<std::string> processes = Utils::SplitString(std::string(argv[4]), " ");
+    std::vector<std::string> outDirs = Utils::SplitString(std::string(argv[5]), " ");
 
     //Call and run Plotter1D class
     Plotter1D plotter(histDir, xParameters, channel);
