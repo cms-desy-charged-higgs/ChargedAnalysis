fcbfb613e96efbb36792e1d036dcd993ad16f6c6
DaveBrun94 <david.brunner@desy.de>
2019-11-04 01:06:28 +0100

    Some changes

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$(getconf PATH)
-export LD_LIBRARY_PATH=""
+export PATH="$(getconf PATH):/cvmfs/grid.cern.ch/emi-ui-3.17.1-1.el6umd4v5/usr/bin/"
+export LD_LIBRARY_PATH="/cvmfs/grid.cern.ch/emi-ui-3.17.1-1.el6umd4v5/usr/lib64/"
 
 ##Set analysis dir
 CHDIR=$(readlink -f $BASH_SOURCE)
@@ -18,7 +18,7 @@ case $1 in
         cd $CHDIR/$CMSSW_VERSION/src
         source /cvmfs/cms.cern.ch/cmsset_default.sh
         eval `scramv1 runtime -sh`
-        source /cvmfs/cms.cern.ch/crab3/crab.sh
+        source /cvmfs/cms.cern.ch/crab3/crab_slc6.sh
 
         alias scramb="scram b -j 20"
 
@@ -28,7 +28,7 @@ case $1 in
     "StandAlone")
         export PYTHONPATH=$CHDIR/Anaconda3/lib/python3.7/site-packages/:$CHDIR/ChargedAnalysis/Analysis/python:$CHDIR/ChargedAnalysis/Workflow/python:$CHDIR/ChargedNetwork/python
         export PATH=$CHDIR/Anaconda3/bin:$CHDIR/ChargedAnalysis/Analysis/bin:$CHDIR/ChargedAnalysis/Workflow/bin:$CHDIR/ChargedAnalysis/Network/bin:$PATH
-        export LD_LIBRARY_PATH=$CHDIR/Anaconda3/lib:$CHDIR/ChargedAnalysis/Analysis/lib:$CHDIR/ChargedAnalysis/Network/lib:$CHDIR/Anaconda3/lib/python3.7/site-packages/torch/lib
+        export LD_LIBRARY_PATH=$CHDIR/Anaconda3/lib:$CHDIR/ChargedAnalysis/Analysis/lib:$CHDIR/ChargedAnalysis/Network/lib:$CHDIR/Anaconda3/lib/python3.7/site-packages/torch/lib:$LD_LIBRARY_PATH
 
         alias make="make -j 20"
 
@@ -46,7 +46,7 @@ export X509_USER_PROXY=$HOME/.globus/x509
 export REDIRECTOR=root://cms-xrd-global.cern.ch/
 export SRM_DESY=srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/
 
-alias voms="voms-proxy-init --voms cms --valid 168:00"
+alias voms="voms-proxy-init --voms cms:/cms/dcms --valid 168:00"
 
 ##CERN stuff
 export CERN_USER="dbrunner"
