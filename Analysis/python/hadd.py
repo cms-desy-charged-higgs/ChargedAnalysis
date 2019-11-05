75f1932635a4d43ba0bc5aa057d9d360c445afd0
DaveBrun94 <david.brunner@desy.de>
2019-11-05 11:21:31 +0100

    Added new Hadd Task Class for append Higgs score

--- Analysis/python/hadd.py
+++ Analysis/python/hadd.py
@@ -1,18 +1,21 @@
 from task import Task
 
 import os
-from pprint import pprint
 
-class Hadd(Task):
+class HaddPlot(Task):
     def __init__(self, config = {}):
         super().__init__(config)
 
     def run(self):
         self["executable"] = "hadd"
-        self["arguments"] = " -f -v 0 {} {}".format(self["output"], " ".join(self["dependent-files"]))
+        self["arguments"] =  [
+                                "-f",
+                                "-v", 
+                                "0",
+                                self["output"],   
+        ] + self["dependent-files"]
 
-        if self["run-mode"] == "Local":
-            os.system("{} {}".format(self["executable"], self["arguments"]))
+        super()._run()
 
     def output(self):
         self["output"] = "{}/{}.root".format(self["dir"], self["process"])
@@ -25,16 +28,53 @@ class Hadd(Task):
 
         for process in conf[channel]["processes"]:
             outDir = os.environ["CHDIR"] + "/Hist/{}/{}".format(conf[channel]["dir"], chanToDir[channel])
+            histDir = os.environ["CHDIR"] + "/Tmp/Hist/{}/{}".format(conf[channel]["dir"], chanToDir[channel])
                 
             haddConf = {"name": "Hadd_{}_{}".format(process, channel),  
                         "dir": outDir,
                         "process": process,
                         "channel": channel,
                         "display-name": "Hadd: {} ({})".format(process, channel),
-                        "dependencies": [t["name"] for t in treeTasks if t["dir"] == outDir and t["process"] == process]
+                        "dependencies": [t["name"] for t in treeTasks if t["dir"] == histDir and t["process"] == process]
             }
 
-            tasks.append(Hadd(haddConf))
+            tasks.append(HaddPlot(haddConf))
+        return tasks
+
+
+class HaddAppend(Task):
+    def __init__(self, config = {}):
+        super().__init__(config)
+
+    def run(self):
+        self["executable"] = "hadd"
+        self["arguments"] =  [
+                                "-f",
+                                "-v", 
+                                "0",
+                                self["output"],   
+        ] + self["dependent-files"]
+
+        super()._run()
+
+    def output(self):
+        self["output"] = "{}/{}.root".format(self["dir"], self["out-file"])
+
+    @staticmethod
+    def configure(otherTasks):
+
+        tasks = []
+        fileNames = list(set([task["input-file"] for task in otherTasks]))
+
+        for fileName in fileNames:
+            haddConf = {"name": "Hadd_{}".format(fileName.split("/")[-1][:-5]), 
+                        "out-file": fileName.split("/")[-1][:-5], 
+                        "dir": "/".join(fileName.split("/")[:-1]),
+                        "display-name": "Hadd: {}".format(fileName.split("/")[-1][:-5]),
+                        "dependencies": [task["name"] for task in otherTasks if task["input-file"] == fileName]
+            }
+
+            tasks.append(HaddAppend(haddConf))
         return tasks
 
         
