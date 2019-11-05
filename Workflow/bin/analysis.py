54df218a85d256e445addd45dc1f03dcad5d0621
DaveBrun94 <david.brunner@desy.de>
2019-11-05 11:27:11 +0100

    Added functionality to append Higgs score

--- Workflow/bin/analysis.py
+++ Workflow/bin/analysis.py
@@ -4,7 +4,9 @@ from taskmanager import TaskManager
 
 from treeread import TreeRead
 from plot1d import Plot1D
-from hadd import Hadd
+from hadd import HaddPlot, HaddAppend
+from treeappend import TreeAppend
+from fileskim import FileSkim
 
 import os
 import argparse
@@ -12,7 +14,7 @@ import yaml
 
 def parser():
     parser = argparse.ArgumentParser(description = "Script to handle and execute analysis tasks", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--task", type=str, choices = ["Plot1D"])
+    parser.add_argument("--task", type=str, choices = ["Plot1D", "Append"])
     parser.add_argument("--config", type=str)
 
     return parser.parse_args()
@@ -22,17 +24,31 @@ def taskReturner(taskName, **kwargs):
         config =  yaml.load(open("{}/ChargedAnalysis/Workflow/config/{}.yaml".format(os.environ["CHDIR"], kwargs["config"]), "r"), Loader=yaml.Loader)
 
     tasks = {
+        "Append": lambda : append(config),
         "Plot1D": lambda : plot1DTask(config),
     }
 
     return tasks[taskName]
+
+def append(config):
+    allTasks = []
+
+    for channel, conf in config.items():
+        appendTasks = TreeAppend.configure(config, channel)
+
+        allTasks.extend(appendTasks)
+
+    allTasks.extend(FileSkim.configure(allTasks, list(config.keys())))
+    allTasks.extend(HaddAppend.configure(allTasks))
+
+    return allTasks
 
 def plot1DTask(config):
     allTasks = []
 
     for channel, conf in config.items():
         treeTasks = TreeRead.configure(config, channel)
-        haddTasks = Hadd.configure(config, treeTasks, channel)
+        haddTasks = HaddPlot.configure(config, treeTasks, channel)
         histTasks = Plot1D.configure(config, haddTasks, channel)
 
         allTasks.extend(treeTasks+haddTasks+histTasks)
@@ -52,4 +68,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
