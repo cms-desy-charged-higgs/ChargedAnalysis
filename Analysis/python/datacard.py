535623cb67229094e77035ca799f4de2a34e547c
DaveBrun94 <david.brunner@desy.de>
2019-11-27 11:09:40 +0100

    Reworked task to write datacard

--- Analysis/python/datacard.py
+++ Analysis/python/datacard.py
@@ -2,39 +2,49 @@ from task import Task
 
 import os
 
-from ROOT import Limit, vector, string
-
-class LimitTask(Task):
+class Datacard(Task):
     def __init__(self, config = {}):
-        Task.__init__(self, config)
-
-    def __toStd(self):
-        self._stdDir = {}
-
-        ##Translate string and list to std::string and std::vector
-        for key, value in self.iteritems():
-            if type(value) == str:
-                self._stdDir[key] = string(value)
-
-            elif type(value) == list:
-                self._stdDir[key] = vector("string")()
-                
-                for v in value:
-                   self._stdDir[key].push_back(v) 
-
-            else:
-                pass
+        super().__init__(config)
 
     def run(self):
-        self.__toStd()
+        self["executable"] = "WriteCard"
 
-        limit = Limit(self._stdDir["mass"], self._stdDir["channel"], self._stdDir["background"], self._stdDir["dir"])
-        limit.WriteDatacard(self._stdDir["hist-dir"], self._stdDir["x-parameter"])
-        limit.CalcLimit()
+        self["arguments"] = [
+                "{}".format(" ".join(self["backgrounds"])),
+                self["signal"],
+                self["data"],
+                self["channel"],
+                self["dir"],
+                "true", #True in ["Single" in proc for proc in self["processes"]],
+                self["hist-dir"], 
+                self["discriminant"], 
+        ]
+
+        super()._run()
 
     def output(self):
-        self["output"] = self["dir"] + "/limit.root"
+        self["output"] = [self["dir"] + "/datacard.txt", self["dir"] + "/shapes.root"]
 
-    def status(self):
-        if os.path.isfile(self["output"]):
-            self["status"] = "FINISHED"
+    @staticmethod
+    def configure(config, channel, mass, haddTasks):
+        chanToDir = {"mu4j": "Muon4J", "e4j": "Ele4J", "mu2j1f": "Muon2J1F", "e2j1f": "Ele2J1F", "mu2f": "Muon2F", "e2f": "Ele2F"}
+        
+        tasks = []
+
+        cardConf = {"name": "Datacard_{}_{}".format(mass, channel), 
+                    "dir":  "{}/{}/{}".format(os.environ["CHDIR"], config[channel]["dir"], chanToDir[channel]), 
+                    "display-name": "Datacard: {} ({})".format(mass, channel), 
+                    "discriminant": config[channel]["x-parameter"][0],
+                    "dependencies": [t["name"] for t in haddTasks if str(mass) in t["dir"] and t["channel"] == channel], 
+                    "backgrounds": config[channel]["backgrounds"],
+                    "signal": config[channel]["signal"],
+                    "data": config[channel]["data"],
+                    "hist-dir": "{}/Hist/{}".format(os.environ["CHDIR"], config[channel]["dir"]),
+                    "channel": channel,
+        }
+
+
+        tasks.append(Datacard(cardConf))
+
+        return tasks
+        
