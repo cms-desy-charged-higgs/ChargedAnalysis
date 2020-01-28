fa91ffbd35b8a5ae73c7286e3e1d347485e484c4
DaveBrun94 <david.brunner@desy.de>
2020-01-28 17:08:54 +0100

    Added options for hyperspace optimization

--- Network/python/bdt.py
+++ Network/python/bdt.py
@@ -10,43 +10,47 @@ class BDT(Task):
         self["executable"] = "BDT"
 
         self["arguments"] = [
+                "--sep-type", self["sep-type"],
+                "--x-parameters", *self["x-parameters"],
+                "--tree-dir", self["tree-dir"],
+                "--result-dir", self["dir"],
+                "--signal", self["signal"],
+                "--backgrounds", *self["backgrounds"],
+                "--masses", *self["masses"],
+                "--optimize" if self["optimize"] != -1 else "",
+        ]
+
+        if self["optimize"] == -1:
+            self["arguments"] += [
                 "--n-trees", self["n-trees"],
                 "--min-node-size", self["min-node-size"],
                 "--lr", self["lr"], 
                 "--n-cuts", self["n-cuts"],
                 "--tree-depth", self["tree-depth"],
                 "--drop-out", self["drop-out"],
-                "--sep-type", self["sep-type"],
-    
-                "--x-parameters", self["x-parameters"],
-                "--tree-dir", *self["tree-dir"],
-                "--result-dir", self["dir"],
-                "--signal", self["signal"],
-                "--backgrounds", *self["backgrounds"],
-                "--masses", *self["masses"],
-                "--event-type", self["event-type"],
-        ]
-
-        return super()._run()
+            ]
 
     def output(self):
         self["output"] = self["dir"] + "/BDT.root"
 
     @staticmethod
-    def configure(config, channel, evType):
+    def configure(config, channel, haddTasks, evType, prefix=""):
         task = {
-                "name": "BDT_{}_{}".format(channel, evType)
-                "dir": "{}/{}".format(os.environ["CHDIR"], config["dir"])
-                "signal": config["signal"]
-                "backgrounds": config["backgrounds"]
+                "name": "BDT_{}_{}".format(channel, evType) + ("_{}".format(prefix) if prefix else ""),
+                "dir": "{}/{}/{}".format(os.environ["CHDIR"], config["dir"].format(evType), config["chan-dir"][channel]),
+                "signal": config["signal"],
+                "backgrounds": config["backgrounds"],
                 "x-parameters": config["x-parameters"].get("all", []) + config["x-parameters"].get(channel, []),
-                "tree-dir": "{}/Tree/{}/{}".format(os.environ["CHDIR"], config["dir"], config["channel-dirs"][channel]), 
+                "tree-dir": "{}/Tree/{}/{}".format(os.environ["CHDIR"], config["dir"].format(evType), config["chan-dir"][channel]), 
                 "masses": config["masses"],
-                "event-type": evType,       
+                "dependencies": [t["name"] for t in haddTasks if evType in t["dir"] and config["chan-dir"][channel] in t["dir"]],
+                "run-mode": config["run-mode"],
+                "optimize": config.get("optimize", -1),
+                "sep-type": config["sep-type"],
             }
 
         task.update(config["hyper-parameter"])
-
-    return [BDT(task)]
+    
+        return [BDT(task)]
                     
         
