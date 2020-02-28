69f8d79ee8ccf47eabb217bddcad3e96f82ef055
DaveBrun94 <david.brunner@desy.de>
2020-02-28 19:30:25 +0100

    Changed function call of plotter1D and different parser arguments

--- Analysis/bin/plot.cc
+++ Analysis/bin/plot.cc
@@ -10,20 +10,12 @@ int main(int argc, char *argv[]){
     Parser parser(argc, argv);
 
     std::string histDir =  parser.GetValue<std::string>("hist-dir");
-    std::vector<std::string> xParameters = parser.GetVector<std::string>("x-parameters");
-    std::vector<std::string> yParameters = parser.GetVector<std::string>("y-parameters");
     std::string channel = parser.GetValue<std::string>("channel");
     std::vector<std::string> processes = parser.GetVector<std::string>("processes");
     std::vector<std::string> outDirs = parser.GetVector<std::string>("out-dirs");
 
     //Call and run Plotter1D class
-    Plotter1D plotter1D(histDir, xParameters, channel, processes);
+    Plotter1D plotter1D(histDir, channel, processes);
     plotter1D.ConfigureHists();
     plotter1D.Draw(outDirs);
-    
-    if(!yParameters.empty()){
-        Plotter2D plotter2D(histDir, xParameters, yParameters, channel, processes);
-        plotter2D.ConfigureHists();
-        plotter2D.Draw(outDirs);
-    }
 }
