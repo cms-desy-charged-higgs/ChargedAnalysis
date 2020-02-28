83ffc46de9e55fd3be0ef0b84fa8b5add29cf86a
DaveBrun94 <david.brunner@desy.de>
2020-02-28 19:30:06 +0100

    Changed function call of treereader and different parser arguments

--- Analysis/bin/treeread.cc
+++ Analysis/bin/treeread.cc
@@ -5,17 +5,15 @@ int main(int argc, char* argv[]){
     //Parser arguments
     Parser parser(argc, argv);
 
-    std::string process = parser.GetValue<std::string>("process");
-    std::vector<std::string> xParameters = parser.GetVector<std::string>("x-parameters");
-    std::vector<std::string> yParameters = parser.GetVector<std::string>("y-parameters");
+    std::vector<std::string> parameters = parser.GetVector<std::string>("parameters");
     std::vector<std::string> cuts = parser.GetVector<std::string>("cuts");
     std::string outName = parser.GetValue<std::string>("out-name");
     std::string channel = parser.GetValue<std::string>("channel");
-    std::string saveMode = parser.GetValue<std::string>("save-mode");
     std::string fileName = parser.GetValue<std::string>("filename");
+    std::string cleanJet = parser.GetValue<std::string>("clean-jet");
     std::vector<int> eventYield = parser.GetVector<int>("event-yield");
 
     //Create treereader instance
-    TreeReader reader(process, xParameters, yParameters, cuts, outName, channel, saveMode);
-    reader.EventLoop(fileName, eventYield[0], eventYield[1]);
+    TreeReader reader(parameters, cuts, outName, channel);
+    reader.EventLoop(fileName, eventYield[0], eventYield[1], cleanJet);
 }
