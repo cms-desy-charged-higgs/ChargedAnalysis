0a923fe252bfc087f5302f57d1c2719cb7157be4
DaveBrun94 <david.brunner@desy.de>
2020-04-03 09:43:37 +0200

    Use fast tree clone option

--- Analysis/bin/fileskim.cc
+++ Analysis/bin/fileskim.cc
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]){
 
         if(obj->InheritsFrom(TTree::Class())){
             TTree* oldTree = (TTree*)obj;
-            TTree* newTree = oldTree->CloneTree();
+            TTree* newTree = oldTree->CloneTree(-1, "fast");
 
             newTree->Write();
             
