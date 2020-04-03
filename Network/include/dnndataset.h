8d14d23376d8bb96c00577861652cba9a99cb35b
DaveBrun94 <david.brunner@desy.de>
2020-04-03 10:13:49 +0200

    Pytorch dataset class for mass parametrized DNN

---
+++ Network/include/dnndataset.h
@@ -0,0 +1,41 @@
+#ifndef DNNDATASET_H
+#define DNNDATASET_H
+
+#include <torch/torch.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <ChargedAnalysis/Utility/include/utils.h>
+
+struct DNNTensor{
+    torch::Tensor input;
+    torch::Tensor label;
+};
+
+class DNNDataset : public torch::data::datasets::Dataset<DNNDataset, DNNTensor>{
+    private:
+        int mass = -999.;
+
+        std::vector<std::ifstream*> files;
+        int fileIndex;
+        int nLines = 0;
+
+        torch::Device device;
+        bool isSignal;
+
+        float eventNumber; 
+
+    public:
+        DNNDataset(const std::vector<std::string>& files, torch::Device& device, const bool& isSignal);
+        ~DNNDataset(){}
+
+        void SetMass(const int& mass);
+        torch::optional<size_t> size() const;
+        DNNTensor get(size_t index);
+        static DNNTensor Merge(std::vector<DNNTensor>& tensors);
+        void clear();
+};
+
+#endif
---
+++ Network/src/dnndataset.cc
@@ -0,0 +1,65 @@
+#include <ChargedAnalysis/Network/include/dnndataset.h>
+
+DNNDataset::DNNDataset(const std::vector<std::string>& files, torch::Device& device, const bool& isSignal) :
+    device(device),
+    isSignal(isSignal){
+
+    for(const std::string& file: files){
+        std::ifstream myfile(file);
+
+        myfile.unsetf(std::ios_base::skipws);
+
+        nLines += std::count(
+            std::istreambuf_iterator<char>(myfile),
+            std::istreambuf_iterator<char>(), 
+            '\n'); 
+
+        myfile.close();
+
+        std::string header;
+        std::ifstream* myFile = new std::ifstream(file, std::ios::in);
+        std::getline(*myFile, header);
+
+        this->files.push_back(myFile);
+    }
+
+    fileIndex = 0;
+}
+
+torch::optional<size_t> DNNDataset::size() const {
+    return nLines;
+}
+
+void DNNDataset::SetMass(const int& mass){this->mass = mass;}
+        
+DNNTensor DNNDataset::get(size_t index){
+    std::string line;
+
+    if(!std::getline(*files[fileIndex], line)){
+        files[fileIndex]->clear();
+        files[fileIndex]->seekg(0, std::ios::beg);
+        fileIndex;
+
+        if(fileIndex == files.size()-1) fileIndex = 0;
+
+        std::getline(*files[fileIndex], line);
+        std::getline(*files[fileIndex], line);
+    }
+
+    std::vector<float> paramValues = Utils::SplitString<float>(line, "\t");
+    paramValues.push_back(mass);
+
+    return {torch::from_blob(paramValues.data(), {1, paramValues.size()}).clone().to(device), torch::tensor({float(isSignal)}).to(device)};
+}
+
+DNNTensor DNNDataset::Merge(std::vector<DNNTensor>& tensors){
+    int charMax = 0; int neutralMax = 0; int SVMax = 0;
+    std::vector<torch::Tensor> input, label; 
+
+    for(DNNTensor& tensor: tensors){
+        input.push_back(tensor.input);
+        label.push_back(tensor.label);
+    }
+
+    return {torch::cat(input, 0), torch::cat(label, 0)};
+}
