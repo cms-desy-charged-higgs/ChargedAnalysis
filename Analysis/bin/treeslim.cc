ebb95a29fed4bc41c6ec483cad08d4af80509eb3
DaveBrun94 <david.brunner@desy.de>
2020-05-07 11:13:46 +0200

    Executable for tree slimming

---
+++ Analysis/bin/treeslim.cc
@@ -0,0 +1,16 @@
+#include <ChargedAnalysis/Utility/include/parser.h>
+#include <ChargedAnalysis/Analysis/include/treeslimmer.h>
+
+int main(int argc, char* argv[]){
+    //Parser arguments
+    Parser parser(argc, argv);
+
+    std::string inName = parser.GetValue<std::string>("input-name");
+    std::string inChannel = parser.GetValue<std::string>("input-channel");
+    std::string outName = parser.GetValue<std::string>("out-name");
+    std::string outChannel = parser.GetValue<std::string>("out-channel");
+    std::vector<std::string> cuts = parser.GetVector<std::string>("cuts");
+
+    TreeSlimmer slimmer(inName, inChannel);    
+    slimmer.DoSlim(outName, outChannel, cuts);
+}
