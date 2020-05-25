366f00d5cadb3174c23e5a5abe397883de28a919
DaveBrun94 <david.brunner@desy.de>
2020-05-25 15:20:54 +0200

    Change LD PATH due to usage of rlink durning compiling

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export PATH="$(getconf PATH):/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/bin:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/sbin:/opt/puppetlabs/bin"
-export LD_LIBRARY_PATH="/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/lib64:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/lib:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/lib64:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/lib:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/lib64/dcap"
-
-export PYTHONPATH="/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/lib64/python2.7/site-packages:/cvmfs/grid.cern.ch/centos7-ui-4.0.3-1_umd4v4/usr/lib/python2.7/site-packages"
+export PATH="$(getconf PATH)"
+export LD_LIBRARY_PATH=""
+export PYTHONPATH=""
 
 ##Set analysis dir
 CHDIR=$(readlink -f $BASH_SOURCE)
 export CHDIR=${CHDIR/"/ChargedAnalysis/setenv.sh"}
+
+source /cvmfs/grid.desy.de/etc/profile.d/grid-ui-env.sh
 
 ##Set actual CMSSW version https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmVAnalysisSummaryTable
 export CMSSW_VERSION=CMSSW_10_2_18
@@ -20,7 +21,7 @@ case $1 in
         cd $CHDIR/$CMSSW_VERSION/src
         source /cvmfs/cms.cern.ch/cmsset_default.sh
         eval `scramv1 runtime -sh`
-        source /cvmfs/cms.cern.ch/crab3/crab.sh
+        source /cvmfs/cms.cern.ch/crab3/crab.sh ""
 
         alias scramb="scram b -j 20"
 
@@ -29,8 +30,8 @@ case $1 in
             
     "Analysis")
         export PYTHONPATH=$CHDIR/Anaconda3/lib/python3.7/site-packages/:$CHDIR/ChargedAnalysis/Analysis/python:$CHDIR/ChargedAnalysis/Workflow/python:$CHDIR/ChargedAnalysis/Utility/python:$CHDIR/ChargedAnalysis/Network/python:$CHDIR/HiggsAnalysis/CombinedLimit/lib/python:$PYTHONPATH
-        export PATH=$CHDIR/Anaconda3/bin:$CHDIR/ChargedAnalysis/Analysis/bin:$CHDIR/ChargedAnalysis/Workflow/bin:$CHDIR/ChargedAnalysis/Network/bin:$CHDIR/ChargedAnalysis/Utility/bin:$CHDIR/HiggsAnalysis/CombinedLimit/exe:$CHDIR/HiggsAnalysis/CombinedLimit/scripts:$PATH
-        export LD_LIBRARY_PATH=$CHDIR/Anaconda3/lib:$CHDIR/ChargedAnalysis/Analysis/lib:$CHDIR/ChargedAnalysis/Network/lib:$CHDIR/ChargedAnalysis/Utility/lib:$CHDIR/Anaconda3/lib/python3.7/site-packages/torch/lib:$CHDIR/HiggsAnalysis/CombinedLimit/lib:$LD_LIBRARY_PATH
+        export PATH=$CHDIR/Anaconda3/bin:$CHDIR/ChargedAnalysis/bin:$CHDIR/HiggsAnalysis/CombinedLimit/exe:$CHDIR/HiggsAnalysis/CombinedLimit/scripts:$PATH
+        export LD_LIBRARY_PATH=$CHDIR/HiggsAnalysis/CombinedLimit/lib:$LD_LIBRARY_PATH
 
         source /cvmfs/sft.cern.ch/lcg/contrib/gcc/9binutils/x86_64-centos7/setup.sh
 
