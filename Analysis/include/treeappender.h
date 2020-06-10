bd3eb393ea9b16ff0fb0de9e0820ac274bb83c87
DaveBrun94 <david.brunner@desy.de>
2020-06-10 17:08:06 +0200

    Use functions moved into the extension namespace to get values for branches

--- Analysis/include/treeappender.h
+++ Analysis/include/treeappender.h
@@ -11,28 +11,17 @@
 #include <TTree.h>
 #include <TBranch.h>
 
-#include <ChargedAnalysis/Utility/include/utils.h>
-#include <ChargedAnalysis/Utility/include/frame.h>
-#include <ChargedAnalysis/Analysis/include/treefunction.h>
-#include <ChargedAnalysis/Analysis/include/treeparser.h>
-#include <ChargedAnalysis/Network/include/bdt.h>
-#include <ChargedAnalysis/Network/include/dnnmodel.h>
-#include <ChargedAnalysis/Network/include/htagger.h>
-#include <ChargedAnalysis/Network/include/htagdataset.h>
+#include <ChargedAnalysis/Utility/include/extension.h>
 
 class TreeAppender{
     private:
-        std::string oldFile, oldTree, newFile; 
-        std::vector<std::string> branchNames;
+        std::string fileName, treeName; 
+        std::vector<std::string> appendFunctions;
         std::string dCacheDir;
-
-        std::vector<float> HScore(const int& FJIndex, const int& length);
-        std::map<int, std::vector<float>> DNNScore(const std::vector<int>& masses, TTree* oldT);
-        std::map<int, std::vector<float>> BDTScore(const std::vector<int>& masses, TTree* oldT);
         
     public:
         TreeAppender();
-        TreeAppender(const std::string& oldFile, const std::string& oldTree, const std::string& newFile, const std::vector<std::string>& branchNames, const std::string& dCacheDir = "");
+        TreeAppender(const std::string& fileName, const std::string& treeName, const std::vector<std::string>& appendFunctions, const std::string& dCacheDir = "");
 
         void Append();
 };
--- Analysis/src/treeappender.cc
+++ Analysis/src/treeappender.cc
@@ -2,329 +2,52 @@
 
 TreeAppender::TreeAppender() {}
 
-TreeAppender::TreeAppender(const std::string& oldFile, const std::string& oldTree, const std::string& newFile, const std::vector<std::string>& branchNames, const std::string& dCacheDir) :
-        oldFile(oldFile), 
-        oldTree(oldTree),
-        newFile(newFile),
-        branchNames(branchNames),
+TreeAppender::TreeAppender(const std::string& fileName, const std::string& treeName, const std::vector<std::string>& appendFunctions, const std::string& dCacheDir) :
+        fileName(fileName), 
+        treeName(treeName),
+        appendFunctions(appendFunctions),
         dCacheDir(dCacheDir){}
 
-std::vector<float> TreeAppender::HScore(const int& FJindex, const int& length){
-    //Vector with final score (FatJetIndex/Score per event)
-    std::vector<float> tagValues(length, -999.);
-
-    //Tagger
-    torch::Device device(torch::kCPU);
-    std::vector<std::shared_ptr<HTagger>> tagger(2, std::make_shared<HTagger>(7, 140, 1, 130, 57, 0.06, device));
-
-    torch::load(tagger[0], std::string(std::getenv("CHDIR")) + "/DNN/Model/Even/htagger.pt");
-    torch::load(tagger[1], std::string(std::getenv("CHDIR")) + "/DNN/Model/Odd/htagger.pt");
-
-    tagger[0]->eval();
-    tagger[1]->eval();
-    torch::NoGradGuard no_grad;
-
-    //Get data set
-    HTagDataset data = HTagDataset({oldFile + "/"  + oldTree}, FJindex, device, true);
-    std::vector<int> entries;
-
-    for(int i = 0; i < length; i++){entries.push_back(i);}
-    std::vector<int>::iterator entry = entries.begin();
-    int batchSize = length > 2500 ? 2500 : length;
-    int counter = 0;
-    
-    while(entry != entries.end()){
-        //For right indexing
-        std::vector<int> evenIndex, oddIndex;
-
-        //Put signal + background in one vector and split by even or odd numbered event
-        std::vector<HTensor> evenTensors;
-        std::vector<HTensor> oddTensors;
-
-        for(int i = 0; i < batchSize; i++){
-            HTensor tensor = data.get(*entry);
-
-            if(tensor.isEven.item<bool>() == true){
-                evenTensors.push_back(tensor);
-                evenIndex.push_back(counter);
-            }
-
-            else{
-                oddTensors.push_back(tensor);
-                oddIndex.push_back(counter);
-            }
-
-            counter++;
-            ++entry;
-            if(entry == entries.end()) break; 
-        }
-
-        //Predictio
-        HTensor even, odd; 
-        torch::Tensor evenPredict, oddPredict;      
-
-        if(evenTensors.size() != 0){
-            even = HTagDataset::PadAndMerge(evenTensors);
-            evenPredict = tagger[1]->forward(even.charged, even.neutral, even.SV);
-        }  
-
-        if(oddTensors.size() != 0){    
-            odd = HTagDataset::PadAndMerge(oddTensors);
-            oddPredict = tagger[0]->forward(odd.charged, odd.neutral, odd.SV);
-        }     
-
-        //Put all predictions back in order again
-        for(int j = 0; j < evenTensors.size(); j++){
-            tagValues[evenIndex[j]] = evenPredict[j].item<float>();
-        }
-
-        for(int j = 0; j < oddTensors.size(); j++){
-            tagValues[oddIndex[j]] = oddPredict[j].item<float>();
-        }
-    }
-
-    return tagValues;
-}
-
-std::map<int, std::vector<float>> TreeAppender::DNNScore(const std::vector<int>& masses, TTree* oldT){
-    //Set values with default values
-    std::map<int, std::vector<float>> values;
-        
-    for(const int& mass: masses){
-        values[mass] = std::vector<float>(oldT->GetEntries(), -999.);
-    }
-
-    //Path with DNN infos
-    std::string dnnPath = std::string(std::getenv("CHDIR")) + "/DNN/Analysis/"; 
-
-    //Set tree parser and tree functions
-    TreeParser parser;
-    std::vector<TreeFunction> functions;
-    TreeFunction evNr(oldT->GetCurrentFile(), oldT->GetName());
-    evNr.SetFunction("EvNr");
-
-    //Read txt with parameter used in the trainind and set tree function
-    std::ifstream params(dnnPath + "/Even/" + oldTree + "/parameter.txt"); 
-    std::string parameter;
-  
-    while(getline(params, parameter)){
-        TreeFunction func(oldT->GetCurrentFile(), oldT->GetName());
-
-        parser.GetParticle(parameter, func);
-        parser.GetFunction(parameter, func);
-
-        functions.push_back(func);
-    }
-    params.close();
-
-    //Frame with optimized hyperparameter
-    std::unique_ptr<Frame> hyperParam = std::make_unique<Frame>(dnnPath + "/HyperOpt/" + oldTree + "/hyperparameter.csv");
-
-    //Get/load model and set to evaluation mode
-    torch::Device device(torch::kCPU);
-    std::vector<std::shared_ptr<DNNModel>> model(2, std::make_shared<DNNModel>(functions.size(), hyperParam->Get("nNodes", 0), hyperParam->Get("nLayers", 0), hyperParam->Get("dropOut", 0), true, device));
-
-    model[0]->eval();
-    model[1]->eval();
-
-    torch::load(model[0], dnnPath + "/Even/" + oldTree + "/model.pt");
-    torch::load(model[1], dnnPath + "/Odd/" + oldTree + "/model.pt");
-
-    torch::NoGradGuard no_grad;
-
-    std::vector<int> entries;
-    for(int i = 0; i < oldT->GetEntries(); i++){entries.push_back(i);}
-    std::vector<int>::iterator entry = entries.begin();
-    int batchSize = oldT->GetEntries() > 2500 ? 2500 : oldT->GetEntries();
-    int counter = 0;
-
-    while(entry != entries.end()){
-        //For right indexing
-        std::vector<int> evenIndex, oddIndex;
-
-        //Put signal + background in one vector and split by even or odd numbered event
-        std::vector<torch::Tensor> evenTensors;
-        std::vector<torch::Tensor> oddTensors;
-
-        for(int j = 0; j < batchSize; j++){
-            TreeFunction::SetEntry(*entry);
-
-            //Fill event class with particle content
-            std::vector<float> paramValues;
-
-            for(int i=0; i < functions.size(); i++){
-                paramValues.push_back(functions[i].Get());
-            }
-
-            if(int(evNr.Get()) % 2 == 0){
-                evenTensors.push_back(torch::from_blob(paramValues.data(), {1, paramValues.size()}).clone().to(device));
-                evenIndex.push_back(counter);
-            }
-
-            else{
-                oddTensors.push_back(torch::from_blob(paramValues.data(), {1, paramValues.size()}).clone().to(device));
-                oddIndex.push_back(counter);
-            }
-
-            counter++;
-            ++entry;
-            if(entry == entries.end()) break; 
-        }
-
-        for(const int& mass: masses){
-            //Prediction
-            torch::Tensor even, odd; 
-            torch::Tensor evenPredict, oddPredict;
-
-            if(evenTensors.size() != 0){    
-                even = torch::cat(evenTensors, 0);
-                evenPredict = model[1]->forward(even, mass*torch::ones({evenTensors.size(), 1}));
-            }  
-
-            if(oddTensors.size() != 0){    
-                odd = torch::cat(oddTensors, 0);
-                oddPredict = model[0]->forward(odd, mass*torch::ones({oddTensors.size(), 1}));
-            }     
-
-            //Put all predictions back in order again
-            for(int j = 0; j < evenTensors.size(); j++){
-                values[mass].at(evenIndex.at(j)) = evenPredict[j].item<float>();
-            }
-
-            for(int j = 0; j < oddTensors.size(); j++){
-                values[mass].at(oddIndex.at(j)) = oddPredict[j].item<float>();
-            }
-        }
-    }
-
-    return values;
-}
-
-std::map<int, std::vector<float>> TreeAppender::BDTScore(const std::vector<int>& masses, TTree* oldT){
-    std::map<int, std::vector<float>> values;
-
-    /*
-    std::string bdtPath = std::string(std::getenv("CHDIR")) + "/BDT/"; 
-
-    Event event;
-    TreeReader reader;
-
-    reader.PrepareEvent<TTree*>(oldT);
-
-    std::vector<Function> functions;
-    std::vector<FuncArgs> args;
-
-    std::ifstream bdtParams(bdtPath + "/Even/" + Utils::ChanPaths(oldTree) + "/parameters.txt"); 
-    std::string parameter;
-  
-    while(getline(bdtParams, parameter)){
-        Function func; FuncArgs arg;
-
-        reader.GetFunction(parameter, func, arg);
-        reader.GetParticle(parameter, arg);
-
-        functions.push_back(func);
-        args.push_back(arg);
-    
-    }
-    bdtParams.close();
-
-    BDT evenClassifier, oddClassifier;
-
-    evenClassifier.SetEvaluation(bdtPath + "/Even/" + Utils::ChanPaths(oldTree));
-    oddClassifier.SetEvaluation(bdtPath + "/Odd/" + Utils::ChanPaths(oldTree));
-
-    for (int i = entryStart; i < entryEnd; i++){
-        oldT->GetEntry(i);
-
-        //Fill event class with particle content
-        reader.SetEvent(event, JET, NONE);
-
-        std::vector<float> paramValues;
-
-        for(int i=0; i < functions.size(); i++){
-            paramValues.push_back(functions[i](event, args[i]));
-        }
-
-        for(const int& mass: masses){
-            paramValues.push_back(mass);
-
-            values[mass].push_back((int)TreeFunction::EventNumber(event, args[i]) % 2 == 0 ? oddClassifier.Evaluate(paramValues) : evenClassifier.Evaluate(paramValues));
-
-            paramValues.pop_back();
-        }
-    }
-
-    */
-    return values;
-}
-
 void TreeAppender::Append(){
-    at::set_num_interop_threads(1);
-    at::set_num_threads(1);
-
     //Get old Tree
-    TFile* oldF = TFile::Open(oldFile.c_str(), "READ");
-    TTree* oldT = (TTree*)oldF->Get(oldTree.c_str());
+    TFile* oldF = TFile::Open(fileName.c_str(), "READ");
+    TTree* oldT = (TTree*)oldF->Get(treeName.c_str());
 
-    std::cout << "Read file: '" << oldFile << "'" << std::endl;
-    std::cout << "Read tree '" << oldTree << "'" << std::endl;
-
-    //Disables wished branches if they are already existing in old tree
-    for(std::string& branchName: branchNames){
-        if(oldT->GetListOfBranches()->Contains(branchName.c_str())){
-            oldT->SetBranchStatus(branchName.c_str(), 0);
-        }
-    }
+    std::cout << "Read file: '" << fileName << "'" << std::endl;
+    std::cout << "Read tree '" << treeName << "'" << std::endl;
 
     //Clone Tree
     std::string tmpFile = "/tmp/tmp_" + std::to_string(getpid()) + ".root";
 
     TFile* newF = TFile::Open(tmpFile.c_str(), "RECREATE");
-    TTree* newT = oldT->CloneTree(-1, "fast");
+    TTree* newT = oldT->CloneTree(-1, "fast");;
 
     //Set new branches
     std::map<std::string, TBranch*> branches;
+    std::vector<std::string> branchNames;
     std::map<std::string, float> branchValues;
     std::map<std::string, std::vector<float>> values;
-    std::vector<int> masses;
 
-    for(std::string& branchName: branchNames){
-        branchValues[branchName] = -999.;
+    std::map<std::string, std::map<std::string, std::vector<float>>(*)(TFile*, const std::string&)> expandFunctions = {
+        {"HTagger", &Extension::HScore}, 
+        {"DNN", &Extension::DNNScore}, 
+        {"HReco", &Extension::HReconstruction}, 
+    };
 
-        branches[branchName] = newT->Branch(branchName.c_str(), &branchValues[branchName]);
+    for(const std::string& function: appendFunctions){
+        for(const std::pair<std::string, std::vector<float>>& funcValues : expandFunctions.at(function)(oldF, treeName)){
+            oldT->SetBranchStatus(funcValues.first.c_str(), 0);
 
-        if(branchName == "ML_HTagFJ1") values[branchName] = HScore(0, oldT->GetEntries());
-        if(branchName == "ML_HTagFJ2") values[branchName] = HScore(1, oldT->GetEntries());
+            branchValues[funcValues.first] = -999.;
+            branches[funcValues.first] = newT->Branch(funcValues.first.c_str(), &branchValues[funcValues.first]);
+            branchNames.push_back(funcValues.first);
 
-        if(Utils::Find<std::string>(branchName, "BDT") != -1.){
-            values[branchName] = std::vector<float>();
-            masses.push_back(std::stoi(branchName.substr(branchName.size()-3,3)));
-        }
-
-        if(Utils::Find<std::string>(branchName, "DNN") != -1.){
-            values[branchName] = std::vector<float>();
-            masses.push_back(std::stoi(branchName.substr(branchName.size()-3,3)));
+            values[funcValues.first] = funcValues.second;  
         }
     }
 
-    std::map<int, std::vector<float>> bdtScores;
-    std::map<int, std::vector<float>> dnnScores;
-
-    if(!masses.empty()){
-        dnnScores = DNNScore(masses, oldT);
-
-        for(const int& mass: masses){
-            for(std::string& branchName: branchNames){
-                if(Utils::Find<int>(branchName, mass) != -1.){
-                    values[branchName] = dnnScores[mass];
-                }
-            }
-        }
-    } 
-
     //Fill branches
-    for(int i=0; i < newT->GetEntries(); i++){
+    for(int i=0; i < oldT->GetEntries(); i++){
         if(i % 100000 == 0 and i != 0){
             std::cout << "Processed events: " << i << std::endl;
         }
@@ -338,7 +61,7 @@ void TreeAppender::Append(){
     newF->cd();
     newT->Write();
 
-    std::cout << "Sucessfully append branches " << branchNames << " in tree " << oldTree << std::endl;
+    std::cout << "Sucessfully append branches " << branchNames << " in tree " << treeName << std::endl;
 
     TList* keys = oldF->GetListOfKeys();
 
@@ -357,7 +80,7 @@ void TreeAppender::Append(){
     delete newT;
     delete newF;
 
-    std::system(("mv -vf " + tmpFile + " " + newFile).c_str());
+    std::system(("mv -vf " + tmpFile + " " + fileName).c_str());
 
-    if(dCacheDir != "") Utils::CopyToCache(newFile, dCacheDir);
+    if(dCacheDir != "") Utils::CopyToCache(fileName, dCacheDir);
 }
