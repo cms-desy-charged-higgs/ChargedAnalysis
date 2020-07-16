1191aa8159fec6eb937b8c4f711af563743a71dc
DaveBrun94 <david.brunner@desy.de>
2020-07-16 11:10:43 +0200

    Do the moving of files in executabl files in the executable

--- Analysis/exesrc/treeappend.cc
+++ Analysis/exesrc/treeappend.cc
@@ -9,10 +9,15 @@ int main(int argc, char* argv[]){
 
     std::string fileName = parser.GetValue<std::string>("file-name");
     std::string treeName = parser.GetValue<std::string>("tree-name");
-    std::string dCache = parser.GetValue<std::string>("dCache");
+    std::string dCache = parser.GetValue<std::string>("dCache", "");
     std::vector<std::string> functions = parser.GetVector<std::string>("functions");
 
-    TreeAppender appender(fileName, treeName, functions, dCache);
-    appender.Append();
+    std::string tmpFile = "tmp_" + std::to_string(getpid()) + ".root";
+
+    TreeAppender appender(fileName, treeName, functions);
+    appender.Append(tmpFile);
+
+    std::system(("mv -vf " + tmpFile + " " + fileName).c_str());
+    if(dCache != "") Utils::CopyToCache(fileName, dCache);
 }
 
