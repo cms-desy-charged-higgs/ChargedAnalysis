89877a495386d528dfccdf03658012ead6dbb2da
DaveBrun94 <david.brunner@desy.de>
2020-08-06 12:02:28 +0200

    Add Pooling and dropout

--- Network/include/htagger.h
+++ Network/include/htagger.h
@@ -8,28 +8,36 @@
 struct HTagger : torch::nn::Module{
     private:
         //Input layer
+        torch::nn::BatchNorm1d normCharged{nullptr};
+        torch::nn::BatchNorm1d normNeutral{nullptr};
+        torch::nn::BatchNorm1d normSV{nullptr};
+
         torch::nn::LSTM lstmCharged{nullptr};
         torch::nn::LSTM lstmNeutral{nullptr};
         torch::nn::LSTM lstmSV{nullptr};
 
+        torch::nn::BatchNorm1d normHidden{nullptr};
+
         //Conv1 layer
         torch::nn::Conv1d convLayer{nullptr};
         torch::nn::ReLU reluLayer{nullptr};
+        torch::nn::Dropout dropLayer{nullptr};
+        torch::nn::MaxPool1d poolLayer{nullptr};
 
         //Output layer
+        torch::nn::BatchNorm1d outNorm{nullptr};
         torch::nn::Linear outLayer{nullptr};
         torch::nn::Sigmoid sigLayer{nullptr};
 
         //Other stuff
         int nHidden;
-        int nLSTM;
         float dropOut;
         torch::Device device;
 
         std::ostringstream modelSummary;
         
     public:
-        HTagger(const int& nFeat, const int& nHidden, const int& nLSTM, const int& nConvFilter, const int& kernelSize, const float& dropOut, torch::Device& device);
+        HTagger(const int& nFeat, const int& nHidden, const int& nConvFilter, const int& kernelSize, const float& dropOut, torch::Device& device);
         torch::Tensor forward(torch::Tensor inputCharged, torch::Tensor inputNeutral, torch::Tensor inputSV);
         void Print();
         int GetNWeights();
--- Network/src/htagger.cc
+++ Network/src/htagger.cc
@@ -1,7 +1,6 @@
 #include <ChargedAnalysis/Network/include/htagger.h>
 
-HTagger::HTagger(const int& nFeat, const int& nHidden, const int& nLSTM, const int& nConvFilter, const int& kernelSize, const float& dropOut, torch::Device& device):
-    nLSTM(nLSTM),
+HTagger::HTagger(const int& nFeat, const int& nHidden, const int& nConvFilter, const int& kernelSize, const float& dropOut, torch::Device& device):
     nHidden(nHidden),
     dropOut(dropOut),
     device(device)
@@ -13,16 +12,15 @@ HTagger::HTagger(const int& nFeat, const int& nHidden, const int& nLSTM, const i
     modelSummary << "----------------------------------------\n";
 
     //Register LSTM Layers
-    lstmCharged = register_module("LSTM charged input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).num_layers(nLSTM).batch_first(true)));
+    lstmCharged = register_module("LSTM charged input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).batch_first(true)));
     lstmCharged->pretty_print(modelSummary); modelSummary << "\n";
 
-    lstmNeutral = register_module("LSTM neutral input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).num_layers(nLSTM).batch_first(true)));
+    lstmNeutral = register_module("LSTM neutral input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).batch_first(true)));
     lstmNeutral->pretty_print(modelSummary); modelSummary << "\n";
 
-    lstmSV = register_module("LSTM SV input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).num_layers(nLSTM).batch_first(true)));
+    lstmSV = register_module("LSTM SV input", torch::nn::LSTM(torch::nn::LSTMOptions(nFeat, nHidden).batch_first(true)));
     lstmSV->pretty_print(modelSummary); modelSummary << "\n";
 
-    //Convolution layer
     convLayer = register_module("Conv layer 1", torch::nn::Conv1d(3, nConvFilter, kernelSize));
     convLayer->pretty_print(modelSummary); modelSummary << "\n";
 
@@ -30,8 +28,16 @@ HTagger::HTagger(const int& nFeat, const int& nHidden, const int& nLSTM, const i
     reluLayer = register_module("ReLU layer", torch::nn::ReLU());
     reluLayer->pretty_print(modelSummary); modelSummary << "\n";
 
+    dropLayer = register_module("Dropout layer", torch::nn::Dropout(dropOut));
+    dropLayer->pretty_print(modelSummary); modelSummary << "\n";
+
+    poolLayer = register_module("Pool layer", torch::nn::MaxPool1d(torch::nn::MaxPool1dOptions(kernelSize)));
+    poolLayer->pretty_print(modelSummary); modelSummary << "\n";
+
     //Output layer
-    int outConv = nHidden + 2 - kernelSize -1;
+    int outConv = (nHidden - kernelSize)/kernelSize;
+
+    //int outConv = nHidden + 2 - kernelSize -1;
     outLayer = register_module("Output layer", torch::nn::Linear(nConvFilter*outConv, 1));
     outLayer->pretty_print(modelSummary); modelSummary << "\n";
 
@@ -64,9 +70,9 @@ int HTagger::GetNWeights(){
 
 torch::Tensor HTagger::forward(torch::Tensor inputCharged, torch::Tensor inputNeutral, torch::Tensor inputSV){
     //Get non-padded length for each batch
-    torch::Tensor charTrueLen = (inputCharged.index({torch::indexing::Slice(), torch::indexing::Ellipsis, torch::indexing::Slice(), torch::indexing::Ellipsis, 0}) != 0).sum(1).squeeze().to(torch::kCPU);
-    torch::Tensor neutralTrueLen = (inputNeutral.index({torch::indexing::Slice(), torch::indexing::Ellipsis, torch::indexing::Slice(), torch::indexing::Ellipsis, 0}) != 0).sum(1).squeeze().to(torch::kCPU);
-    torch::Tensor SVTrueLen = (inputSV.index({torch::indexing::Slice(), torch::indexing::Ellipsis, torch::indexing::Slice(), torch::indexing::Ellipsis, 0}) != 0).sum(1).squeeze().to(torch::kCPU);
+    torch::Tensor charTrueLen = (inputCharged.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}) != 0).sum(1).squeeze().to(torch::kCPU);
+    torch::Tensor neutralTrueLen = (inputNeutral.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}) != 0).sum(1).squeeze().to(torch::kCPU);
+    torch::Tensor SVTrueLen = (inputSV.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}) != 0).sum(1).squeeze().to(torch::kCPU);
 
     charTrueLen = torch::where(charTrueLen > 0, charTrueLen, torch::ones({1}).to(torch::kLong));
     neutralTrueLen = torch::where(neutralTrueLen > 0, neutralTrueLen, torch::ones({1}).to(torch::kLong));
@@ -81,12 +87,14 @@ torch::Tensor HTagger::forward(torch::Tensor inputCharged, torch::Tensor inputNe
     torch::Tensor chargedHidden = std::get<0>(std::get<1>(lstmCharged->forward_with_packed_input(chargedPack))).transpose(1, 0);
     torch::Tensor neutralHidden = std::get<0>(std::get<1>(lstmNeutral->forward_with_packed_input(neutralPack))).transpose(1, 0);
     torch::Tensor SVHidden = std::get<0>(std::get<1>(lstmSV->forward_with_packed_input(SVPack))).transpose(1, 0);
-
+    
     torch::Tensor z = torch::cat({chargedHidden, neutralHidden, SVHidden}, 1);
 
     //Convolution layer
     z = convLayer->forward(z);
     z = reluLayer->forward(z);
+    z = dropLayer->forward(z);
+    z = poolLayer->forward(z);
     z = torch::flatten(z, 1, 2);
     
     //Output layer
