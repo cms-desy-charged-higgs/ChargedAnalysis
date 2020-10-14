21c2c6070b63fcfb48963a6dfc6d7c3fe2911bc5
DaveBrun94 <david.brunner@desy.de>
2020-10-14 14:07:17 +0200

    Add merge of histograms for plots

--- Analysis/python/merge.py
+++ Analysis/python/merge.py
@@ -11,7 +11,7 @@ class Merge(Task):
         self["executable"] = "merge"
 
         self["arguments"] = [
-                "--input-files", *self["dependent-files"],
+                "--input-files", *self["input-files"],
                 "--out-file", self["output"], 
                 "--exclude-objects", *self["exclude-objects"],
                 "--delete-input" if "delete-input" in self else "",
@@ -21,8 +21,32 @@ class Merge(Task):
         self["output"] = "{}/{}".format(self["dir"], self["out-name"])
 
     @staticmethod
-    def configure(config, dependentTasks, taskName, prefix = ""):
+    def configure(config, dependentTasks, taskName, prefix = "", **kwargs):
         tasks = []
+
+        if taskName == "plot":
+            unmergedDirs = list(set(["/".join(d.split("/")[:-1]) for task in dependentTasks for d in [task["dir"]] + task["syst-dirs"]]))
+            toMerge = [[task for task in dependentTasks if True in [d == "/".join(out.split("/")[:-1]) for out in [task["dir"]] + task["syst-dirs"]]] for d in unmergedDirs]
+
+            for index, d in enumerate(unmergedDirs):
+                channel = toMerge[index][0]["channel"]
+                process = toMerge[index][0]["process"]
+                systName = d.split("/")[-1]
+
+                task = {
+                    "name": "Merge_{}_{}_{}{}".format(process, channel, kwargs["era"], systName) + ("_{}".format(prefix) if prefix else ""),  
+                    "dir": d.replace("unmerged", "merged"),
+                    "channel": channel,
+                    "process": process,
+                    "out-name": "{}.root".format(process), 
+                    "display-name": "Merge: {} ({}/{})".format(process, channel, kwargs["era"]),
+                    "dependencies": [t["name"] for t in toMerge[index]],
+                    "exclude-objects": [], 
+                    "input-files": ["{}/{}/{}.root".format(d, i, process) for i in range(len(toMerge[index]))]
+                }
+
+                tasks.append(Merge(task))
+            return tasks
 
         if taskName == "slim":
             unmergedDirs = list(set(["/".join(task["dir"].split("/")[:-1]) for task in dependentTasks]))
@@ -35,8 +59,10 @@ class Merge(Task):
                     "out-name": mergeTasks[0]["out-name"], 
                     "dir": unmergedDirs[index].replace("unmerged", "merged"),
                     "dependencies": [t["name"] for t in mergeTasks],
+                    "input-files": ["{}/{}".format(t["dir"], t["out-name"]) for t in mergeTasks],
                     "exclude-objects": ["Lumi", "xSec", "pileUp", "pileUpUp", "pileUpDown", "nGen", "nGenWeighted", "puMC", "nLooseDeepbTag", "nLooseDeepbTag", "nMediumDeepbTag", "nTightDeepbTag", "nLooseCSVbTag", "nMediumCSVbTag", "nTightCSVbTag", "nTrueB"],
                     "delete-input": True,
+                    "run-mode": config["run-mode"],
                 }
 
                 tasks.append(Merge(task))            
