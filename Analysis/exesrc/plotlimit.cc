adcfb6cbd52fb26ee0cc55eab78395bab147b40d
DaveBrun94 <david.brunner@desy.de>
2020-10-28 17:19:01 +0100

    Changed call

--- Analysis/exesrc/plotlimit.cc
+++ Analysis/exesrc/plotlimit.cc
@@ -8,13 +8,16 @@ int main(int argc, char* argv[]){
     //Parser arguments
     Parser parser(argc, argv);
 
-    std::vector<int> masses = parser.GetVector<int>("masses");
+    std::vector<int> chargedMasses = parser.GetVector<int>("charged-masses");
+    std::vector<int> neutralMasses = parser.GetVector<int>("neutral-masses");
+    std::string era = parser.GetValue<std::string>("era");
     std::vector<std::string> channels = parser.GetVector<std::string>("channels");
     std::string limitDir = parser.GetValue<std::string>("limit-dir");
     std::vector<std::string> outDirs = parser.GetVector<std::string>("out-dirs");
+    std::vector<float> xSecs = parser.GetVector<float>("x-secs");
   
     //Call and run Plotter1D class
-    PlotterLimit plotter(limitDir, masses, channels);
+    PlotterLimit plotter(limitDir, chargedMasses, neutralMasses, channels, era, xSecs);
     plotter.ConfigureHists();
     plotter.Draw(outDirs);
 }
