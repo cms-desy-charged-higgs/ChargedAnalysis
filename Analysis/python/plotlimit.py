b3d67e7d94ad9df7f786ca4502e56f1e3ca80fb0
DaveBrun94 <david.brunner@desy.de>
2020-10-28 17:21:54 +0100

    Enable era and combination of all eras

--- Analysis/python/plotlimit.py
+++ Analysis/python/plotlimit.py
@@ -1,4 +1,5 @@
 from task import Task
+from ROOT import TFile
 
 import os
 
@@ -10,28 +11,61 @@ class PlotLimit(Task):
         self["executable"] = "plotlimit"
 
         self["arguments"] = [
-                "--masses", *self["masses"],
+                "--charged-masses", *self["charged-masses"],
+                "--neutral-masses", *self["neutral-masses"],
                 "--channels", *self["channels"],
                 "--limit-dir", self["limit-dir"],
-                "--out-dirs", self["dir"]
+                "--out-dirs", self["dir"],
+                "--era", self["era"],
+                "--x-secs", *self["xSecs"] 
         ]
 
     def output(self):
         self["output"] = [self["dir"] + "/limit.pdf", self["dir"] + "/limit_by_channel.pdf"]
 
     @staticmethod
-    def configure(config):
+    def configure(config, limitTasks):
         tasks = []
 
-        plotConf = {"name": "PlotLimit", 
-                    "limit-dir": "{}/{}".format(os.environ["CHDIR"], config["dir"]), 
-                    "dir":  os.environ["CHDIR"] + "/CernWebpage/Plots/Limits", 
-                    "display-name": "Plot: Limit", 
-                    "dependencies": ["Limit_{}".format(mass) for mass in config["masses"]],
-                    "masses": config["masses"],
-                    "channels": config["channels"]
-        }
+        xSecs = []
 
+        for mHC in config["charged-masses"]:
+            for mh in config["neutral-masses"]:
+                f = TFile.Open("$CHDIR/Skim/Inclusive/2016/HPlusAndH_ToWHH_ToL4B_{MHC}_{MH}/merged/HPlusAndH_ToWHH_ToL4B_{MHC}_{MH}.root".format(MHC=mHC, MH=mh))
+                xSecs.append(f.Get("xSec").GetBinContent(1))
+
+
+        for era in config["era"]:
+            outDir = config["dir"].replace("[E]", era).replace("[C]", "")
+           
+            plotConf = {"name": "PlotLimit_{}".format(era), 
+                        "limit-dir": "{}/{}".format(os.environ["CHDIR"], config["dir"].replace("[E]", "").replace("[C]", "")), 
+                        "dir":  "{}/CernWebpage/Plots/{}".format(os.environ["CHDIR"], outDir),
+                        "display-name": "Plot: Limit", 
+                        "dependencies": [t["name"] for t in limitTasks if "Limit" in t["name"] and t["era"] == era],
+                        "charged-masses": config["charged-masses"],
+                        "neutral-masses": config["neutral-masses"],
+                        "channels": ["Combined"] + config["channels"],
+                        "era": era,
+                        "xSecs": xSecs,
+            }
+
+            tasks.append(PlotLimit(plotConf))
+
+        outDir = config["dir"].replace("[E]", "RunII").replace("[C]", "")
+
+        plotConf = {"name": "PlotLimit_{}".format("AllEras"), 
+                    "limit-dir": "{}/{}".format(os.environ["CHDIR"], config["dir"].replace("[E]", "").replace("[C]", "")), 
+                    "dir":  "{}/CernWebpage/Plots/{}".format(os.environ["CHDIR"], outDir),
+                    "display-name": "Plot: Limit", 
+                    "dependencies": [t["name"] for t in limitTasks if "Limit" in t["name"] and t["era"] == "RunII"],
+                    "charged-masses": config["charged-masses"],
+                    "neutral-masses": config["neutral-masses"],
+                    "channels": ["Combined"],
+                    "era": "RunII",
+                    "xSecs": xSecs,
+        }
+        
         tasks.append(PlotLimit(plotConf))
 
         return tasks
