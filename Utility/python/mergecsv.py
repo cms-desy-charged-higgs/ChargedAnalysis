8c4fce7dd5eba892e1dbb47fbceeae35198efa3c
DaveBrun94 <david.brunner@desy.de>
2020-10-29 10:15:01 +0100

    Add era

--- Utility/python/mergecsv.py
+++ Utility/python/mergecsv.py
@@ -18,19 +18,20 @@ class MergeCSV(Task):
         self["output"] = "{}/{}.csv".format(self["dir"], self["process"])
 
     @staticmethod
-    def configure(config, treeTasks, channel, prefix=""):
+    def configure(config, treeTasks, channel, era, prefix=""):
         tasks = []
 
         for process in config["processes"] + config["data"].get(channel, []):
             outDir = os.environ["CHDIR"] + "/{}/{}/{}".format(config["dir"], channel, process)
                 
             task = {
-                    "name": "MergeCSV{}_{}".format(process, channel) + ("_{}".format(prefix) if prefix else ""),  
+                    "name": "MergeCSV_{}_{}_{}".format(process, channel, era) + ("_{}".format(prefix) if prefix else ""),  
                     "dir": outDir,
                     "channel": channel,
                     "process": process,
-                    "display-name": "Merge CSV: {} ({})".format(process, channel),
-                    "dependencies": [t["name"] for t in treeTasks if process == t["process"] and channel == t["channel"]]
+                    "era": era,
+                    "display-name": "Merge CSV: {} ({}/{})".format(process, channel, era),
+                    "dependencies": [t["name"] for t in treeTasks if process == t["process"] and channel == t["channel"] and era == t["era"]]
             }
 
             tasks.append(MergeCSV(task))
