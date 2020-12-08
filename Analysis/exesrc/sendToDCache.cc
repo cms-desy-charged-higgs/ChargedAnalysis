318e8331b7a6f0e73a954ebaf354af7345dcfef7
DaveBrun94 <david.brunner@desy.de>
2020-12-08 16:07:44 +0100

    Check for file validity

--- Analysis/exesrc/sendToDCache.cc
+++ Analysis/exesrc/sendToDCache.cc
@@ -1,15 +1,25 @@
 #include <string>
+#include <filesystem>
 
 #include <ChargedAnalysis/Utility/include/parser.h>
 #include <ChargedAnalysis/Utility/include/stringutil.h>
+#include <ChargedAnalysis/Utility/include/rootutil.h>
 
 int main(int argc, char *argv[]){
     //Parser arguments
     Parser parser(argc, argv);
 
+    //Read parser
     std::string inputFile = parser.GetValue<std::string>("input-file");
     std::string dCachePath = parser.GetValue<std::string>("dcache-path");
     std::string relativePath = parser.GetValue<std::string>("relative-path");
+
+    //Check if file is valid
+    RUtil::Open(inputFile).reset();
+
+    if(std::filesystem::is_symlink(inputFile)){
+        throw std::runtime_error(StrUtil::Merge("File is a sym link: ", inputFile));
+    }
     
     std::string gfalPrefix = "srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=";
     std::string fileName = StrUtil::Split(inputFile, "/").back();
