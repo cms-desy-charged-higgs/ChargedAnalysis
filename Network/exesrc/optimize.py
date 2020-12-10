80ba3fb2e8e50b2255cdc2d29bc4eeb0c208edf3
DaveBrun94 <david.brunner@desy.de>
2020-12-10 11:13:51 +0100

    Reworked to use condor and as an input a shell executable

--- Network/exesrc/optimize.py
+++ Network/exesrc/optimize.py
@@ -5,118 +5,150 @@ import argparse
 import os
 import yaml
 import pandas as pd
-import numpy as np
 import time
+import numpy as np
 import sys
 from multiprocessing import Pool, cpu_count
-from functools import partial
 
 def parser():
     parser = argparse.ArgumentParser(description = "Script to handle and execute analysis tasks", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--config", type=str, action="store", help="Yaml config with DNN configuration")
-    parser.add_argument("--type", type=str, action="store", help="Which DNN to optimize")
+    parser.add_argument("--dir", type=str, action="store", required = True, help="Dir with DNN information")
+    parser.add_argument("--channels", type=str, nargs = "+", required = True, help="Channel name")
+    parser.add_argument("--era", type=str, nargs = "+", required = True, help="Era")
+    parser.add_argument("--type", type=str, action="store", required = True, choices = ["DNN", "HTag"], help="Which DNN to optimize")
     return parser.parse_args()
 
-def dnn(config, channel, batchSize, lr, nNodes, nLayers, dropOut):
-    try:
-        dnnDir = "{}/{}/{}".format(os.environ["CHDIR"], config["dir"].format("Odd"), channel)
+def prepareJobs(oldExeFile, hyperParam):
+    jobDirs, params = [], []
+    N = 200
 
-        hyperParam = ["--batch-size", str(batchSize), "--lr", str(10**(int(lr))), "--n-nodes", str(nNodes), "--n-layers", str(nLayers), "--drop-out", str(dropOut)]
-        files = ["--sig-files", *["{d}/{sig}/{sig}.csv".format(d = dnnDir, sig = config["signal"].format(m)) for m in config["masses"]], "--bkg-files", *["{d}/{bkg}/{bkg}.csv".format(d = dnnDir, bkg = bkg) for bkg in config["backgrounds"]]]
-
-        out = subprocess.run(["dnn", "--optimize", "--out-path", dnnDir] + hyperParam + files, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        return float(out.stdout.decode('utf-8').split("\n")[-2].split(" ")[-1])
-
-    except:
-        return 0.
-
-def htag(batchSize, nHidden, nConvFilter, kernelSize, dropOut, lr):
-    try:
-        hyperParam = ["--batch-size", str(batchSize), "--lr", str(10**(int(lr))), "--n-hidden", str(nHidden), "--n-convfilter", str(nConvFilter), "--n-kernelsize", str(kernelSize), "--drop-out", str(dropOut)]
-
-        out = subprocess.run(["htag", "--optimize"] + hyperParam, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        return float(out.stdout.decode('utf-8').split("\n")[-2].split(" ")[-1])
-
-    except:
-        return 0.
-
-def optimize(func, hyperParam, hyperDir):
-    os.makedirs(hyperDir, exist_ok = True)
-
-    np.random.seed(sum(time.localtime()))
-    pool = Pool(8)#cpu_count())
-
-    param = [{key: value[0](*value[1]) for key, value in hyperParam.items()} for i in range(cpu_count())]
-    jobs = [pool.apply_async(func, kwds=p) for p in param]
-    nJobs = len(jobs)
-    bestLoss = 1e7
-
-    while(nJobs < 200 or len(jobs) != 0):
-        for i, p in enumerate(param):
-            if jobs[i].ready():
-                loss = jobs[i].get()
-
-                if(loss < bestLoss and loss != 0):
-                    bestLoss = loss
-
-                    print("Best loss: {}, Hyperparam:".format(loss), p, "Iteration:", nJobs) 
-                    p["lr"] =  10**(p["lr"])
-
-                    frame = pd.DataFrame.from_dict({k: [v] for (k, v) in p.items()})
-
-                    frame.to_csv("{}/hyperparameter.csv".format(hyperDir), index=False, sep="\t")
-
-                jobs.pop(i)
-                param.pop(i)
-
-                if(nJobs < 200):
-                    param.append({key: value[0](*value[1]) for key, value in hyperParam.items()})
-                    jobs.append(pool.apply_async(func, kwds=param[-1]))
+    condorSub = [
+            "universe = vanilla",
+            "executable = $(DIR)/run.sh",
+            "getenv = True",
+            "log = $(DIR)/log.txt",
+            "error = $(DIR)/err.txt",
+            "output = $(DIR)/out.txt",
+    ]
+           
+    ##Write submit file
+    with open("/tmp/condor.sub", "w") as condFile:
+        for line in condorSub:
+            condFile.write(line + "\n")
     
-                    if(loss != 0): 
-                        nJobs += 1
+    for i in range(N):
+        tmpDir = "Tmp/tmpDir{}".format(time.time_ns())
+        os.makedirs(tmpDir, exist_ok = True)
+        jobDirs.append(tmpDir)
+        
+        param = {key: value[0](*value[1]) for key, value in hyperParam.items()}
+        params.append(param)
+
+        with open(tmpDir + "/run.sh", "w") as exe:
+            with open(oldExeFile, "r") as oldExe:
+                while(True):
+                    line = oldExe.readline()
+                    
+                    if not line:
+                        break
+            
+                    if not "opt-param" in line:
+                        exe.write(line)
+
+                    else:
+                        oldExe.readline()
+
+            for p, v in param.items():        
+                exe.write("{}--{} \\".format(2*" ", p))
+                exe.write("\n")
+                exe.write("{}{} \\".format(6*" ", v if p != "lr" else 10**v))
+                exe.write("\n")
+                
+            exe.write("--optimize\n")
+
+        subprocess.run("chmod a+rx {}/run.sh".format(tmpDir), shell = True)
+    
+    subprocess.run("condor_submit /tmp/condor.sub -queue DIR in " + " ".join(jobDirs), shell = True)
+
+    return jobDirs, params
+
+def optimize(outDir, jobDirs, params):
+    os.makedirs(outDir, exist_ok = True)
+
+    bestLoss = 1e7
+    N = len(jobDirs)
+
+    while(jobDirs):
+        for idx, jobDir in enumerate(jobDirs):
+            if not os.path.exists("{}/log.txt".format(jobDir)): continue
+        
+            with open("{}/log.txt".format(jobDir), "r") as log:
+                if "(return value" in log.read():
+                    with open("{}/out.txt".format(jobDir), "r") as out:
+                        try:
+                            loss = float(out.read().split("\n")[-2])
+                        
+                        except:
+                            loss = 1e7
+       
+                        if loss < abs(bestLoss):
+                            bestLoss = loss
+                            p = params[idx]
+                            p["lr"] = 10**(p["lr"])
+                                 
+                            frame = pd.DataFrame.from_dict({k: [v] for (k, v) in p.items()})    
+                            frame.to_csv("{}/hyperparameter.csv".format(outDir), index=False, sep="\t")
+                            
+                    jobDirs.pop(idx)
+                    params.pop(idx)
 
 def main():
     ##Parser argument
     args = parser()
 
     ##Set high niceness
-    os.nice(19)
+    pool = Pool(cpu_count())
+    jobs = []
 
-    if args.type == "dnn":
-        config = yaml.load(open(args.config, "r"), Loader=yaml.Loader)
-
+    if args.type == "DNN":
         # Bounded region of parameter space
         hyperParam = {
-                'batchSize': [np.random.randint, (500, 4000)], 
+                'batch-size': [np.random.randint, (200, 4000)], 
                 'lr': [np.random.randint, (-6, -2)], 
-                "nNodes": [np.random.randint, (30, 300)], 
-                "nLayers": [np.random.randint, (1, 10)], 
-                "dropOut": [np.random.uniform, (0., 0.5)],
+                "n-nodes": [np.random.randint, (30, 500)], 
+                "n-layers": [np.random.randint, (1, 10)], 
+                "drop-out": [np.random.uniform, (0., 0.5)],
         }
 
-        for channel in config["channels"]:
-            hyperDir = "{}/{}/{}".format(os.environ["CHDIR"], config["dir"].format("HyperOpt"), channel)
-            optimize(partial(dnn, config, channel), hyperParam, hyperDir)
-
-
-    elif args.type == "htag":
+        for era in args.era:
+            for channel in args.channels:
+                outDir = "{}/HyperOpt/{}/{}".format(args.dir, channel, era)
+                oldExe = "{}/Even/{}/{}/run.sh".format(args.dir, channel, era)
+                
+                jobDirs, params = prepareJobs(oldExe, hyperParam)
+                jobs.append(pool.apply_async(optimize, args = (outDir, jobDirs, params)))
+                
+        [job.get() for job in jobs]
+        
+        subprocess.run("command rm -rf Tmp", shell = True)
+        
+    elif args.type == "HTag":
         # Bounded region of parameter space
         hyperParam = {
-                'batchSize': [np.random.randint, (200, 4000)], 
+                'batch-size': [np.random.randint, (200, 4000)], 
                 'lr': [np.random.randint, (-6, -2)], 
-                "nHidden": [np.random.randint, (5, 300)], 
-                "nConvFilter": [np.random.randint, (5, 300)],
-                "kernelSize": [np.random.randint, (2, 200)],
-                "dropOut": [np.random.uniform, (0., 0.5)],
+                "drop-out": [np.random.uniform, (0., 0.5)],
+                "n-hidden": [np.random.randint, (5, 300)], 
+                "n-convfilter": [np.random.randint, (5, 300)],
+                "n-kernelsize": [np.random.randint, (2, 200)],
         }
 
-        hyperDir = "{}/DNN/Tagger/HyperOptNew".format(os.environ["CHDIR"])
+        outDir = "{}/HyperOpt".format(args.dir)
+        oldExe = "{}/Even/{}/run.sh".format(args.dir, args.era)
 
-        optimize(htag, hyperParam, hyperDir)
+        optimize(hyperParam, outDir, oldExe)
     
 if __name__ == "__main__":
     main()
+    print()
 
