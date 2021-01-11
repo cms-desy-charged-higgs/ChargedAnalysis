2545ae5b12808727f3c7b02c1fc1fab895751862
DaveBrun94 <david.brunner@desy.de>
2021-01-11 18:00:58 +0100

    add configuration for several tasks

--- Workflow/exesrc/analysis.py
+++ Workflow/exesrc/analysis.py
@@ -15,23 +15,12 @@ def parser():
     parser.add_argument("--task", type=str, choices = ["Plot", "HTagger", "Append", "Limit", "DNN", "Slim", "Merge", "Estimate"])
     parser.add_argument("--config", type=str)
     parser.add_argument("--era", nargs='+', default = [""])
-    parser.add_argument("--check-output", action = "store_true")
-    parser.add_argument("--no-http", action = "store_true")
+    parser.add_argument('--dry-run', default=False, action='store_true')
+    parser.add_argument("--run-again", type=str)
 
     return parser.parse_args()
 
-def taskReturner(taskName, era, **kwargs):
-    confDir = "{}/ChargedAnalysis/Workflow/config/".format(os.environ["CHDIR"])
-
-    ##Load general config
-    config = yaml.load(open("{}/general.yaml".format(confDir), "r"), Loader=yaml.Loader)
-    
-    ##Append task specifig config
-    if kwargs["config"]:
-        config.update(yaml.load(open("{}/{}.yaml".format(confDir, kwargs["config"]), "r"), Loader=yaml.Loader))
-
-    config["era"] = era
-
+def taskReturner(taskName, config):
     tasks = {
         "Append": lambda : append(config),
         "HTagger": lambda : htagger(config),
@@ -50,23 +39,79 @@ def plot(config):
 
     for channel in config["channels"]:
         for era in config["era"]:
+            processes = sorted(config["processes"] + config.get("data", {}).get("channel", []))
+
             for process in config["processes"]:
                 for syst in config["shape-systs"].get("all", []) + config["shape-systs"].get(channel, []):
                     for shift in ["Up", "Down"]:
                         treeread(tasks, config, channel, era, process, syst, shift)
                         mergeHists(tasks, config, channel, era, process, syst, shift)
 
+            plotting(tasks, config, channel, era, processes)
+
+    return tasks
+
+def dnn(config):
+    tasks = []
+
+    for channel in config["channels"]:
+        for era in config["era"]:
+            for evType, operator in zip(["Even", "Odd"], ["divisible", "notdivisible"]):
+                c = copy.deepcopy(config)
+                c["cuts"].setdefault("all", []).append("f:n=EvNr/c:n={},v=2".format(operator))
+
+                DNN(tasks, config, channel, era, evType)
+
+    return tasks
+
+def append(config):
+    tasks = []
+
+    for channel in config["channels"]:
+        for era in config["era"]:
+            for syst in config["shape-systs"].get("all", []) + config["shape-systs"].get(channel, []):
+                for shift in ["Up", "Down"]:
+                    treeappend(tasks, config, channel, era, syst, shift)
+
+                    if "dCache" in config:
+                        DCacheDir = config["dCache"].format_map(dd(str, {"C": channel, "E": era}))
+
+
+    sendToDCache(config, tasks, os.environ["CHDIR"])
+    
     return tasks
 
 def main():
     args = parser()
 
-    ##Configure and get the tasks
-    tasks = taskReturner(args.task, args.era, config=args.config)()
+    tasks = []
+    taskDir = ""
+
+    ##Read general config and update with given config
+    if args.config:
+        confDir = "{}/ChargedAnalysis/Workflow/config/".format(os.environ["CHDIR"])
+
+        config = yaml.load(open("{}/general.yaml".format(confDir), "r"), Loader=yaml.Loader)
+        config.update(yaml.load(open("{}/{}.yaml".format(confDir, args.config), "r"), Loader=yaml.Loader))
+
+        taskDir = "{}/Results/{}/{}".format(os.environ["CHDIR"], args.task, config["dir"].split("/")[0])
+        os.makedirs(taskDir, exist_ok = True)
+
+        with open("{}/config.yaml".format(taskDir), "w") as conf:
+            yaml.dump(config, conf, default_flow_style=False, indent=4)
+
+        config["dir"] = "{}/Results/{}/{}".format(os.environ["CHDIR"], args.task, config["dir"])
+        config["era"] = args.era
+
+        ##Configure and get the tasks
+        tasks = taskReturner(args.task, config)()
+
+    elif args.run_again:
+        taskDir = "/".join(args.run_again.split("/")[:-1])
 
     ##Run the manager
-    manager = TaskManager(tasks)
-    manager.run()
+    manager = TaskManager(tasks = tasks, existingFlow = args.run_again, dir = taskDir)
+    manager.run(args.dry_run)
 
 if __name__=="__main__":
     main()
--- Workflow/python/configuration.py
+++ Workflow/python/configuration.py
@@ -2,11 +2,14 @@ from task import Task
 
 import yaml
 import os
+import copy
 import csv
 
-def treeread(tasks, config, channel, era, process, syst, shift):
+from collections import defaultdict as dd
+
+def treeread(tasks, config, channel, era, process, syst, shift, postFix = ""):
     ##Dic with process:filenames 
-    processDic = yaml.load(open("{}/ChargedAnalysis/Analysis/data/process.yaml".format(os.environ["CHDIR"]), "r"), Loader=yaml.Loader)
+    processes = yaml.safe_load(open("{}/ChargedAnalysis/Analysis/data/process.yaml".format(os.environ["CHDIR"]), "r"))
 
     ##Skip Down for nominal case or and skip data if systematic
     if (syst == "" and shift == "Down") or (process in ["SingleE", "SingleMu"] and syst != ""): 
@@ -14,7 +17,7 @@ def treeread(tasks, config, channel, era, process, syst, shift):
 
     systName = "{}{}".format(syst, shift) if syst != "" else ""
 
-    skimDir = "{}/{}".format(os.environ["CHDIR"], config["skim-dir"].replace("[C]", channel).replace("[E]", era))
+    skimDir = "{}/{}".format(os.environ["CHDIR"], config["skim-dir"].format_map(dd(str, {"C": channel, "E": era})))
         
     ## Scale systematics
     scaleSysts = config["scale-systs"].get("all", []) + config["scale-systs"].get(channel, []) if process not in ["SingleE", "SingleMu"] and syst == "" else [""]
@@ -26,7 +29,7 @@ def treeread(tasks, config, channel, era, process, syst, shift):
 
     if "scaleFactors" in config:
         if "Single" not in process and "HPlus" not in process:
-            scaleFactors = "{}/{}".format(os.environ["CHDIR"], config["scaleFactors"].replace("[C]", channel).replace("[E]", era))
+            scaleFactors = "{}/{}".format(os.environ["CHDIR"], config["scaleFactors"].format_map(dd(str, {"C": channel, "E": era})))
 
             with open(scaleFactors) as f:
                 reader = csv.reader(f, delimiter='\t')
@@ -42,18 +45,18 @@ def treeread(tasks, config, channel, era, process, syst, shift):
 
     ##List of filenames for each process
     for d in os.listdir(skimDir):
-        for processName in processDic[process]:
+        for processName in processes[process]:
             if d.startswith(processName):
                 fileNames.append("{skim}/{file}/merged/{file}.root".format(skim=skimDir, file = d))
 
     for idx, fileName in enumerate(fileNames):
-        outDir = os.environ["CHDIR"] + "/{}/{}/unmerged/{}/{}".format(config["dir"].replace("[E]", era).replace("[C]", channel), process, systName, idx)
+        outDir = config["dir"].format_map(dd(str, {"D": "Histograms", "C": channel, "E": era})) + "/{}/{}/unmerged/{}".format(process, systName, idx)
 
         systDirs = [outDir.replace("unmerged/", "unmerged/{}{}/".format(scaleSyst, scaleShift)) for scaleSyst in scaleSysts if scaleSyst != "" for scaleShift in ["Up", "Down"]]
 
         ##Configuration for treeread Task
         task = {
-            "name": "TreeRead_{}_{}_{}_{}_{}".format(channel, era, process, systName, idx),
+            "name": "TreeRead_{}_{}_{}_{}_{}_{}".format(channel, era, process, systName, idx, postFix),
             "executable": "treeread",
             "dir": outDir,
             "run-mode": config["run-mode"],
@@ -74,7 +77,7 @@ def treeread(tasks, config, channel, era, process, syst, shift):
 
         tasks.append(Task(task, "--"))
 
-def mergeHists(tasks, config, channel, era, process, syst, shift):
+def mergeHists(tasks, config, channel, era, process, syst, shift, postFix = ""):
     inputFiles, dependencies = [], []
 
     ##Skip Down for nominal case or and skip data if systematic
@@ -83,7 +86,7 @@ def mergeHists(tasks, config, channel, era, process, syst, shift):
 
     systName = "{}{}".format(syst, shift) if syst != "" else ""
 
-    outDir = os.environ["CHDIR"] + "/{}/{}/merged/{}/".format(config["dir"].replace("[E]", era).replace("[C]", channel), process, systName)
+    outDir = config["dir"].format_map(dd(str, {"D": "Plots", "C": channel, "E": era})) + "/{}/{}/merged".format(process, systName)
 
     for t in tasks:
         if "TreeRead_{}_{}_{}_{}".format(channel, era, process, systName) in t["name"]:
@@ -91,15 +94,170 @@ def mergeHists(tasks, config, channel, era, process, syst, shift):
             inputFiles.append("{}/{}".format(t["arguments"]["out-dir"], t["arguments"]["out-file"]))
     
     task = {
-        "name": "MergeHists_{}_{}_{}_{}".format(channel, era, process, systName),
+        "name": "MergeHists_{}_{}_{}_{}_{}".format(channel, era, process, systName, postFix),
         "executable": "merge",
         "dir": outDir,
         "dependencies": dependencies,
+        "process": process,
         "arguments": {
             "input-files": inputFiles,
             "out-file": "{}/{}.root".format(outDir, process),
             "exclude-objects": [], 
         }
     }
+
+    tasks.append(Task(task, "--"))
+
+def plotting(tasks, config, channel, era, processes, postFix = ""):
+    dependencies = []
+    bkgProcesses, sigProcesses = [], []
+    bkgFiles, sigFiles = [], []
+    data, dataFile = "", ""
+
+    processes.sort()
+
+    outDir = config["dir"].format(D = "PDF", E = era, C = channel)
+
+    for t in tasks:
+        for process in processes:
+            if "MergeHists" in t["name"] and process == t["process"] and not "Up" in t["name"] and not "Down" in t["name"]:
+                dependencies.append(t["name"])
+
+                if "HPlus" in process:
+                    sigProcesses.append(process)
+                    sigFiles.append(t["arguments"]["out-file"])
+
+                elif "Single" in process:
+                    data = process
+                    dataFile = t["arguments"]["out-file"]
+
+                else:
+                    bkgProcesses.append(process)
+                    bkgFiles.append(t["arguments"]["out-file"])
+                    
+
+    task = {
+        "name": "Plot_{}_{}_{}".format(channel, era, postFix), 
+        "executable": "plot",
+        "dir": outDir,
+        "dependencies": dependencies,
+        "arguments": {
+            "channel": channel,
+            "era": era,
+            "bkg-processes": bkgProcesses,
+            "bkg-files": bkgFiles,
+            "sig-processes": sigProcesses,
+            "sig-files": sigFiles,
+            "data": data,
+            "data-file": dataFile,
+            "out-dirs": [outDir],
+        }
+    }
+
+    tasks.append(Task(task, "--"))
+
+def treeappend(tasks, config, channel, era, syst, shift, postFix = ""):
+    ##Skip Down for nominal case
+    if syst == "" and shift == "Down":
+        return
+
+    systName = "{}{}".format(syst, shift) if syst != "" else ""
+    skimDir = "{}/{}".format(os.environ["CHDIR"], config["skim-dir"].format_map(dd(str, {"C": channel, "E": era})))
+    outDir = config["dir"].format_map(dd(str, {"C": channel, "E": era}))
+
+    for fileName in os.listdir(skimDir):
+        if "Run2" in fileName and syst != "":
+            continue
+
+        ##Configuration for treeread Task
+        task = {
+            "name": "Append_{}_{}_{}_{}_{}".format(channel, era, fileName, systName, postFix),
+            "dir":  "{}/{}/{}".format(outDir, fileName, systName),
+            "executable": "treeappend",
+            "run-mode": config["run-mode"],
+            "arguments": {
+                "out-file": "{}/{}/merged/{}/{}.root".format(skimDir, fileName, systName, fileName),
+                "tree-name": channel,
+                "functions": config["functions"].get("all", []) + config["functions"].get(channel, []),
+                "era": era,
+            }
+        }
+
+        tasks.append(Task(task, "--"))
+
+def sendToDCache(config, tasks, removePrefix, postFix = ""):
+    for idx, t in enumerate(copy.deepcopy(tasks)):
+        taskDir = t["dir"] + "/toDCache"
+
+        task = {
+            "name": "toDache_{}_{}".format(len(tasks), postFix), 
+            "dir": taskDir,
+            "executable": "sendToDCache",
+            "dependencies": [t["name"]],
+            "arguments": {
+                "input-file": t["arguments"]["out-file"],
+                "out-file": t["arguments"]["out-file"].split("/")[-1], 
+                "dcache-path": "/pnfs/desy.de/cms/tier2/store/user/dbrunner/",
+                "relative-path": "/".join(t["arguments"]["out-file"].split("/")[:-1]).replace(removePrefix, ""), 
+            }
+        }
+
+        tasks.append(Task(task, "--"))
+
+def DNN(tasks, config, channel, era, evType, postFix = ""):
+    processes = yaml.safe_load(open("{}/ChargedAnalysis/Analysis/data/process.yaml".format(os.environ["CHDIR"]), "r"))
+
+    skimDir = "{}/{}".format(os.environ["CHDIR"], config["skim-dir"].format_map(dd(str, {"C": channel, "E": era})))
+    outDir = config["dir"].format_map(dd(str, {"C": channel, "E": era, "T": evType}))
+
+    classes, signals = {}, []
+
+    for d in sorted(os.listdir(skimDir)):
+        for process in config["classes"] + config.get("Misc", []):
+            for processName in processes[process]:
+                if d.startswith(processName):
+                    cls = process if process in config["classes"] else "Misc"
+                        
+                    classes.setdefault(cls, []).append("{skim}/{file}/merged/{file}.root".format(skim=skimDir, file = d))
+
+        for process in [config["signal"].format_map(dd(str, {"MHC": mass})) for mass in config["masses"]]:
+            for processName in processes[process]:
+                if d.startswith(processName):
+                    signals.append("{skim}/{file}/merged/{file}.root".format(skim=skimDir, file = d))
+
+    os.makedirs(outDir, exist_ok=True)
+
+    with open(outDir + "/parameter.txt", "w") as param:
+        for parameter in config["parameters"]:
+            param.write(parameter + "\n")
+                
+    with open(outDir + "/classes.txt", "w") as param:
+        for cls in classes.keys():
+            param.write(cls + "\n")
+                
+    with open(outDir + "/masses.txt", "w") as param:
+        for mass in config["masses"]:
+            param.write(str(mass) + "\n")
+
+    task = {
+        "name": "DNN_{}_{}_{}_{}".format(channel, era, evType, postFix),
+        "executable": "dnn",
+        "dir": outDir,
+        "run-mode": config["run-mode"],
+        "arguments": {
+            "out-path": outDir,
+            "channel": channel,   
+            "cuts": config["cuts"].get("all", []) + config["cuts"].get(channel, []),
+            "parameters": config["parameters"].get("all", []) + config["parameters"].get(channel, []),
+            "sig-files": signals,
+            "masses": config["masses"],
+            "opt-param": config.get("hyper-opt", "").format_map(dd(str, {"C": channel, "E": era, "T": evType})),
+            "clean-jets": config["clean-jets"].get(channel, ""),
+            "bkg-classes": list(classes.keys()),
+        }
+    }
+
+    for cls, files in classes.items():
+        task["arguments"]["{}-files".format(cls)] = files
 
     tasks.append(Task(task, "--"))
