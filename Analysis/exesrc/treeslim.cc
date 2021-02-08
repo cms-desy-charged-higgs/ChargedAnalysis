fc684d57aacb8c3d0429db2d9b1dd1e3f9b66bbd
DaveBrun94 <david.brunner@desy.de>
2021-02-08 19:50:19 +0100

    Create outdir if nedded

--- Analysis/exesrc/treeslim.cc
+++ Analysis/exesrc/treeslim.cc
@@ -5,14 +5,19 @@ int main(int argc, char* argv[]){
     //Parser arguments
     Parser parser(argc, argv);
 
-    std::string inName = parser.GetValue<std::string>("input-name");
+    std::string inName = parser.GetValue<std::string>("input-file");
     std::string inChannel = parser.GetValue<std::string>("input-channel");
-    std::string outName = parser.GetValue<std::string>("out-name");
+    std::string outFile = parser.GetValue<std::string>("out-file");
     std::string outChannel = parser.GetValue<std::string>("out-channel");
     std::vector<std::string> cuts = parser.GetVector<std::string>("cuts");
     int start = parser.GetValue<int>("event-start");
     int end = parser.GetValue<int>("event-end");
 
+    //Create output directory if needed
+    if(!StrUtil::Find(outFile, "/").empty()){
+        std::filesystem::create_directories(outFile.substr(0, StrUtil::Find(outFile, "/").back()));
+    }
+    
     TreeSlimmer slimmer(inName, inChannel);    
-    slimmer.DoSlim(outName, outChannel, cuts, start, end);
+    slimmer.DoSlim(outFile, outChannel, cuts, start, end);
 }
