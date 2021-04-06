d22c2fb2a2f562b9b980d96e511833f42746afea
DaveBrun94 <david.brunner@desy.de>
2021-04-06 18:16:12 +0200

    Implement topological sorting back

--- Workflow/python/taskmanager.py
+++ Workflow/python/taskmanager.py
@@ -12,23 +12,21 @@ from taskmonitor import TaskMonitor
 from task import Task
 
 class TaskManager(object):
-    def __init__(self, tasks = [], existingFlow = "", dir = "", condorSub = "$CHDIR/ChargedAnalysis/Workflow/templates/tasks.sub", html = "$CHDIR/ChargedAnalysis/Workflow/templates/workflow.html"):
+    def __init__(self, tasks = [], existingFlow = "", dir = "", condorSub = "$CHDIR/ChargedAnalysis/Workflow/templates/tasks.sub", html = "$CHDIR/ChargedAnalysis/Workflow/templates/workflow.html", longCondor = False):
         self.time = time.time()
 
         ##Read in tasks or create from given csv summary file of other workflow
-        self._tasks = {}
+        self._tasks = {t["name"] : t for t in tasks}
 
         if tasks:
-            self._tasks = {t["name"]: t for t in tasks}
-
             ##Check for unique name/directory
-            names = list(self._tasks.keys())   
+            names = list(self._tasks.keys())
             nonUnique = list(set([n for n in names if names.count(n) > 1]))
 
             if nonUnique:
                 raise RuntimeError("Each task has to have an unique name! Non unique names: \n{}".format("\n".join(nonUnique)))
 
-            dirs = [t["dir"] for t in self._tasks.values()]  
+            dirs = [t["dir"] for t in tasks]  
             nonUnique = list(set([d for d in dirs if dirs.count(d) > 1]))
 
             if nonUnique:
@@ -56,6 +54,7 @@ class TaskManager(object):
         os.makedirs(self.dir, exist_ok = True)
 
         self.condorSub = condorSub
+        self.longCondor = longCondor
         self.html = html
 
         ##Template for workflow.html displaying jobs
@@ -71,9 +70,9 @@ class TaskManager(object):
                     self.htmlTemplate += line
         
         ##Monitor displayed
-        self.monitor = TaskMonitor(len(self._tasks.keys()))
+        self.monitor = TaskMonitor(len(self._tasks))
         
-        self.remove = []
+        ##Track jobs
         self.runningTasks = {}
         self.status = {}
 
@@ -85,6 +84,33 @@ class TaskManager(object):
         ##Write remaining jobs in case of failure
         for t in self._tasks.values():
             self.summary.writerow({"Task": t["name"], "Dir": t["dir"], "Status": t["status"]})
+            
+    def __sortTasks(self):
+        ##Sort tasks by Kahn’s algorithm for topological sorting
+        in_degrees = [len(task["dependencies"]) for task in self._tasks.values()]
+        queue = [task for task in self._tasks.values() if len(task["dependencies"]) == 0]
+
+        sortedTasks = {}
+        counter=0
+
+        while queue:
+            t = queue.pop(0)
+            sortedTasks.setdefault(t["run-mode"], []).append(t["name"])
+
+            for i, task in enumerate(self._tasks.values()):
+                if t["name"] in task["dependencies"]:
+                    in_degrees[i] -= 1
+
+                    if in_degrees[i] == 0:
+                        queue.append(task)
+        
+            counter+=1
+           
+        if counter != len(self._tasks):
+            raise RuntimeError("There exists a cyclic dependency in your tasks!")
+    
+        else:
+            self._taskNames = sortedTasks    
 
     def __writeHTML(self):
         finished = ""
@@ -111,84 +137,106 @@ class TaskManager(object):
             ##Decrement number of old status
             self.status.setdefault(runType, {}).setdefault(task["status"], 0)
             self.status[runType][task["status"]] -= 1
-            
-        ##Mark task to be removed from self._tasks
-        if status == "Finished":
-            self.remove.append(task["name"])
-
+     
         ##Increment number of new status
         self.status.setdefault(runType, {}).setdefault(status, 0)
         self.status[runType][status] += 1
         task["status"] = status
 
         ##Write workflow.html
-       # self.__writeHTML()
+      #  self.__writeHTML()
         
         ##Break if job failed
         if status == "Failed":
             self.monitor.updateMonitor(time.time() - self.time, self.status)
             raise RuntimeError("Task '{}' failed! See error message: {}/err.txt".format(task["name"], task["dir"]))
-        
+
     def __getJobs(self):
         pool = mp.Pool(processes=20)
-        condorSubmit = "condor_submit {} -batch-name TaskManager -queue DIR in ".format(self.condorSub)
-        toSubmit = []        
+        condorSubmit = "condor_submit {} -batch-name TaskManager".format(self.condorSub) + (" -append '+RequestRuntime = 86400' " if self.longCondor else "") + " -queue DIR in "
+        toSubmit = []  
+        
+        runModes = list(self._taskNames.keys())
+        lookForward = {mode : 0 for mode in runModes}  
 
-        while(self._tasks):
-            ##Remove finished jobs
-            while(self.remove):
-                t = self._tasks.pop(self.remove.pop())
-                self.summary.writerow({"Task": t["name"], "Dir": t["dir"], "Status": t["status"]})
+        while(True):
+            yield
+                 
+            ##No jobs left (everything finished)
+            if not self._tasks and len([t for m in self.runningTasks.keys() for t in self.runningTasks[m]]) == 0:
+                break
+               
+            ##No jobs left, but still jobs are running
+            elif not self._tasks:
+                yield
+                continue
+           
+            for mode in runModes:
+                if(lookForward[mode] >= len(self._taskNames[mode])):
+                    lookForward[mode] = 0
 
-            for name, task in self._tasks.items():
-                yield True
+                ##Pop front task
+                if len(self._taskNames[mode]) != 0:
+                    name = self._taskNames[mode].pop(lookForward[mode])
+                    task = self._tasks.pop(name)
 
-                if task["status"] != "Valid":
+                else:
                     continue
 
                 ##Check if dependencies all finished
                 try:          
                     for dep in task["dependencies"]:
-                        if dep in self._tasks:
+                        if dep in self._tasks or dep in [name for m in self.runningTasks.keys() for name in self.runningTasks[m]]:
                             raise ValueError("")
                             
+                    lookForward[mode] = 0
+                                
                 except ValueError:
+                    self._taskNames[mode].insert(lookForward[mode], name)
+                    self._tasks[name] = task
+           
+                    lookForward[mode] += 1
+                    
                     continue
-                
+                    
                 ##Local job configuration
                 if task["run-mode"] == "Local":
                     if len(self.runningTasks.get("Local", [])) >= 20:
+                        self._taskNames[mode].insert(lookForward[mode], name)
+                        self._tasks[name] = task
                         continue
 
                     task.job = pool.apply_async(copy.deepcopy(task.run))
 
                     self.__changeStatus(task, "Running", "Local")
                     self.runningTasks.setdefault("Local", OrderedDict())[name] = task 
-            
+                
                 ##Condor job configuration
                 if task["run-mode"] == "Condor":
-                    if len(self.runningTasks.get("Condor", [])) >= 500:
+                    if len(self.runningTasks.get("Condor", [])) >= 600:
+                        self._taskNames[mode].insert(lookForward[mode], name)
+                        self._tasks[name] = task
                         continue
-
-                    if len(toSubmit) >= 100:
-                        subprocess.run(condorSubmit + " ".join(toSubmit), shell = True, stdout=open(os.devnull, 'wb'))
-                        toSubmit.clear()
 
                     self.runningTasks.setdefault("Condor", OrderedDict())[name] = task
                     self.__changeStatus(task, "Submitted", "Condor")
                     toSubmit.append(task["dir"])
 
-            if len(toSubmit) != 0:
+            ##Submit remaining condors jobs
+            if "Condor" in lookForward and (len(toSubmit) >= 200 or (lookForward["Condor"] >= len(self._taskNames["Condor"]) and len(toSubmit) != 0)):
                 subprocess.run(condorSubmit + " ".join(toSubmit), shell = True, stdout=open(os.devnull, 'wb'))
                 toSubmit.clear()
 
     def run(self, dryRun = False):
+        ##Sort tasks
+        self.__sortTasks()
+    
         ##Create directories/executable etc.
         for task in self._tasks.values():
-            task.prepare()
             self.__changeStatus(task, "Valid", task["run-mode"])
-        
-        if dryRun:
+            task.prepare()
+                
+        if dryRun: 
             return 0
 
         ##Update monitor
@@ -228,6 +276,12 @@ class TaskManager(object):
                         
                     else:
                         self.__changeStatus(task, "Failed", "Condor")
+
+                elif "SYSTEM_PERIODIC_REMOVE" in condorLog:
+                    self.__changeStatus(task, "Valid", "Condor")
+                    self._taskNames["Condor"].append(name)
+                    self._tasks[name] = task
+                    task.prepare()
                
                 else:
                     self.runningTasks["Condor"][name] = task
@@ -237,5 +291,5 @@ class TaskManager(object):
 
             next(jobHandler, None)
                
-            if not self._tasks:
+            if not self._tasks and len([t for m in self.runningTasks.keys() for t in self.runningTasks[m]]) == 0:
                 break
