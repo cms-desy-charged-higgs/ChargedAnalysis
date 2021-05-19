1c459a0608db68e71c527c76db975a9226746476
DaveBrun94 <david.brunner@desy.de>
2021-05-19 12:14:15 +0200

    Different way of excluding stuff from merging

--- Analysis/exesrc/merge.cc
+++ Analysis/exesrc/merge.cc
@@ -3,12 +3,13 @@
 
 #include <ChargedAnalysis/Utility/include/parser.h>
 #include <ChargedAnalysis/Utility/include/stringutil.h>
+#include <ChargedAnalysis/Utility/include/rootutil.h>
 
 #include <TFileMerger.h>
 #include <TFile.h>
 #include <TTree.h>
 
-void Merge(const std::vector<std::string> inFiles, const std::string outFile, const std::vector<std::string> excludeObjects){
+void Merge(const std::vector<std::string>& inFiles, const std::string& outFile, const std::vector<std::string>& excludeObjects){
     //Set Max Tree Size to 1 TB
     TTree::SetMaxTreeSize(1000000000000);
 
@@ -32,25 +33,38 @@ void Merge(const std::vector<std::string> inFiles, const std::string outFile, co
     merger.SetFastMethod(true);
     merger.SetPrintLevel(99);
 
+    std::vector<std::shared_ptr<TFile>> files;
+    std::vector<TObject*> toIncludeLater;
+
     for(const std::string file: inFiles){
-        merger.AddFile(file.c_str());
+        files.push_back(RUtil::Open(file));
+        merger.AddFile(files.back().get());
+
+        //Get objects which should not be merged from first file
+        if(files.size() == 1){
+            for(const std::string& objName : excludeObjects){
+                if(files.back()->Get(objName.c_str()) == nullptr){
+                    std::cout << StrUtil::Join(" ", "Object with name", objName, "not existing! It will be skipped") << std::endl;
+                    continue;
+                }
+
+                toIncludeLater.push_back(files.back()->Get(objName.c_str()));
+            }
+        }
     }
 
     merger.OutputFile(outFile.c_str(), "RECREATE");
     merger.Merge();
 
-    std::unique_ptr<TFile> in(TFile::Open(inFiles[0].c_str(), "READ"));
-    std::unique_ptr<TFile> out(TFile::Open(outFile.c_str(), "UPDATE"));
+    std::unique_ptr<TFile> out(TFile::Open(outFile.c_str(), "UPDATE")); 
 
-    for(const std::string& objName : excludeObjects){
-        if(out->Get(objName.c_str()) == nullptr){
-            std::cout << StrUtil::Join(" ", "Object with name", objName, "not existing! It will be skipped") << std::endl;
-            continue;
-        }
-
-        out->Delete((objName + ";*").c_str());
-        in->Get(objName.c_str())->Write();
+    for(TObject* obj : toIncludeLater){
+        out->Delete(StrUtil::Merge(obj->GetName(), ";*").c_str());
+        obj->Write();
+        obj->Print();
     }
+
+    out->Close();
 }
 
 int main(int argc, char *argv[]){
@@ -60,7 +74,7 @@ int main(int argc, char *argv[]){
     std::vector<std::string> inFiles = parser.GetVector<std::string>("input-files");
     std::string outFile = parser.GetValue<std::string>("out-file");
     std::vector<std::string> excludeObjects = parser.GetVector<std::string>("exclude-objects");
-    bool deleteInput = parser.GetValue<bool>("delete-input");    
+    bool deleteInput = parser.GetValue<bool>("delete-input");
 
     Merge(inFiles, outFile, excludeObjects);
 
