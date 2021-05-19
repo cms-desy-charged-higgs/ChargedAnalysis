99996ff08c6b0b37bee5734fd104295615499e3f
DaveBrun94 <david.brunner@desy.de>
2021-05-19 12:11:42 +0200

    Brute force proper copy to dCache

--- Analysis/exesrc/sendToDCache.cc
+++ Analysis/exesrc/sendToDCache.cc
@@ -27,8 +27,18 @@ int main(int argc, char *argv[]){
     std::string totalPath = StrUtil::Merge(gfalPrefix, dCachePath, relativePath, "/");
     std::string outFile = StrUtil::Merge(dCachePath, relativePath, "/", fileName);
 
-    std::system(("gfal-mkdir -p " + totalPath).c_str());
-    std::system(StrUtil::Merge("gfal-copy -f ", inputFile, " ", totalPath, fileName).c_str());
+    while(true){
+        std::system(("gfal-mkdir -p " + totalPath).c_str());
+        std::system(StrUtil::Merge("gfal-copy -f ", inputFile, " ", totalPath, fileName).c_str());
+
+        try{
+            RUtil::Open(outFile);
+            break;
+        }
+
+        catch(...){}
+    }
+
     std::system(("rm -fv " + inputFile).c_str());
     std::system(StrUtil::Merge("ln -sv ", outFile, " ", inputFile).c_str());
 }
