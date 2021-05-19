845c0b4eb124813bf50bc754ca52e4f213c05c08
DaveBrun94 <david.brunner@desy.de>
2021-05-19 12:57:48 +0200

    Use templates

--- Utility/include/parser.h
+++ Utility/include/parser.h
@@ -6,6 +6,10 @@
 #include <map>
 #include <iostream>
 #include <sstream>
+#include <typeinfo>
+#include <experimental/source_location>
+
+#include <ChargedAnalysis/Utility/include/stringutil.h>
 
 class Parser{
     private:
@@ -14,17 +18,73 @@ class Parser{
     public:
         Parser(int argc, char** argv);
 
-        template <typename T>
-        T GetValue(const std::string& option);
+        template <typename T = std::string>
+        T GetValue(const std::string& option, const std::experimental::source_location& location = std::experimental::source_location::current()){
+            T value;
+            std::string type = typeid(value).name();
+            std::string data;
 
-        template <typename T>
-        T GetValue(const std::string& option, const T& defaultValue);
+            if(options.find(option) != options.end()){
+                if(options[option].size() == 0) data = type == "b" ? "1" : "";
+                else data = options[option][0];
+            }
 
-        template <typename T>
-        std::vector<T> GetVector(const std::string& option);
+            else{
+                if(type == "b"){
+                    data = "0";
+                }
 
-        template <typename T>
-        std::vector<T> GetVector(const std::string& option, const std::vector<T>& defaultValue);
+                else throw std::runtime_error(StrUtil::PrettyError(location, "No parser argument: ", option));
+            }
+
+            std::istringstream iss(data);
+            iss >> value;
+
+            return value;
+        }
+
+        template <typename T = std::string, typename U>
+        T GetValue(const std::string& option, const U& defaultValue, const std::experimental::source_location& location = std::experimental::source_location::current()){
+            try{
+                return GetValue<T>(option);
+            }
+            
+            catch(...){
+                return defaultValue;
+            }
+        }
+
+        template <typename T = std::string>
+        std::vector<T> GetVector(const std::string& option, const std::experimental::source_location& location = std::experimental::source_location::current()){
+            std::vector<T> values;
+
+            if(options.find(option) == options.end()){
+                throw std::runtime_error(StrUtil::PrettyError(location, "No parser argument: ", option));
+            }
+
+            for(const std::string& data: options[option]){
+                T value;
+                std::istringstream iss(data);
+                iss >> value;
+                values.push_back(value);
+            }
+
+            return values;
+        }
+
+        template <typename T = std::string, typename U>
+        std::vector<T> GetVector(const std::string& option, const std::initializer_list<U>& defaultValue, const std::experimental::source_location& location = std::experimental::source_location::current()){
+            try{
+                return GetVector<T>(option);
+            }
+            
+            catch(...){
+                std::vector<T> defaultV;
+                for(const U& v : defaultValue) defaultV.push_back(static_cast<T>(v));
+
+                return defaultV;
+            }
+        }
 };
 
 #endif
