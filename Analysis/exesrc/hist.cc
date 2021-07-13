8cbe8dea734591dda371a14fa2fac8067faf20fb
DaveBrun94 <david.brunner@desy.de>
2021-07-13 14:10:17 +0200

    Use stack trace class

--- Analysis/exesrc/hist.cc
+++ Analysis/exesrc/hist.cc
@@ -1,7 +1,11 @@
 #include <ChargedAnalysis/Utility/include/parser.h>
+#include <ChargedAnalysis/Utility/include/backtracer.h>
 #include <ChargedAnalysis/Analysis/include/histmaker.h>
 
 int main(int argc, char* argv[]){
+    //Class for backtrace
+    Backtracer trace;
+
     //Parser arguments
     Parser parser(argc, argv);
 
@@ -11,12 +15,16 @@ int main(int argc, char* argv[]){
     std::string outDir = parser.GetValue<std::string>("out-dir");
     std::string outFile = parser.GetValue<std::string>("out-file");
     std::string channel = parser.GetValue<std::string>("channel");
-    std::vector<std::string> scaleDirs = parser.GetVector<std::string>("syst-dirs", {});
+    std::string bkgYieldFac = parser.GetValue<std::string>("bkg-yield-factor", "");
+    std::vector<std::string> bkgYieldFacSyst = parser.GetVector<std::string>("bkg-yield-factor-syst", {});
+    std::string bkgType = parser.GetValue<std::string>("bkg-type", "");
     std::vector<std::string> scaleSysts = parser.GetVector<std::string>("scale-systs", {""});
-    std::string scaleFactors = parser.GetValue<std::string>("scale-factors", "");
+    std::vector<std::string> systDirs = parser.GetVector<std::string>("syst-dirs", {});
     int era = parser.GetValue<int>("era", 2017);
+    int eventStart = parser.GetValue<int>("event-start");
+    int eventEnd = parser.GetValue<int>("event-end");
 
     //Create treereader instance
-    HistMaker h(parameters, cuts, outDir, outFile, channel, scaleDirs, scaleSysts, scaleFactors, era);
-    h.Produce(fileName);
+    HistMaker h(parameters, cuts, outDir, outFile, channel, systDirs, scaleSysts, era);
+    h.Produce(fileName, eventStart, eventEnd, bkgYieldFac, bkgType, bkgYieldFacSyst);
 }
