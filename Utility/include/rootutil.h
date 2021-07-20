24d787a0db41ccce9f5333a6c24fdf9231b86ef6
DaveBrun94 <david.brunner@desy.de>
2021-07-20 12:21:28 +0200

    Function for 2D variable rebinning

--- Utility/include/rootutil.h
+++ Utility/include/rootutil.h
@@ -8,6 +8,7 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 #include <experimental/source_location>
 
 #include <ChargedAnalysis/Utility/include/stringutil.h>
@@ -19,12 +20,16 @@
 #include <TTree.h>
 #include <TBranch.h>
 #include <TLeaf.h>
+#include <TH2.h>
 
 /**
 * @brief Utility library to handle getter function for ROOT pointer objects
 */
 
 namespace RUtil{
+    template <typename H>
+    concept Hist2D = std::is_base_of<TH2, H>::value;
+
     /**
     * @brief Open ROOT TFile with exception handling in READ mode
     *
@@ -196,6 +201,54 @@ namespace RUtil{
         }
 
         return v;
+    }
+
+    template <Hist2D H> 
+    std::shared_ptr<H> Rebin2D(const std::shared_ptr<H>& hist, const std::vector<double>& xBins, const std::vector<double>& yBins){
+        //Get current binning
+        std::vector<double> currentX(hist->GetNbinsX() + 1, 0.), currentY(hist->GetNbinsY() + 1, 0.);
+
+        for(unsigned int i = 1; i <= hist->GetNbinsX() + 1; ++i){
+            currentX[i - 1] = hist->GetXaxis()->GetBinLowEdge(i);
+        }
+
+        for(unsigned int i = 1; i <= hist->GetNbinsY() + 1; ++i){
+            currentY[i - 1] = hist->GetYaxis()->GetBinLowEdge(i);
+        }
+
+        if(xBins.size() > currentX.size()) throw std::runtime_error("New x-binning has more bins than current binning!");
+        if(yBins.size() > currentY.size()) throw std::runtime_error("New y-binning has more bins than current binning!");
+
+        //Create new hist
+        std::shared_ptr<H> newHist = std::make_shared<H>("", "", xBins.size() - 1, xBins.data(), yBins.size() - 1, yBins.data());
+
+        //Intermediate hist
+        std::shared_ptr<H> xBinned = std::make_shared<H>("x", "x", xBins.size() - 1, xBins.data(), currentY.size() - 1, currentY.data());
+
+        //Rebin x-axis first
+        for(unsigned int y = 1; y < currentY.size(); ++y){
+            std::shared_ptr<TH1D> projX(static_cast<TH1D*>(hist->ProjectionX("pr", y, y)->Rebin(xBins.size() - 1, "re", xBins.data())));
+
+            for(unsigned int x = 1; x < xBins.size(); ++x){
+                xBinned->SetBinContent(x, y, projX->GetBinContent(x));
+                xBinned->SetBinError(x, y, projX->GetBinError(x));
+            }
+        }
+
+        //Rebin y-axis second
+        for(unsigned int x = 1; x < xBins.size(); ++x){
+            std::shared_ptr<TH1D> projY(static_cast<TH1D*>(xBinned->ProjectionY("pr", x, x)->Rebin(yBins.size() - 1, "re", yBins.data())));
+
+            for(unsigned int y = 1; y < yBins.size(); ++y){
+                newHist->SetBinContent(x, y, projY->GetBinContent(y));
+                newHist->SetBinError(x, y, projY->GetBinError(y));
+            }
+        }
+
+        newHist->SetName(hist->GetName());
+        newHist->SetTitle(hist->GetTitle());
+
+        return newHist;
     }
 };
 
