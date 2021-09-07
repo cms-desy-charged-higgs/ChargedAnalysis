8116e9a974646d9f031f4b9981a6eec60d4548a6
DaveBrun94 <david.brunner@desy.de>
2021-09-07 18:12:48 +0200

    Use CSV class for merging

--- Utility/exesrc/mergeCSV.cc
+++ Utility/exesrc/mergeCSV.cc
@@ -1,14 +1,12 @@
 #include <ChargedAnalysis/Utility/include/parser.h>
-#include <ChargedAnalysis/Utility/include/frame.h>
+#include <ChargedAnalysis/Utility/include/csv.h>
 
 int main(int argc, char* argv[]){
     //Extract informations of command line
     Parser parser(argc, argv);
 
-    std::vector<std::string> inFiles = parser.GetVector<std::string>("input");
-    std::string outName = parser.GetValue<std::string>("output");
+    std::vector<std::string> inFiles = parser.GetVector<std::string>("input-files");
+    std::string outFile = parser.GetValue<std::string>("out-file");
 
-    Frame frame(inFiles);
-
-    frame.WriteCSV(outName);
+    CSV::Merge(outFile, inFiles);
 }
