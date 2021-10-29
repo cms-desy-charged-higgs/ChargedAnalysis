21393be540f6d8e36f0a2ddc3fb280c25c175f56
DaveBrun94 <david.brunner@desy.de>
2021-10-29 12:15:53 +0200

    Handle several input files

--- Network/include/dnndataset.h
+++ Network/include/dnndataset.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <experimental/random>
 
 #include <TTree.h>
 #include <TFile.h>
@@ -21,7 +22,8 @@
 struct DNNTensor{
     torch::Tensor input;    //!< Tensor with kinematic input
     torch::Tensor label;    //!< Label to check if Signal or not (isSignal = 1, isBackground = 0)
-   
+    torch::Tensor mHPlus;
+    torch::Tensor mH;
 };
 
 /**
@@ -30,28 +32,31 @@ struct DNNTensor{
 
 class DNNDataSet{
     private:
-        std::string fileName, channel;
-        int era, classLabel;
+        std::string channel;
+        std::vector<std::string> fileNames;
+        int era, classLabel, nClass;
         std::vector<std::string> parameters;
         torch::Device device;
 
-        std::shared_ptr<NCache> cache;
-        std::vector<NTupleReader> functions;
+        std::vector<std::shared_ptr<NTupleReader>> readers;
+        std::vector<NTupleFunction> functions;
 
-        std::vector<std::size_t> entryList;
-        std::shared_ptr<TFile> file;
-        std::shared_ptr<TTree> tree;
+        std::vector<std::size_t> chargedMasses, neutralMasses;
+        std::vector<std::pair<std::size_t, std::size_t>> entryList;
+        std::vector<std::shared_ptr<TFile>> files;
+        std::vector<std::shared_ptr<TTree>> trees;
+
+        torch::Tensor OneHotEncoder(const int& classLabel, const int& nClasses, torch::Device& device);
 
     public:
-        DNNDataSet(const std::string& fileName, const std::string& channel, const std::string& entryListName, const std::vector<std::string>& parameters, const int& era, torch::Device& device, const int& classLabel);
-
-        int chargedMass, neutralMass;
-        int GetClass(){return classLabel;}
+        DNNDataSet(const std::vector<std::string>& fileNames, const std::string& channel, const std::vector<std::string>& entryListName, const std::vector<std::string>& parameters, const int& era, torch::Device& device, const int& classLabel, const int& nClass, const std::vector<std::size_t>& chargedMasses, const std::vector<std::size_t>& neutralMasses);
 
         void Init();
+        std::size_t Size() const;
+
+        int GetClass(){return classLabel;}
         DNNTensor Get(const std::size_t& entry);
-        std::vector<DNNTensor> GetBatch(const std::size_t& entryStart, const std::size_t& entryEnd);
-        std::size_t Size();
+
         static DNNTensor Merge(const std::vector<DNNTensor>& tensors);
 };
 
--- Network/src/dnndataset.cc
+++ Network/src/dnndataset.cc
@@ -4,78 +4,106 @@
 */
 
 #include <ChargedAnalysis/Network/include/dnndataset.h>
+#include <ChargedAnalysis/Utility/include/stopwatch.h>
 
-DNNDataSet::DNNDataSet(const std::string& fileName, const std::string& channel, const std::string& entryListName, const std::vector<std::string>& parameters, const int& era, torch::Device& device, const int& classLabel) :
-    fileName(fileName),
+DNNDataSet::DNNDataSet(const std::vector<std::string>& fileNames, const std::string& channel, const std::vector<std::string>& entryListName, const std::vector<std::string>& parameters, const int& era, torch::Device& device, const int& classLabel, const int& nClass, const std::vector<std::size_t>& chargedMasses, const std::vector<std::size_t>& neutralMasses) :
+    fileNames(fileNames),
     channel(channel),
     era(era),
     parameters(parameters),
     device(device),
-    classLabel(classLabel){
+    classLabel(classLabel), 
+    nClass(nClass),
+    chargedMasses(chargedMasses),
+    neutralMasses(neutralMasses){
 
-    CSV idx(entryListName, "r", ",");
+    std::vector<std::shared_ptr<CSV>> idx(fileNames.size(), 0);
+    std::vector<std::size_t> p(fileNames.size(), 0);
 
-    entryList = std::vector<std::size_t>(idx.GetNRows());
+    for(std::size_t i = 0; i < fileNames.size(); ++i){
+        idx[i] = std::make_shared<CSV>(entryListName[i], "r", "\t");
+    }
 
-    for(int row = 0; row < idx.GetNRows(); ++row){
-        entryList[row] = idx.Get<int>(row, 0);
+    while(true){
+        int nFin = 0;
+
+        for(std::size_t i = 0; i < fileNames.size(); ++i){
+            if(p[i] >= idx[i]->GetNRows()){
+                ++nFin;
+                continue;
+            }
+    
+            entryList.push_back({i, idx[i]->Get<int>(p[i], 0)});
+            ++p[i];
+        }
+
+        if(nFin == fileNames.size()) break;
     }
 }
 
 void DNNDataSet::Init(){
-    //Open file/get tree (call this function in local thread)
-    file = RUtil::Open(fileName);
-    tree = RUtil::GetSmart<TTree>(file.get(), channel);
-
     Decoder parser;
-    functions = std::vector<NTupleReader>(parameters.size());
-    cache = std::make_shared<NCache>();
 
-    for(std::size_t idx = 0; idx < parameters.size(); ++idx){
-        //Functor structure and arguments
-        NTupleReader function(tree, era, cache);
-        
-        //Read in everything, orders matter
-        parser.GetParticle(parameters.at(idx), function);
-        parser.GetFunction(parameters.at(idx), function);
-        function.Compile();
+    for(std::size_t i = 0; i < fileNames.size(); ++i){
+        //Open file/get tree (call this function in local thread)
+        files.push_back(RUtil::Open(fileNames.at(i)));
+        trees.push_back(RUtil::GetSmart<TTree>(files[i].get(), channel));
 
-        functions[idx] = function;
+        readers.push_back(std::make_shared<NTupleReader>(trees.back(), era));
+          
+        for(std::size_t p = 0; p < parameters.size(); ++p){
+            //Functor structure and arguments
+            NTupleFunction function = readers[i]->BuildFunc();
+            
+            //Read in everything, orders matter
+            parser.GetParticle(parameters.at(p), function);
+            parser.GetFunction(parameters.at(p), function);
+            function.Compile();
+
+            functions.push_back(function);
+        }
     }
 }
 
-std::size_t DNNDataSet::Size(){
+std::size_t DNNDataSet::Size() const{
     return entryList.size();
 }
 
-DNNTensor DNNDataSet::Get(const std::size_t& entry){
-    cache->clear();
-    std::vector<float> paramValues(parameters.size());
+torch::Tensor DNNDataSet::OneHotEncoder(const int& classLabel, const int& nClasses, torch::Device& device){
+    torch::Tensor hotLabels = torch::zeros({1, nClasses}).to(device);
+    hotLabels.index_put_({0, classLabel}, 1);
 
-    for(std::size_t idx = 0; idx < functions.size(); ++idx){
-        paramValues.at(idx) = functions.at(idx).Get(entryList.at(entry));
-    }
-
-    return {torch::from_blob(paramValues.data(), {1, paramValues.size()}).clone().to(device), torch::tensor({classLabel}).to(device)};
-}
-
-std::vector<DNNTensor> DNNDataSet::GetBatch(const std::size_t& entryStart, const std::size_t& entryEnd){
-    std::vector<DNNTensor> batch(entryEnd - entryStart);
-
-    for(std::size_t entry = entryStart, idx = 0; entry < entryEnd; ++entry, ++idx){
-        batch.at(idx) = Get(entry);
-    }
-
-    return batch;
+    return hotLabels;
 }
 
 DNNTensor DNNDataSet::Merge(const std::vector<DNNTensor>& tensors){
-    std::vector<torch::Tensor> input(tensors.size()), label(tensors.size()); 
-
-    for(std::size_t idx = 0; idx < tensors.size(); ++idx){
-        input[idx] = tensors[idx].input;
-        label[idx] = tensors[idx].label;
+    std::vector<torch::Tensor> input(tensors.size()), label(tensors.size()), mHPlus(tensors.size()), mH(tensors.size());
+    
+    for(std::size_t i = 0; i < input.size(); ++i){
+        input[i] = tensors[i].input;
+        label[i] = tensors[i].label;
+        mHPlus[i] = tensors[i].mHPlus;
+        mH[i] = tensors[i].mH;
     }
 
-    return {torch::cat(input, 0), torch::cat(label, 0)};
+    return {torch::cat(input, 0), torch::cat(label, 0), torch::cat(mHPlus, 0), torch::cat(mH, 0)};
+}
+
+DNNTensor DNNDataSet::Get(const std::size_t& entry){
+    std::size_t fIdx, entr;
+    std::tie(fIdx, entr) = entryList.at(entry);
+
+    readers.at(fIdx)->SetEntry(entr);
+    std::vector<float> paramValues(parameters.size());
+
+    for(std::size_t idx = 0; idx < parameters.size(); ++idx){
+        paramValues.at(idx) = functions.at(fIdx*parameters.size() + idx).Get();
+        if(std::isnan(paramValues.at(idx))) paramValues.at(idx) = 0;
+    }
+
+    return {torch::from_blob(paramValues.data(), {1, paramValues.size()}).clone().to(device), 
+            OneHotEncoder(classLabel, nClass, device),
+            torch::full({1, 1}, (float)chargedMasses.at(classLabel == nClass - 1 ? fIdx : std::experimental::randint(0, (int)chargedMasses.size() -1))).to(device),
+            torch::full({1, 1}, (float)neutralMasses.at(classLabel == nClass - 1 ? fIdx : std::experimental::randint(0, (int)neutralMasses.size() -1))).to(device),
+    };
 }
