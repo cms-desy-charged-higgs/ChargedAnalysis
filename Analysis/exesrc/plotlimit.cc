e44ee2c6a04c4c1e9bd3d1f0434d223d87ca74eb
DaveBrun94 <david.brunner@desy.de>
2021-10-29 12:22:48 +0200

    Give masses as tuple

--- Analysis/exesrc/plotlimit.cc
+++ Analysis/exesrc/plotlimit.cc
@@ -3,13 +3,22 @@
 
 #include <ChargedAnalysis/Analysis/include/plotterLimit.h>
 #include <ChargedAnalysis/Utility/include/parser.h>
+#include <ChargedAnalysis/Utility/include/stringutil.h>
 
 int main(int argc, char* argv[]){
     //Parser arguments
     Parser parser(argc, argv);
 
-    std::vector<int> chargedMasses = parser.GetVector<int>("charged-masses");
-    std::vector<int> neutralMasses = parser.GetVector<int>("neutral-masses");
+    std::vector<std::string> masses = parser.GetVector<std::string>("masses");
+    std::vector<int> neutralMasses, chargedMasses;
+
+    for(const std::string& mass : masses){
+        std::vector<int> m = StrUtil::Split<int>(mass, "-");
+
+        chargedMasses.push_back(m.at(0));   
+        neutralMasses.push_back(m.at(1));
+    }
+
     std::string era = parser.GetValue<std::string>("era");
     std::string channel = parser.GetValue<std::string>("channel");
     std::vector<std::string> limitFiles = parser.GetVector<std::string>("limit-files");
